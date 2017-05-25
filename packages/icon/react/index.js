763c706455831e1a92a8a171daf56ce7f3544d0e
jaketrent <trent.jake@gmail.com>
2017-05-25 12:37:44 -0600

    feat(icon): icon component initial commit; logo only

---
+++ packages/icon/react/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styleable from 'react-styleable'
+
+import css from './index.module.css'
+
+import logo from '../svg/logo.icon.svg'
+
+const ids = {
+  logo
+}
+
+export default styleable(css)(props => (
+  <span className={props.css.root}>
+    {React.createElement(ids[props.id])}
+  </span>
+))
