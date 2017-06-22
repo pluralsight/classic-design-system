81f68ebd40343a9fb1b3c4f6453a77ea27dd7a15
jaketrent <trent.jake@gmail.com>
2017-06-22 15:54:47 -0600

    feat(examples): scss core elements example

---
+++ examples/scss/README.md
@@ -0,0 +1,12 @@
+## SCSS Install Example
+
+An example of how to integrate the SCSS-version Pluralsight Design System Core elements into a project.
+
+## To Run
+
+```
+nvm use
+npm install
+npm start
+open http://localhost:8080
+```
---
+++ examples/scss/src/app.js
@@ -0,0 +1,20 @@
+// import of react version of design system component
+import Button from '@pluralsight/ps-button/react'
+import React from 'react'
+import styleable from 'react-styleable'
+
+import css from './app.module.scss'
+
+export default styleable(css)(props =>
+  <div className={props.css.app}>
+    <header>
+      <h3>Pluralsight Design System</h3>
+      <h2>Sass Install Example</h2>
+    </header>
+    <div className={props.css.box}>
+      <Button size="large" onClick={_ => alert("It's working")}>
+        Design System component
+      </Button>
+    </div>
+  </div>
+)
