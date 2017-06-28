24cef46185bbfbbb53c6f448311d908010e24988
jaketrent <trent.jake@gmail.com>
2017-06-28 16:15:50 -0600

    refactor(normalize): add build; always build; entry point is built
    BREAKING CHANGE this is now normal css, diff entry point.  Possibly no
    huge deal, because vanilla css should be a subset of and work in
    preprocessors.  Just being careful.

---
+++ packages/normalize/build/index.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const { fs, postcss } = require('@pluralsight/ps-design-system-build')
+;(async _ => {
+  await fs.mkdir('dist')
+
+  const src = path.join('css', 'index.css')
+  postcss.transform(src, path.join('dist', 'index.css'), [
+    require('postcss-import'),
+    require('postcss-cssnext')({ browsers: ['Last 2 versions', 'IE >= 10'] }),
+    require('cssnano')
+  ])
+})()
