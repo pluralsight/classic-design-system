01f002f340f6b96c788025785b80d1e7ee454676
jaketrent <trent.jake@gmail.com>
2017-06-29 11:34:45 -0600

    feat(examples): production build

---
+++ examples/prod-build/README.md
@@ -0,0 +1,11 @@
+## Production Build Example
+
+An example of how to include the Design System in a production webpack build.
+
+## To Run
+
+```
+nvm use
+npm install
+npm run build
+```
---
+++ examples/prod-build/src/app.js
@@ -0,0 +1,19 @@
+import Button from '@pluralsight/ps-button/react'
+import React from 'react'
+import styleable from 'react-styleable'
+
+import css from './app.module.css'
+
+export default styleable(css)(props =>
+  <div className={props.css.app}>
+    <header>
+      <h3>Pluralsight Design System</h3>
+      <h2>Prod Build Example</h2>
+    </header>
+    <div className={props.css.box}>
+      <Button size="large" onClick={_ => alert("It's working")}>
+        Design System component
+      </Button>
+    </div>
+  </div>
+)
