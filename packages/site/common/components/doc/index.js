1dcb1e66f3274fce063ac08aa65dbbb40be0a261
jaketrent <trent.jake@gmail.com>
2017-07-05 10:01:14 -0600

    feat(site): code highlight install markdown docs

--- packages/site/common/components/doc/index.js
+++ packages/site/common/components/doc/index.js
@@ -1,9 +1,23 @@
+import hljs from 'highlightjs'
 import Markdown from 'react-markdown'
 import React from 'react'
 import styleable from 'react-styleable'
 
 import css from './index.module.css'
 
-export default styleable(css)(props =>
-  <Markdown className={props.css.root} source={props.children} />
-)
+class Doc extends React.Component {
+  componentDidMount() {
+    document
+      .querySelectorAll('.' + this.props.css.root + ' pre code')
+      .forEach(node => hljs.highlightBlock(node))
+  }
+  componentShouldUpdate() {
+    return false
+  }
+  render() {
+    return (
+      <Markdown className={this.props.css.root} source={this.props.children} />
+    )
+  }
+}
+export default styleable(css)(Doc)
