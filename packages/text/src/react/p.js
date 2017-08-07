904581ee742e2d27beddf38b0d78ae3f8ad253d2
jaketrent <trent.jake@gmail.com>
2017-08-07 13:51:58 -0600

    refactor(text): p to use glamorous element
    Requires className casting.
    
    See https://gist.github.com/jaketrent/d7001b870952a5616a85543b1c207ebd

--- packages/text/src/react/p.js
+++ packages/text/src/react/p.js
@@ -1,17 +1,12 @@
-import classNames from 'classnames'
 import core from '@pluralsight/ps-design-system-core'
-import * as glamor from 'glamor'
+import glamorous from 'glamorous'
 import React from 'react'
 
-const formatClassname = props =>
-  classNames({
-    [glamor.css({
-      color: core.colors.bone,
-      fontSize: core.type.fontSizeSmall,
-      margin: `${core.type.spacingSmall} 0`,
-      lineHeight: core.type.lineHeightStandard
-    })]: true,
-    [props.className]: props.className
-  })
+const P = glamorous.p({
+  color: core.colors.bone,
+  fontSize: core.type.fontSizeSmall,
+  margin: `${core.type.spacingSmall} 0`,
+  lineHeight: core.type.lineHeightStandard
+})
 
-export default props => <p {...props} className={formatClassname(props)} />
+export default props => <P {...props} className={`${props.className}`} />
