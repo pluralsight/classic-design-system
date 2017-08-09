6c8f1e3bbda4b6f4653ed5c13610fe19c80863bc
jaketrent <trent.jake@gmail.com>
2017-08-09 14:59:34 -0600

    fix(button): broken test from prebuild movearound

--- packages/button/src/react/__specs__/index.spec.js
+++ packages/button/src/react/__specs__/index.spec.js
@@ -2,7 +2,7 @@ import identity from 'identity-obj-proxy'
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import { Button } from '../index'
+import Button from '../index'
 
 test('renders correctly', () => {
   const tree = renderer.create(<Button css={identity}>Label</Button>)
