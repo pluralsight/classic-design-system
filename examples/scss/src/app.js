3869f7fa191ba2d213eac2ba03ece024aadb3208
jaketrent <trent.jake@gmail.com>
2017-08-10 10:43:15 -0600

    refactor(examples): rm build; inline button

--- examples/scss/src/app.js
+++ examples/scss/src/app.js
@@ -1,20 +1,18 @@
 // import of react version of design system component
 import Button from '@pluralsight/ps-design-system-button/react'
 import React from 'react'
-import styleable from 'react-styleable'
 
 import css from './app.module.scss'
 
-export default styleable(css)(props =>
-  <div className={props.css.app}>
+export default props =>
+  <div className={css.app}>
     <header>
       <h3>Pluralsight Design System</h3>
       <h2>Sass Install Example</h2>
     </header>
-    <div className={props.css.box}>
+    <div className={css.box}>
       <Button size="large" onClick={_ => alert("It's working")}>
         Design System component
       </Button>
     </div>
   </div>
-)
--- examples/scss/webpack.config.js
+++ examples/scss/webpack.config.js
@@ -1,48 +1,49 @@
-const {
-  decorateConfig
-} = require('@pluralsight/ps-design-system-build/webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const htmlWebpackTemplate = require('html-webpack-template')
 const path = require('path')
 
-module.exports = decorateConfig(
-  {
-    entry: './src/index.js',
-    module: {
-      rules: [
-        {
-          test: /\.module\.scss/,
-          use: [
-            'style-loader',
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                importLoaders: 1,
-                localIdentName: '[local]___[hash:base64:5]'
-              }
-            },
-            'sass-loader'
-          ],
-          include: [path.resolve('src')]
-        }
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        inject: false,
-        template: htmlWebpackTemplate,
-        appMountId: 'app',
-        // NOTE: because sass importing is weird
-        links: [
-          'https://cloud.typography.com/6966154/691568/css/fonts.css',
-          'node_modules/@pluralsight/ps-design-system-normalize/index.css'
-        ]
-      })
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { babelrc: false, presets: ['react', 'es2015'] }
+          }
+        ],
+        include: [path.resolve('src')]
+      },
+      {
+        test: /\.module\.scss/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[local]___[hash:base64:5]'
+            }
+          },
+          'sass-loader'
+        ],
+        include: [path.resolve('src')]
+      }
     ]
   },
-  {
-    extraInclude: [path.resolve('src')],
-    packageJson: require('./package.json')
-  }
-)
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: htmlWebpackTemplate,
+      appMountId: 'app',
+      // NOTE: because sass importing is weird
+      links: [
+        'https://cloud.typography.com/6966154/691568/css/fonts.css',
+        'node_modules/@pluralsight/ps-design-system-normalize/index.css'
+      ]
+    })
+  ]
+}
