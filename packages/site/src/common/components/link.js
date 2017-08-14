a7d9b1c36b3b696d5ebd0490d5d611d9be17a020
jaketrent <trent.jake@gmail.com>
2017-08-14 15:04:52 -0600

    fix(site): scroll to top when nav between pages
    Fixes #23

--- packages/site/src/common/components/link.js
+++ packages/site/src/common/components/link.js
@@ -3,9 +3,18 @@ import { NavLink } from 'react-router-dom'
 import React from 'react'
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick(evt) {
+    if (document)
+      document.body.scrollTop = document.documentElement.scrollTop = 0
+    if (this.props.onClick) this.props.onClick(evt)
+  }
   render() {
     return (
-      <Link {...this.props}>
+      <Link {...this.props} onClick={this.handleClick}>
         {/^http/.test(this.props.href)
           ? <a href={this.props.href}>{this.props.children}</a>
           : <NavLink to={this.props.href}>{this.props.children}</NavLink>}
