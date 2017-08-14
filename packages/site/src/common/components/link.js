417d1245fe5c776de3ce44a27d1c36e3043141ce
jaketrent <trent.jake@gmail.com>
2017-08-14 14:26:23 -0600

    chore(site): regen

--- packages/site/src/common/components/link.js
+++ packages/site/src/common/components/link.js
@@ -2,9 +2,14 @@ import Link from '@pluralsight/ps-design-system-link/react'
 import { NavLink } from 'react-router-dom'
 import React from 'react'
 
-export default props =>
-  <Link {...props}>
-    {/^http/.test(props.href)
-      ? <a href={props.href}>{props.children}</a>
-      : <NavLink to={props.href}>{props.children}</NavLink>}
-  </Link>
+export default class extends React.Component {
+  render() {
+    return (
+      <Link {...this.props}>
+        {/^http/.test(this.props.href)
+          ? <a href={this.props.href}>{this.props.children}</a>
+          : <NavLink to={this.props.href}>{this.props.children}</NavLink>}
+      </Link>
+    )
+  }
+}
