f18f193f9e930b59d5f6461c93d3e69a1a076d88
jaketrent <trent.jake@gmail.com>
2017-08-18 11:16:59 -0600

    refactor(site): remove recommended paragraph

--- packages/site/src/install/index.js
+++ packages/site/src/install/index.js
@@ -8,20 +8,6 @@ export default _ =>
       <h1>Install FTW!</h1>
     </Heading>
     <Doc>{`
-## Recommended Usage
-
-Use npm. Follow these streamlined install instructions. Fun. Profit.
-
-## Use Components
-
-Each component is installed separately.  JavaScript is prebuilt as Node modules.  Assets are inlined.  Find and use what you need.  For example:
-
-\`\`\`bash
-npm install @pluralsight/ps-design-system-button --save-dev
-\`\`\`
-
-See individual reference pages for usage examples.
-
 ## Use Core
 
 First install the dependency:
@@ -35,6 +21,16 @@ Core design elements are represented as a set of variables.  We recommend you us
 For build support, consult the [CSS Build docs](/install/css).
 
 For usage details, see the [Core Usage docs](/core/usage).
+
+## Use Components
+
+Each component is installed separately.  JavaScript is prebuilt as Node modules.  Assets are inlined.  Find and use what you need.  For example:
+
+\`\`\`bash
+npm install @pluralsight/ps-design-system-button --save-dev
+\`\`\`
+
+See individual reference pages for usage examples.
 
 For full, working project examples, see the [examples on github](https://github.com/pluralsight/design-system/tree/master/examples).
 `}</Doc>
