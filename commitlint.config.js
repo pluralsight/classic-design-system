b79b2d2398d902d57e2a5ced4f09bfd7ec675cf4
jaketrent <trent.jake@gmail.com>
2017-08-18 10:31:27 -0600

    chore: update lerna scopes in commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,16 @@
+const Repository = require('lerna/lib/Repository')
+
+function getPackageAbbreviations() {
+  const repo = new Repository(process.cwd())
+  return repo.packages
+    .map(pkg => pkg.name)
+    .map(name => (name.charAt(0) === '@' ? name.split('/')[1] : name))
+    .map(name => (/-/.test(name) ? name.split('-').pop() : name))
+}
+
 module.exports = {
-  extends: ['@commitlint/config-angular', '@commitlint/config-lerna-scopes']
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'scope-enum': _ => [2, 'always', getPackageAbbreviations()]
+  }
 }
