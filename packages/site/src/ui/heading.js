962998884a274f5b4fa5f82caf5b2fd6c9d14055
jaketrent <trent.jake@gmail.com>
2017-08-22 14:44:29 -0600

    fix(site): margin and color on overridden heading styles

--- packages/site/src/ui/heading.js
+++ packages/site/src/ui/heading.js
@@ -21,6 +21,29 @@ const style = props =>
   )
 
 export default props =>
-  <Heading {...props} {...style(props)}>
-    {props.children}
-  </Heading>
+  <div className={`heading ${props.size}`}>
+    <Heading>
+      {props.children}
+    </Heading>
+    <style jsx>{`
+      /* TODO: style with jsx without global tag */
+      .heading :global(h1),
+      .heading :global(h2),
+      .heading :global(h3),
+      .heading :global(h4) {
+        color: ${core.colors.gray06}
+      }
+      .heading.${Heading.sizes.large} :global(h1),
+      .heading.${Heading.sizes.large} :global(h2),
+      .heading.${Heading.sizes.large} :global(h3),
+      .heading.${Heading.sizes.large} :global(h4) {
+         margin: 56px 0 0 0;
+      }
+      .heading.${Heading.sizes.xxLarge} :global(h1),
+      .heading.${Heading.sizes.xxLarge} :global(h2),
+      .heading.${Heading.sizes.xxLarge} :global(h3),
+      .heading.${Heading.sizes.xxLarge} :global(h4) {
+         margin: 56px 0;
+      }
+`}</style>
+  </div>
