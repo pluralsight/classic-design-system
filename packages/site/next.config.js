9c202e35c879665723549df49916362309d92c21
jaketrent <trent.jake@gmail.com>
2017-08-28 14:05:56 -0600

    refactor(site): setup nextjs export

---
+++ packages/site/next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // TODO: read directory to make more dynamic
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/install': { page: '/install' },
+      '/components/badge': { page: '/components/badge' },
+      '/components/button': { page: '/components/button' },
+      '/components/card': { page: '/components/card' },
+      '/components/icon': { page: '/components/icon' },
+      '/components/row': { page: '/components/row' },
+      '/components/tab': { page: '/components/tab' },
+      '/components/text': { page: '/components/text' },
+      '/core/build': { page: '/core/build' },
+      '/core/usage': { page: '/core/usage' },
+      '/core/color': { page: '/core/color' },
+      '/core/spacing': { page: '/core/spacing' },
+      '/core/typography': { page: '/core/typography' }
+    }
+  }
+}
