6f0ce567121e1759e4e4304e24f9e06f17fb9fe7
jaketrent <trent.jake@gmail.com>
2017-08-31 14:54:10 -0600

    feat(actionmenu): style hoverable nested menus

--- packages/actionmenu/src/react/index.js
+++ packages/actionmenu/src/react/index.js
@@ -3,34 +3,67 @@ import glamorous, { Div } from 'glamorous'
 import Icon from '@pluralsight/ps-design-system-icon/react'
 import PropTypes from 'prop-types'
 import React from 'react'
+import sizeMe from 'react-sizeme'
 
-const Menu = glamorous.div({
+import Arrow from './arrow'
+
+const origins = {
+  topLeft: 'topLeft',
+  topRight: 'topRight',
+  bottomRight: 'bottomRight',
+  bottomLeft: 'bottomLeft'
+}
+
+const menuPaddingVert = core.layout.spacingTiny
+const nestedMenuHorzOverlap = 0 // 3
+
+const Menu = glamorous.ul({
   position: 'absolute',
   display: 'inline-block',
+  marginLeft: 0,
   background: core.colors.white,
   borderRadius: '2px',
-  padding: `${core.layout.spacingTiny} 0`,
+  padding: `${menuPaddingVert} 0`,
+  minWidth: '160px',
   maxWidth: '320px',
-  overflow: 'hidden'
+  // overflow: 'hidden',
+  listStyle: 'none',
+  boxShadow: `0 2px 4px rgba(0, 0, 0, 0.5)`
 })
 
-const Item = glamorous.div({
+const itemHoverStyles = {
+  background: core.colors.bone,
+  '> ul': {
+    display: 'inline-block'
+  }
+}
+
+const Item = glamorous.li({
   display: 'flex',
+  width: '100%',
   alignItems: 'center',
   padding: `0 ${core.layout.spacingMedium}`,
+  '> ul': {
+    display: 'none'
+  },
+  ':hover': itemHoverStyles,
+  ':focus': itemHoverStyles,
+  ':active': itemHoverStyles
+})
+
+const ItemChildrenButton = glamorous.button({
+  flexGrow: 1,
+  padding: 0,
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  textAlign: 'left',
   fontSize: core.type.fontSizeSmall,
   lineHeight: core.type.lineHeightExtra,
   fontWeight: core.type.fontWeightMedium,
   cursor: 'pointer',
-  ':hover, :active, :focus': {
-    background: core.colors.bone
-  }
-})
-
-const ItemChildren = glamorous.div({
-  whiteSpace: 'nowrap',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis'
+  background: 'none',
+  border: 'none'
 })
 
 const IconComponent = props =>
@@ -42,16 +75,39 @@ const IconComponent = props =>
     <Icon id={props.iconId} size={Icon.sizes.small} />
   </Div>
 
+const NestedArrow = _ =>
+  <Div marginLeft="auto" paddingLeft={core.layout.spacingXSmall}>
+    <Arrow />
+  </Div>
+
+const renderNested = props =>
+  props.nested
+    ? React.cloneElement(props.nested, {
+        css: {
+          left: props.size.width + nestedMenuHorzOverlap,
+          top: `calc(-1 * ${menuPaddingVert})`
+        }
+      })
+    : null
+
+// TODO: if nested, no button, make li the button, add a tabindex
+// when click button, focus on menu
+// TODO: menu role tablist aria-expanded false by default
 const ItemComponent = props => {
   return (
     <Item css={props.css} className={props.className} iconId={props.iconId}>
       {props.iconId && <IconComponent iconId={props.iconId} />}
-      <ItemChildren>{props.children}</ItemChildren>
+      <ItemChildrenButton onClick={props.onClick}>
+        {props.children}
+      </ItemChildrenButton>
+      {props.nested && <NestedArrow />}
+      {renderNested(props)}
     </Item>
   )
 }
 ItemComponent.propTypes = {
-  iconId: PropTypes.oneOf(Object.keys(Icon.ids))
+  iconId: PropTypes.oneOf(Object.keys(Icon.ids)),
+  nested: PropTypes.element // ActionMenu
 }
 
 const Divider = glamorous.div({
@@ -65,10 +121,16 @@ const ActionMenuComponent = props =>
   <Menu css={props.css} className={props.className}>
     {props.children}
   </Menu>
+ActionMenuComponent.displayName = 'ActionMenu'
 
-ActionMenuComponent.Item = ItemComponent
+ActionMenuComponent.Item = sizeMe({ monitorWidth: true })(ItemComponent)
 ActionMenuComponent.Divider = Divider
-ActionMenuComponent.propTypes = {}
-ActionMenuComponent.defaultProps = {}
+ActionMenuComponent.origins = origins
+ActionMenuComponent.propTypes = {
+  origin: PropTypes.oneOf(Object.keys(origins))
+}
+ActionMenuComponent.defaultProps = {
+  origin: origins.topLeft
+}
 
 export default ActionMenuComponent
