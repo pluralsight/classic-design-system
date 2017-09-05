284376dc24f8ca74007ffb431ff025886e310426
jaketrent <trent.jake@gmail.com>
2017-09-05 13:06:42 -0600

    feat(actionmenu): arrow nav to/from nested menus

--- packages/actionmenu/src/react/index.js
+++ packages/actionmenu/src/react/index.js
@@ -5,8 +5,6 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import sizeMe from 'react-sizeme'
 
-console.log('module')
-
 import Arrow from './arrow'
 
 const origins = {
@@ -19,7 +17,7 @@ const origins = {
 const menuPaddingVert = core.layout.spacingTiny
 const nestedMenuHorzOverlap = 0 // 3
 
-const Menu = glamorous.ul({
+const Menu = glamorous.div({
   position: 'absolute',
   display: 'inline-block',
   marginLeft: 0,
@@ -35,40 +33,48 @@ const Menu = glamorous.ul({
 })
 
 const itemHoverStyles = {
-  background: core.colors.bone,
-  '> ul': {
-    display: 'inline-block'
-  }
+  background: core.colors.bone
+  // '> ul': {
+  //   display: 'inline-block'
+  // }
 }
 
-const Item = glamorous.li({
+const Item = glamorous.button({
   display: 'flex',
   width: '100%',
   alignItems: 'center',
   lineHeight: core.type.lineHeightExtra,
   fontWeight: core.type.fontWeightMedium,
+
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  textAlign: 'left',
+  cursor: 'pointer',
+  background: 'none',
+  border: 'none',
   padding: `0 ${core.layout.spacingMedium}`,
-  '> ul': {
-    display: 'none'
-  },
+  // '> ul': {
+  //   display: 'none'
+  // },
   ':hover': itemHoverStyles,
   ':focus': itemHoverStyles,
   ':active': itemHoverStyles
 })
 
-const ItemChildrenButton = glamorous.button({
-  flexGrow: 1,
-  padding: 0,
-  whiteSpace: 'nowrap',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  textAlign: 'left',
-  lineHeight: core.type.lineHeightExtra,
-  fontWeight: core.type.fontWeightMedium,
-  cursor: 'pointer',
-  background: 'none',
-  border: 'none'
-})
+// const ItemChildrenButton = glamorous.button({
+//   flexGrow: 1,
+//   padding: 0,
+//   whiteSpace: 'nowrap',
+//   overflow: 'hidden',
+//   textOverflow: 'ellipsis',
+//   textAlign: 'left',
+//   lineHeight: core.type.lineHeightExtra,
+//   fontWeight: core.type.fontWeightMedium,
+//   cursor: 'pointer',
+//   background: 'none',
+//   border: 'none'
+// })
 
 const IconComponent = props =>
   <Div
@@ -84,53 +90,63 @@ const NestedArrow = _ =>
     <Arrow />
   </Div>
 
-const renderNested = props =>
-  props.nested
-    ? React.cloneElement(props.nested, {
-        css: {
-          left: props.size.width + nestedMenuHorzOverlap,
-          top: `calc(-1 * ${menuPaddingVert})`
-        }
-      })
-    : null
-
-// TODO: if nested, no button, make li the button, add a tabindex
-// when click button, focus on menu
-// TODO: menu role tablist aria-expanded false by default
 class ItemComponent extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      isNestedRendered: false
+    }
     this.handleKeyDown = this.handleKeyDown.bind(this)
+    this.handleNestedClose = this.handleNestedClose.bind(this)
   }
   componentDidMount() {
-    console.log('mounted')
+    if (this.props.isActive) this.item.focus()
   }
   handleKeyDown(evt) {
-    console.log('keydown', evt.key)
     if (evt.key === 'ArrowRight') {
       evt.stopPropagation()
       evt.preventDefault()
+      if (this.props.nested) {
+        this.setState({ isNestedRendered: true })
+      }
     }
+  }
+  handleNestedClose() {
+    this.setState({ isNestedRendered: false })
+    this.item.focus()
+  }
+  renderNested() {
+    return this.state.isNestedRendered
+      ? React.cloneElement(this.props.nested, {
+          css: {
+            left: this.props.size.width + nestedMenuHorzOverlap,
+            top: `calc(-1 * ${menuPaddingVert})`
+          },
+          onClose: this.handleNestedClose
+        })
+      : null
   }
   render() {
     const filteredProps = {
       css: this.props.css,
       className: this.props.className,
-      iconId: this.props.iconId,
-      ...(this.props.nested ? { tabIndex: 0 } : {})
+      iconId: this.props.iconId
+      // ...(this.props.nested ? { tabIndex: 0 } : {})
     }
     return (
-      <Item {...filteredProps}>
-        {this.props.iconId && <IconComponent iconId={this.props.iconId} />}
-        <ItemChildrenButton
+      <div>
+        <Item
+          {...filteredProps}
           onClick={this.props.onClick}
           onKeyDown={this.handleKeyDown}
+          innerRef={el => (this.item = el)}
         >
+          {this.props.iconId && <IconComponent iconId={this.props.iconId} />}
           {this.props.children}
-        </ItemChildrenButton>
-        {this.props.nested && <NestedArrow />}
-        {renderNested(this.props)}
-      </Item>
+          {this.props.nested && <NestedArrow />}
+        </Item>
+        {this.renderNested()}
+      </div>
     )
   }
 }
@@ -146,16 +162,41 @@ const Divider = glamorous.div({
   margin: `${core.layout.spacingTiny} 0`
 })
 
-const ActionMenuComponent = props =>
-  <Menu css={props.css} className={props.className}>
-    {props.children}
-  </Menu>
+class ActionMenuComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+  }
+  handleKeyDown(evt) {
+    if (evt.key === 'ArrowLeft') {
+      evt.stopPropagation()
+      evt.preventDefault()
+      if (typeof this.props.onClose === 'function') this.props.onClose()
+    }
+  }
+  render() {
+    return (
+      <Menu
+        css={this.props.css}
+        className={this.props.className}
+        onKeyDown={this.handleKeyDown}
+      >
+        {React.Children.map(this.props.children, (child, i) =>
+          React.cloneElement(child, {
+            isActive: i === 0
+          })
+        )}
+      </Menu>
+    )
+  }
+}
 ActionMenuComponent.displayName = 'ActionMenu'
 
 ActionMenuComponent.Item = sizeMe({ monitorWidth: true })(ItemComponent)
 ActionMenuComponent.Divider = Divider
 ActionMenuComponent.origins = origins
 ActionMenuComponent.propTypes = {
+  onClose: PropTypes.func,
   origin: PropTypes.oneOf(Object.keys(origins))
 }
 ActionMenuComponent.defaultProps = {
