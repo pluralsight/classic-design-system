bedd53adc0ab2b9549c8a714ab5ccac334ab3c55
jaketrent <trent.jake@gmail.com>
2017-09-06 17:01:55 -0600

    feat(site): add favicon

--- packages/site/src/ui/head.js
+++ packages/site/src/ui/head.js
@@ -3,10 +3,12 @@ import React from 'react'
 
 export default _ =>
   <Head>
+    <title>Pluralsight Design System</title>
+    <link rel="shortcut icon" type="image/png" href="/static/img/favicon.png" />
+
     <meta name="HandheldFriendly" content="True" />
     <meta name="MobileOptimized" content="320" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Pluralsight Design System</title>
     <link
       rel="stylesheet"
       href="https://cloud.typography.com/6966154/691568/css/fonts.css"
