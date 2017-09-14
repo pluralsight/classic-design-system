070e5d0af7915b1b8d75386fcc002dbc8838b357
jaketrent <trent.jake@gmail.com>
2017-09-14 16:32:01 -0600

    feat(site): add in-app and panel tab examples
    Fixes #106

--- packages/site/pages/components/tab.js
+++ packages/site/pages/components/tab.js
@@ -10,8 +10,73 @@ import {
   Link,
   P,
   PageHeading,
+  SectionHeading,
   withServerProps
 } from '../../src/ui'
+
+class InAppExample extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { activeIndex: 2 }
+    this.menus = [1, 2, 3, 4, 5].map(i => ({
+      id: `example${i}`,
+      label: `Menu ${i}`,
+      content: `Menu stuff ${i}`
+    }))
+    this.handleTabClick = this.handleTabClick.bind(this)
+  }
+  handleTabClick(i) {
+    this.setState({ activeIndex: i })
+  }
+  render() {
+    return (
+      <div>
+        <SectionHeading>In-app example</SectionHeading>
+        <P>
+          The Tab component provides the look and feel and accessibility
+          standards
+          for the UI. You control the hiding and showing of content or other
+          interaction specific to your application.
+        </P>
+        <div className="app">
+          <Tab.List>
+            {this.menus.map((menu, i) =>
+              <Tab.ListItem
+                id={menu.id}
+                key={menu.id}
+                onClick={this.handleTabClick}
+                {...(i === this.state.activeIndex ? { active: true } : null)}
+              >
+                a - {menu.label}
+              </Tab.ListItem>
+            )}
+          </Tab.List>
+          {this.menus.map(
+            (menu, i) =>
+              i === this.state.activeIndex
+                ? <Tab.Panel labelledBy={menu.id} key={menu.id}>
+                    <div className="content">
+                      {menu.content}
+                    </div>
+                  </Tab.Panel>
+                : null
+          )}
+        </div>
+        <style jsx>{`
+          .app {
+            background: ${core.colors.gray06};
+            padding: ${core.layout.spacingSmall} ${core.layout.spacingLarge};
+          }
+          .content {
+            color: ${core.colors.white};
+            padding: ${core.layout.spacingLarge} 0;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
 
 export default withServerProps(_ =>
   <Chrome>
@@ -19,6 +84,7 @@ export default withServerProps(_ =>
 
       <PageHeading>Tab</PageHeading>
 
+      <SectionHeading>Tab List</SectionHeading>
       <P>
         Tabs are a navigational element used to show and pivot between related
         subsections of an interface.
@@ -40,6 +106,26 @@ export default withServerProps(_ =>
         ]}
       />
 
+      <SectionHeading>Tab Panel</SectionHeading>
+      <P>
+        Panels are wrappers that provide an accessibility link between the
+        Tab.ListItem and your content. Ensure your ids match to make the link.
+      </P>
+      <Example.React
+        orient="vertical"
+        includes={{ Tab }}
+        codes={[
+          `
+<Tab.List>
+  <Tab.ListItem id="menu1">Menu Item</Tab.ListItem>
+</Tab.List>
+<Tab.Panel labelledBy="menu1">
+  <div style={{ marginTop: 24 }}>Content stuff</div>
+</Tab.Panel>
+`
+        ]}
+      />
+      <InAppExample />
     </Content>
   </Chrome>
 )
