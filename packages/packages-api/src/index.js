a03e6076e62dddb693f0ed61ac07414955e0d28a
jaketrent <trent.jake@gmail.com>
2017-09-26 11:56:24 -0600

    feat(api): add cors

--- packages/packages-api/src/index.js
+++ packages/packages-api/src/index.js
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 
 import config from './config'
@@ -5,6 +6,7 @@ import packages from './packages'
 
 const app = express()
 
+app.use('/api', cors({ origin: '*' }))
 app.use('/api/v1/packages', packages)
 
 app.listen(config.port, _ =>
