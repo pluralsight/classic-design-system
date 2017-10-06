1d51fc50848f1a2762217fde5e395050cfec1045
jaketrent <trent.jake@gmail.com>
2017-10-06 13:17:42 -0600

    fix(notifier): await the promise so usages are tallied in time

--- packages/packages-notifier/src/index.js
+++ packages/packages-notifier/src/index.js
@@ -29,84 +29,88 @@ github.authenticate({
     { org: 'ps-dev', per_page: 100 },
     github
   )
-  allRepos.forEach(async repo => {
-    const allDeps = await deps.getForRepo(
-      { repo: repo.name, owner: repo.owner.login, path: '' },
-      github
-    )
-    const packageJson = allDeps.find(dep => dep.name === 'package.json')
-    const packagesDir = allDeps.find(
-      dep => dep.name === 'packages' && dep.size === 0
-    )
-    const lernaJson = allDeps.find(dep => dep.name === 'lerna.json')
-
-    if (packagesDir && lernaJson) {
-      // console.log('Found lerna.json and packages/. Recursing', repo.full_name)
-      const packageDirs = await deps.getForRepo(
-        {
-          repo: repo.name,
-          owner: repo.owner.login,
-          path: packagesDir.path
-        },
+  await Promise.all(
+    allRepos.map(async repo => {
+      const allDeps = await deps.getForRepo(
+        { repo: repo.name, owner: repo.owner.login, path: '' },
         github
       )
 
-      packageDirs.forEach(async dirDep => {
-        const pkgPackageJsonPath = dirDep.path + '/package.json'
-        const pkgPackageJson = await deps.getForRepo(
+      const packageJson = allDeps.find(dep => dep.name === 'package.json')
+      const packagesDir = allDeps.find(
+        dep => dep.name === 'packages' && dep.size === 0
+      )
+      const lernaJson = allDeps.find(dep => dep.name === 'lerna.json')
+
+      if (packagesDir && lernaJson) {
+        // console.log('Found lerna.json and packages/. Recursing', repo.full_name)
+        const packageDirs = await deps.getForRepo(
           {
             repo: repo.name,
             owner: repo.owner.login,
-            path: pkgPackageJsonPath
+            path: packagesDir.path
           },
           github
         )
-
-        if (pkgPackageJson.length > 0) {
-          const json = await deps.readJson(
-            { token: config.githubToken },
-            pkgPackageJson[0]
-          )
-
-          if (json) {
-            const designSystemPackages = deps.filterDesignSystem(json)
-            const count = Object.keys(designSystemPackages).length
-            if (count > 0) {
-              const path = `${repo.full_name}/${pkgPackageJsonPath}`
-              console.log(`Found usages in ${path}`)
-              console.log(designSystemPackages)
-              usages[path] = designSystemPackages
-            } else {
-              // console.log(
-              //   'No usage in repo',
-              //   repo.full_name,
-              //   `in ${pkgPackageJsonPath}`
-              // )
+        await Promise.all(
+          packageDirs.map(async dirDep => {
+            const pkgPackageJsonPath = dirDep.path + '/package.json'
+            const pkgPackageJson = await deps.getForRepo(
+              {
+                repo: repo.name,
+                owner: repo.owner.login,
+                path: pkgPackageJsonPath
+              },
+              github
+            )
+            if (pkgPackageJson.length > 0) {
+              const json = await deps.readJson(
+                { token: config.githubToken },
+                pkgPackageJson[0]
+              )
+              if (json) {
+                const designSystemPackages = deps.filterDesignSystem(json)
+                const count = Object.keys(designSystemPackages).length
+                if (count > 0) {
+                  const path = `${repo.full_name}/${pkgPackageJsonPath}`
+                  // console.log(`Found usages in ${path}`)
+                  // console.log(designSystemPackages)
+                  usages[path] = designSystemPackages
+                } else {
+                  // console.log(
+                  //   'No usage in repo',
+                  //   repo.full_name,
+                  //   `in ${pkgPackageJsonPath}`
+                  // )
+                }
+              }
             }
+          })
+        )
+      }
+
+      if (packageJson) {
+        const json = await deps.readJson(
+          { token: config.githubToken },
+          packageJson
+        )
+        if (json) {
+          const designSystemPackages = deps.filterDesignSystem(json)
+          const count = Object.keys(designSystemPackages).length
+          if (count > 0) {
+            const path = `${repo.full_name}/package.json`
+            // console.log(`Found usages in ${path}`)
+            // console.log(designSystemPackages)
+            usages[path] = designSystemPackages
+          } else {
+            // console.log('No usage in repo', repo.full_name, 'in /package.json')
           }
         }
-      })
-    }
-
-    if (packageJson) {
-      const json = await deps.readJson(
-        { token: config.githubToken },
-        packageJson
-      )
-      if (json) {
-        const designSystemPackages = deps.filterDesignSystem(json)
-        const count = Object.keys(designSystemPackages).length
-        if (count > 0) {
-          const path = `${repo.full_name}/package.json`
-          console.log(`Found usages in ${path}`)
-          console.log(designSystemPackages)
-          usages[path] = designSystemPackages
-        } else {
-          // console.log('No usage in repo', repo.full_name, 'in /package.json')
-        }
       }
-    }
-  })
+
+      return Promise.resolve()
+    })
+  )
 
   // TODO: reenable
   // try {
@@ -124,7 +128,4 @@ github.authenticate({
   // } catch (err) {
   //   console.log('failure to post to slack', err)
   // }
-
-  console.log('usages', usages)
-  console.log('done.')
 })()
