2c8069c08ba4b80978446177cefae637c5d9400c
jaketrent <trent.jake@gmail.com>
2017-10-18 17:32:40 -0700

    refactor(site): use heading component sizes in wrapper

--- packages/site/src/ui/heading.js
+++ packages/site/src/ui/heading.js
@@ -4,10 +4,10 @@ import { Heading } from '@pluralsight/ps-design-system-text/react'
 
 const styleSize = ({ size }) =>
   ({
-    large: {
+    [Heading.sizes.large]: {
       margin: '56px 0 0 0'
     },
-    xxLarge: {
+    [Heading.sizes.xLarge]: {
       margin: '56px 0'
     }
   }[size])
@@ -20,30 +20,35 @@ const style = props =>
     styleSize(props)
   )
 
-export default props =>
+const HeadingComponent = props => (
   <div className={`heading ${props.size}`}>
-    <Heading size={props.size}>
-      {props.children}
-    </Heading>
+    <Heading size={props.size}>{props.children}</Heading>
     <style jsx>{`
       /* TODO: style with jsx without global tag */
       .heading :global(h1),
       .heading :global(h2),
       .heading :global(h3),
       .heading :global(h4) {
-        color: ${core.colors.gray06}
+        color: ${core.colors.gray06};
       }
       .heading.${Heading.sizes.large} :global(h1),
       .heading.${Heading.sizes.large} :global(h2),
       .heading.${Heading.sizes.large} :global(h3),
       .heading.${Heading.sizes.large} :global(h4) {
-         margin: 56px 0 0 0;
+        margin: 56px 0 0 0;
       }
-      .heading.${Heading.sizes.xxLarge} :global(h1),
-      .heading.${Heading.sizes.xxLarge} :global(h2),
-      .heading.${Heading.sizes.xxLarge} :global(h3),
-      .heading.${Heading.sizes.xxLarge} :global(h4) {
-         margin: 56px 0;
+      .heading.${Heading.sizes.xLarge} :global(h1),
+      .heading.${Heading.sizes.xLarge} :global(h2),
+      .heading.${Heading.sizes.xLarge} :global(h3),
+      .heading.${Heading.sizes.xLarge} :global(h4) {
+        margin: 56px 0;
       }
-`}</style>
+    `}</style>
   </div>
+)
+
+HeadingComponent.defaultProps = {
+  size: Heading.sizes.large
+}
+HeadingComponent.sizes = Heading.sizes
+export default HeadingComponent
