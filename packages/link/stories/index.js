5b906d952cb3518d339eea0f0f5501326e28100b
jaketrent <trent.jake@gmail.com>
2017-10-23 11:30:44 -0600

    chore(link): add own test suite; separate build

---
+++ packages/link/stories/index.js
@@ -0,0 +1,30 @@
+import backgrounds from '@storybook/addon-backgrounds'
+import core from '@pluralsight/ps-design-system-core'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { storiesOf } from '@storybook/react'
+
+import Link from '../react'
+
+const bg = backgrounds([
+  { name: 'product', value: core.colors.gray06, default: true }
+])
+
+const appearanceStory = storiesOf('appearance', module)
+  .addDecorator(bg)
+  .add('default', _ => (
+    <p style={{ color: 'white' }}>
+      <Link>
+        <a href="http://duckduckgo.com">Click me</a>
+      </Link>
+    </p>
+  ))
+Object.keys(Link.appearances).forEach(appearance =>
+  appearanceStory.add(appearance, _ => (
+    <p style={{ color: 'white' }}>
+      <Link appearance={appearance}>
+        <a href="http://duckduckgo.com">Click me</a>
+      </Link>
+    </p>
+  ))
+)
