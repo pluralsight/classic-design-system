ef38ceaa43d5f1efaa2f0fa1caa44cb148c722ec
jaketrent <trent.jake@gmail.com>
2017-11-03 12:03:48 -0600

    feat(site): motion reference page

--- packages/site/pages/core/motion.js
+++ packages/site/pages/core/motion.js
@@ -1,4 +1,6 @@
 import core from '@pluralsight/ps-design-system-core'
+import { transparentize } from 'polished'
+
 import {
   Chrome,
   Code,
@@ -12,6 +14,7 @@ import {
 const speeds = Object.keys(core.motion)
   .filter(key => /^speed/.test(key))
   .map(key => ({ varName: key, time: core.motion[key] }))
+  .reverse()
   .map((speed, i) => ({
     ...speed,
     title: ['Extra Slow', 'Slow', 'Normal', 'Fast', 'Extra Fast'][i],
@@ -68,6 +71,597 @@ const Speeds = _ => (
   </div>
 )
 
+const Dots = props => (
+  <div className="dots">
+    {props.arr.map((_, i, arr) => (
+      <div
+        className={`dot ${props.size === 'large' ? 'dotLarge' : ''}`}
+        style={{ ...props.style, opacity: (i + 1) / arr.length }}
+      />
+    ))}
+    <style jsx>{`
+      .dots {
+        display: flex;
+        margin: -1px;
+      }
+      .dot {
+        display: block;
+        height: 9px;
+        width: 9px;
+        margin: 1px;
+        background: ${core.colors.pink};
+        border-radius: 50%;
+      }
+      .dotLarge {
+        height: 75px;
+        width: 75px;
+      }
+    `}</style>
+  </div>
+)
+
+const VertTriangle = props => (
+  <div className="triangle">
+    <div className="graphic">
+      <svg viewBox="0 0 96 465" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <linearGradient x1="50%" y1="50%" x2="50%" y2="0%" id="vertTriangle">
+            <stop stopColor="#FFF" offset="0%" />
+            <stop stopColor="#FFF" stopOpacity="0" offset="100%" />
+          </linearGradient>
+        </defs>
+        <path
+          d="M48.458 466L96 1H0"
+          transform="translate(0 -1)"
+          fill="url(#vertTriangle)"
+          fillRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="label">{props.label}</div>
+    <style jsx>{`
+      .triangle {
+        position: relative;
+        height: 465px;
+      }
+      .graphic {
+        position: relative;
+      }
+      .graphic :global(svg) {
+        height: 465px;
+      }
+      .label {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) rotate(-90deg);
+        white-space: nowrap;
+        font-size: ${core.type.fontSizeSmall};
+        font-weight: ${core.type.fontWeightBold};
+        text-transform: uppercase;
+      }
+    `}</style>
+  </div>
+)
+
+const HorzTriangle = props => (
+  <div className="triangle">
+    <svg
+      className="graphic"
+      viewBox="0 0 466 95"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <linearGradient x1="50%" y1="50%" y2="50%" id="horzTriangle">
+          <stop stopColor="#FFF" offset="0%" />
+          <stop stopColor="#FFF" stopOpacity="0" offset="100%" />
+        </linearGradient>
+      </defs>
+      <path
+        d="M108 520.95L574 474v95"
+        transform="translate(-108 -474)"
+        fill="url(#horzTriangle)"
+        fillRule="evenodd"
+      />
+    </svg>
+    <div className="label">{props.label}</div>
+    <style jsx>{`
+      .triangle {
+        position: relative;
+        max-width: 475px;
+      }
+      .graphic {
+        position: relative;
+        width: 100%;
+        margin-top: 6px;
+      }
+      .label {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        white-space: nowrap;
+        font-size: ${core.type.fontSizeSmall};
+        font-weight: ${core.type.fontWeightBold};
+        text-transform: uppercase;
+      }
+    `}</style>
+  </div>
+)
+
+const Distance = _ => (
+  <div className="distance">
+    <Dots arr={[1, 2, 3, 4, 5, 6, 7]} />
+    <VertTriangle label="Relative Distance Travelled" />
+    <Dots arr={[1, 2]} />
+    <style jsx>{`
+      .distance {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        color: ${core.colors.black};
+      }
+    `}</style>
+  </div>
+)
+
+const Size = _ => (
+  <div className="size">
+    <Dots arr={[1]} />
+    <HorzTriangle label="Object Size" />
+    <Dots arr={[1]} size="large" style={{ transform: 'translateX(-100%)' }} />
+    <style jsx>{`
+      .size {
+        display: flex;
+        align-items: center;
+        color: ${core.colors.black};
+      }
+    `}</style>
+  </div>
+)
+const Square = props => (
+  <div className="squareContainer">
+    <div className="square" style={props.style}>
+      <div className="label">
+        <div className="icon">{props.icon}</div>
+        <div className="time">{props.time}</div>
+      </div>
+      {props.children}
+    </div>
+    <style jsx>{`
+      .squareContainer {
+        position: relative;
+        width: calc(100% - 85px);
+        height: 0;
+        padding-bottom: calc(100% - 85px);
+      }
+      .square {
+        display: flex;
+        align-items: flex-end;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        border-top: 1px dotted ${core.colors.blue};
+        border-right: 1px dotted ${core.colors.blue};
+        background: ${transparentize(0.8, core.colors.blue)};
+      }
+      .label {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 75px;
+        padding-top: ${core.layout.spacingMedium};
+        text-align: center;
+      }
+      .icon {
+        font-size: ${core.type.fontSizeLarge};
+      }
+      .time {
+        font-weight: ${core.type.fontWeightBold};
+      }
+    `}</style>
+  </div>
+)
+
+const Scale = _ => (
+  <div className="scale">
+    {speeds.reverse().reduce(
+      (acc, speed, i) => (
+        <Square
+          icon={speed.icon}
+          time={speed.time}
+          style={
+            i === speeds.length - 1
+              ? { border: `1px solid ${core.colors.blue}` }
+              : {}
+          }
+        >
+          {acc}
+        </Square>
+      ),
+      null
+    )}
+    <style jsx>{`
+      .scale {
+        max-width: 570px; /* 570px = 475px * 1.2 */
+        max-height: 570px; /* 570px = 475px * 1.2 */
+      }
+    `}</style>
+  </div>
+)
+
+const Relative = _ => (
+  <div className="relative">
+    <div className="lhs">
+      <Distance />
+    </div>
+    <div className="rhs">
+      <Scale />
+      <Size />
+    </div>
+    <style jsx>{`
+      .relative {
+        min-width: 766px; /* remove when ready to get further on responsiveness of this graphic */
+        display: flex;
+        justify-content: center;
+        padding: ${core.layout.spacingLarge} 0;
+        background: ${core.colors.bone};
+        border-radius: 12px;
+        color: ${core.colors.black};
+        overflow: hidden;
+      }
+      .lhs {
+        flex: 1;
+        display: flex;
+        justify-content: flex-end;
+      }
+      .rhs {
+        flex: 3;
+      }
+    `}</style>
+  </div>
+)
+
+const Travel = _ => (
+  <div className="travel">
+    <div className="lines">
+      <div className="line" />
+      <div className="line" />
+      <div className="line" />
+      <div className="line" />
+      <div className="line" />
+      <div className="line" />
+      <div className="line" />
+    </div>
+    <div className="pills">
+      <div className="pill pill1" />
+      <div className="pill2Container">
+        <div className="pill pill2" />
+      </div>
+      <div className="pill3Container">
+        <div className="pill pill3" />
+      </div>
+    </div>
+    <style jsx>{`
+      .travel {
+        position: relative;
+        height: 213px;
+        width: 100%;
+        background: ${core.colors.bone};
+        border-radius: 12px;
+        overflow: hidden;
+        margin-bottom: ${core.layout.spacingLarge};
+      }
+      .pills {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        padding: 0 ${core.layout.spacingMedium};
+      }
+      .pill {
+        background: ${core.colors.pink};
+        height: 44px;
+        margin-top: ${core.layout.spacingLarge};
+        border-radius: 22px;
+      }
+      @keyframes fullrange {
+        0% {
+          transform: translateX(0%);
+        }
+        50% {
+          transform: translateX(100%);
+        }
+        100% {
+          transform: translateX(0%);
+        }
+      }
+      @keyframes partialrange {
+        0% {
+          transform: translateX(0%);
+        }
+        50% {
+          transform: translateX(25%);
+        }
+        100% {
+          transform: translateX(0%);
+        }
+      }
+      .pill1 {
+        width: 50%;
+        animation: 2s 1s linear infinite fullrange;
+      }
+      .pill2Container {
+        width: calc(100% - 44px);
+        animation: 1600ms 1s linear infinite fullrange;
+      }
+      .pill3Container {
+        width: calc(100% - 44px);
+        animation: 800ms 1s linear infinite partialrange;
+      }
+      .pill2,
+      .pill3 {
+        width: 44px;
+      }
+      .lines {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        padding: ${core.layout.spacingSmall} 12.5% ${core.layout.spacingSmall} 0;
+      }
+      .line {
+        flex: 1;
+        border-right: 1px dashed ${core.colors.blue};
+      }
+    `}</style>
+  </div>
+)
+
+const Easing = _ => (
+  <div className="easing">
+    <div className="functions">
+      <div className="function">
+        <div className="label">
+          Use <b>ease-in-out</b> for moving point to point.
+          <div className="dot" />
+        </div>
+        <div className="graphic">
+          <svg
+            width="151"
+            height="103"
+            viewBox="0 0 151 103"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fill="none" fill-rule="evenodd">
+              <path
+                d="M1 100h149"
+                stroke="#AAA"
+                strokeWidth="2"
+                opacity=".54"
+              />
+              <path
+                d="M76.12 2v98"
+                stroke="#979797"
+                strokeWidth="2"
+                opacity=".54"
+                strokeDasharray="4,4"
+              />
+              <path
+                d="M2 100.117s53.59 4.667 73.973-49.437C96.357-3.424 148.567 2.164 148.567 2.164"
+                stroke="#8FC4E9"
+                strokeWidth="4"
+                strokeLinecap="round"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+      <div className="function">
+        <div className="label">
+          Use <b>ease-out</b> for entering elements.
+          <div className="dot" />
+        </div>
+        <div className="graphic">
+          <svg
+            width="151"
+            height="102"
+            viewBox="0 0 151 102"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fill="none" fill-rule="evenodd">
+              <path
+                d="M1 100h149"
+                stroke="#AAA"
+                strokeWidth="2"
+                opacity=".54"
+              />
+              <path
+                d="M76.12 2v98"
+                stroke="#979797"
+                strokeWidth="2"
+                opacity=".54"
+                strokeDasharray="4,4"
+              />
+              <path
+                d="M2 99.953S56.412 2 148.567 2"
+                stroke="#8FC4E9"
+                strokeWidth="4"
+                strokeLinecap="round"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+      <div className="function">
+        <div className="label">
+          Use <b>ease-in</b> for exiting elements.
+          <div className="dot" />
+        </div>
+        <div className="graphic">
+          <svg
+            width="151"
+            height="102"
+            viewBox="0 0 151 102"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fill="none" fill-rule="evenodd">
+              <path
+                d="M1 100h149"
+                stroke="#AAA"
+                strokeWidth="2"
+                opacity=".54"
+              />
+              <path
+                d="M76.12 2v98"
+                stroke="#979797"
+                strokeWidth="2"
+                opacity=".54"
+                strokeDasharray="4,4"
+              />
+              <path
+                d="M2 99.953S133.785 68.603 148.567 2"
+                stroke="#8FC4E9"
+                strokeWidth="4"
+                strokeLinecap="round"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+      <div className="function">
+        <div className="label">
+          Use <b>linear</b> for opacity or color changes.
+          <div className="dot" />
+        </div>
+        <div className="graphic">
+          <svg
+            width="151"
+            height="103"
+            viewBox="0 0 151 103"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fill="none" fill-rule="evenodd">
+              <path
+                d="M1 100h149H1z"
+                stroke="#AAA"
+                strokeWidth="2"
+                opacity=".54"
+              />
+              <path
+                d="M76.12 2v98"
+                stroke="#979797"
+                strokeWidth="2"
+                opacity=".54"
+                strokeDasharray="4,4"
+              />
+              <path
+                d="M2 100.218L148.567 2"
+                stroke="#8FC4E9"
+                strokeWidth="4"
+                strokeLinecap="round"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div className="defaultFunction">
+      When in doubt, use <b>ease-in-out</b>.
+    </div>
+    <style jsx>{`
+      b {
+        font-weight: ${core.type.fontWeightBold};
+      }
+      .easing {
+        overflow: hidden;
+        margin-bottom: ${core.layout.spacingLarge};
+      }
+      .functions {
+        display: flex;
+        flex-wrap: wrap;
+        margin: calc(${core.layout.spacingLarge} / -2);
+      }
+      .function {
+        position: relative;
+        display: flex;
+        width: calc(50% - ${core.layout.spacingLarge});
+        margin: calc(${core.layout.spacingLarge} / 2);
+        padding: ${core.layout.spacingLarge};
+        background: ${core.colors.bone};
+        border-radius: 12px;
+      }
+      .defaultFunction {
+        margin-top: ${core.layout.spacingLarge};
+        text-align: center;
+        padding: ${core.layout.spacingLarge};
+        background: ${core.colors.bone};
+        border-radius: 12px;
+        font-size: ${core.type.fontSizeMedium};
+        line-height: ${core.type.lineHeightStandard};
+        font-weight: ${core.type.fontWeightLight};
+      }
+      .label {
+        font-size: ${core.type.fontSizeMedium};
+        line-height: ${core.type.lineHeightStandard};
+        font-weight: ${core.type.fontWeightLight};
+      }
+      .dot {
+        position: absolute;
+        left: ${core.layout.spacingLarge};
+        bottom: ${core.layout.spacingLarge};
+        height: 16px;
+        width: 16px;
+        border-radius: 50%;
+        background: ${core.colors.pink};
+      }
+    `}</style>
+  </div>
+)
+
+const Rotation = _ => (
+  <div className="rotation">
+    <div className="box boxLarge" />
+    <div className="box boxSmall" />
+    <style jsx>{`
+      .rotation {
+        display: flex;
+        margin-bottom: ${core.layout.spacingLarge};
+        padding: ${core.layout.spacingLarge};
+        background: ${core.colors.bone};
+        border-radius: 12px;
+        overflow: hidden;
+      }
+      @keyframes rotateme {
+        to {
+          transform: rotate(360deg);
+        }
+      }
+      .box {
+        background: ${core.colors.white};
+        margin-right: ${core.layout.spacingXLarge};
+        animation: 5s linear infinite rotateme;
+      }
+      .boxSmall {
+        border: ${core.layout.spacingXSmall} solid ${core.colors.pink};
+        height: 32px;
+        width: 32px;
+      }
+      .boxLarge {
+        border: ${core.layout.spacingSmall} solid ${core.colors.pink};
+        height: 128px;
+        width: 128px;
+      }
+    `}</style>
+  </div>
+)
+
 export default withServerProps(_ => (
   <Chrome>
     <Content title="Motion">
@@ -84,6 +678,41 @@ export default withServerProps(_ => (
         increments.
       </P>
       <Speeds />
+
+      <SectionHeading>Speed</SectionHeading>
+      <P>Some general guidance on selecting a duration.</P>
+      <Relative />
+
+      <P>
+        A small object moving a great relative distance may have slightly longer
+        duration so that the movement isn’t too fast and abrupt.
+      </P>
+      <Travel />
+      <Code lang="css">{`@import "@pluralsight/ps-design-system-core";
+.mySelector {
+  animation-duration: var(--psMotionSpeedXFast);
+}`}</Code>
+
+      <SectionHeading>Easing</SectionHeading>
+      <P>Some general guidance on selecting an easing curve.</P>
+      <Easing />
+      <Code lang="css">{`@import "@pluralsight/ps-design-system-core";
+.mySelector {
+  animation-timing-function: ease-in-out;
+}`}</Code>
+
+      <SectionHeading>Rotation</SectionHeading>
+      <P>
+        Generally, larger objects look more natural when rotating quicker than
+        smaller objects. The easing property for rotation should be{' '}
+        <b>linear</b>.
+      </P>
+      <Rotation />
+      <Code lang="css">{`@import "@pluralsight/ps-design-system-core";
+@keyframes rotateme { to { transform: rotate(360deg); }}
+.mySelector {
+  animation: var(--psMotionSpeedNormal) linear infinite rotateme;
+}`}</Code>
     </Content>
   </Chrome>
 ))
