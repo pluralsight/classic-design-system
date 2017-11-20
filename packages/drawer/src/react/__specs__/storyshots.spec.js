d61c1a9f9eaed481d23973d1e664951591c87416
Bressain Dinkelman <bressain-dinkelman@pluralsight.com>
2017-11-20 14:30:18 -0700

    feat(drawer): new component

---
+++ packages/drawer/src/react/__specs__/storyshots.spec.js
@@ -0,0 +1,14 @@
+import initStoryshots, { snapshotWithOptions } from '@storybook/addon-storyshots'
+
+function createNodeMock(element) {
+  if (element.type === 'div') {
+    return { style: {} }
+  }
+  return null
+}
+
+initStoryshots({
+  test: snapshotWithOptions({
+    createNodeMock
+  })
+})
