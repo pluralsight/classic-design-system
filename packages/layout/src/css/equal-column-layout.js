6dedb74001002ad92b7a3352392a71b8a999f550
jaketrent <trent.jake@gmail.com>
2017-12-08 13:44:40 -0700

    refactor(layout): increase equal column gutter to large

--- packages/layout/src/css/equal-column-layout.js
+++ packages/layout/src/css/equal-column-layout.js
@@ -6,10 +6,10 @@ export default {
   ['.psds-equal-column-layout']: {
     display: 'flex',
     flexWrap: 'wrap',
-    margin: `calc(${core.layout.spacingMedium} / -2)`
+    margin: `calc(${core.layout.spacingLarge} / -2)`
   },
   ['.psds-equal-column-layout__column']: {
-    margin: `calc(${core.layout.spacingMedium} / 2)`
+    margin: `calc(${core.layout.spacingLarge} / 2)`
   },
   [`.psds-equal-column-layout__column--count-${vars.counts.two}`]: {
     width: '100%'
@@ -18,23 +18,23 @@ export default {
     width: '100%'
   },
   [`.psds-equal-column-layout__column--count-${vars.counts.four}`]: {
-    width: `calc(50% - ${core.layout.spacingMedium})`
+    width: `calc(50% - ${core.layout.spacingLarge})`
   },
   [`.psds-equal-column-layout__column--count-${vars.counts.six}`]: {
-    width: `calc(50% - ${core.layout.spacingMedium})`
+    width: `calc(50% - ${core.layout.spacingLarge})`
   },
   '@media (min-width: 769px)': {
     [`.psds-equal-column-layout__column--count-${vars.counts.two}`]: {
-      width: `calc(50% - ${core.layout.spacingMedium})`
+      width: `calc(50% - ${core.layout.spacingLarge})`
     },
     [`.psds-equal-column-layout__column--count-${vars.counts.three}`]: {
-      width: `calc(33.33% - ${core.layout.spacingMedium})`
+      width: `calc(33.33% - ${core.layout.spacingLarge})`
     },
     [`.psds-equal-column-layout__column--count-${vars.counts.four}`]: {
-      width: `calc(25% - ${core.layout.spacingMedium})`
+      width: `calc(25% - ${core.layout.spacingLarge})`
     },
     [`.psds-equal-column-layout__column--count-${vars.counts.six}`]: {
-      width: `calc(16.66% - ${core.layout.spacingMedium})`
+      width: `calc(16.66% - ${core.layout.spacingLarge})`
     }
   }
 }
