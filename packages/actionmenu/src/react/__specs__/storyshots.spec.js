e6d8c88b95daa72cb6320675838ab6019119023a
jaketrent <trent.jake@gmail.com>
2017-12-14 15:31:04 -0700

    chore(actionmenu): fix long-busted snapshots

--- packages/actionmenu/src/react/__specs__/storyshots.spec.js
+++ packages/actionmenu/src/react/__specs__/storyshots.spec.js
@@ -1,3 +1,24 @@
-import initStoryshots from '@storybook/addon-storyshots'
+import initStoryshots, {
+  snapshotWithOptions
+} from '@storybook/addon-storyshots'
+import React from 'react'
 
-initStoryshots()
+function createNodeMock(element) {
+  return {}
+}
+
+// mocks for react-sizeme's use of findDOMNode and DOM element methods
+// makes snapshots less helpful, but at least they run
+jest.mock('react-dom', () => ({
+  findDOMNode: () => ({
+    appendChild: _ => {},
+    nodeType: 1,
+    ownerDocument: { body: { contains: _ => false } }
+  })
+}))
+
+initStoryshots({
+  test: snapshotWithOptions({
+    createNodeMock
+  })
+})
