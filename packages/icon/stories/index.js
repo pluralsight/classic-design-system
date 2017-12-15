6a4e860f05496e29604288d5a00036ebd1a9a871
jaketrent <trent.jake@gmail.com>
2017-12-15 11:06:06 -0700

    chore(icon): update dependencies; add tests, storybook

---
+++ packages/icon/stories/index.js
@@ -0,0 +1,28 @@
+import addons from '@storybook/addons'
+import core from '@pluralsight/ps-design-system-core'
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import themeDecorator from '@pluralsight/ps-design-system-storybook-addon-theme'
+
+import Icon from '../react'
+
+const colorStory = storiesOf('appearance', module).addDecorator(
+  themeDecorator(addons)
+)
+Object.keys(Icon.colors).forEach(color =>
+  colorStory.add(color, _ => <Icon color={color} id={Icon.ids.check} />)
+)
+
+const sizeStory = storiesOf('size', module).addDecorator(themeDecorator(addons))
+Object.keys(Icon.sizes).forEach(size =>
+  sizeStory.add(size, _ => (
+    <Icon color={Icon.colors.white} size={size} id={Icon.ids.check} />
+  ))
+)
+
+const idStory = storiesOf('id', module).addDecorator(themeDecorator(addons))
+Object.keys(Icon.ids).forEach(id =>
+  idStory.add(id, _ => (
+    <Icon color={Icon.colors.white} size={Icon.sizes.large} id={id} />
+  ))
+)
