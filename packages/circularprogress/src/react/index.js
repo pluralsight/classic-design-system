0a23f249954186f788808f03149cd15c518517a0
jaketrent <trent.jake@gmail.com>
2018-01-11 14:28:20 -0700

    feat(circularprogress): new component

---
+++ packages/circularprogress/src/react/index.js
@@ -0,0 +1,106 @@
+import core from '@pluralsight/ps-design-system-core'
+import * as glamor from 'glamor'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { names as themeNames } from '@pluralsight/ps-design-system-theme/react'
+import { transparentize } from 'polished'
+
+const style = {
+  width: 48,
+  strokeWidth: 4
+}
+const radius = style.width / 2 - style.strokeWidth / 2
+const circumference = 2 * Math.PI * radius
+const spinAnimation = glamor.css.keyframes({
+  to: {
+    transform: 'rotate(270deg)'
+  }
+})
+
+const css = {
+  bg: ({ themeName }) =>
+    glamor.css({
+      fill: 'transparent',
+      strokeWidth: `${style.strokeWidth}px`,
+      stroke:
+        themeName === themeNames.light
+          ? transparentize(0.8, core.colors.gray04)
+          : core.colors.gray04
+    }),
+  fg: ({ themeName }) =>
+    glamor.css({
+      fill: 'transparent',
+      strokeWidth: `${style.strokeWidth}px`,
+      stroke:
+        themeName === themeNames.light ? core.colors.gray04 : core.colors.white,
+      transition: 'stroke-dashoffset 1s'
+    }),
+  root: ({ size }) =>
+    glamor.css(
+      {
+        [sizes.small]: {
+          height: '24px',
+          width: '24px'
+        },
+        [sizes.medium]: {
+          height: '48px',
+          width: '48px'
+        }
+      }[size]
+    ),
+  svg: ({ value }) =>
+    glamor.css({
+      transform: 'rotate(-90deg)',
+      animation:
+        typeof value === 'undefined'
+          ? `${spinAnimation} 1s linear infinite`
+          : ''
+    })
+}
+
+export const sizes = {
+  small: 'small',
+  medium: 'medium'
+}
+
+const CircularProgress = (props, context) => {
+  const allProps = { ...props, themeName: context.themeName }
+  const value = typeof allProps.value === 'undefined' ? 25 : allProps.value
+  const dashOffset = (100 - value) / 100 * circumference
+
+  return (
+    <div {...css.root(allProps)}>
+      <svg
+        {...css.svg(allProps)}
+        viewBox="0 0 48 48"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle r={radius} cx="24" cy="24" {...css.bg(allProps)} />
+        <circle
+          r={radius}
+          cx="24"
+          cy="24"
+          {...css.fg(allProps)}
+          strokeDasharray={`${circumference} ${circumference}`}
+          strokeDashoffset={dashOffset}
+        />
+      </svg>
+    </div>
+  )
+}
+
+CircularProgress.propTypes = {
+  value: PropTypes.number,
+  size: PropTypes.oneOf(Object.keys(sizes))
+}
+CircularProgress.defaultProps = {
+  size: sizes.medium
+}
+CircularProgress.contextTypes = {
+  themeName: PropTypes.string
+}
+
+CircularProgress.sizes = sizes
+
+export default CircularProgress
---
+++ packages/circularprogress/stories/index.js
@@ -0,0 +1,60 @@
+import addons from '@storybook/addons'
+import core from '@pluralsight/ps-design-system-core'
+import * as glamor from 'glamor'
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Theme from '@pluralsight/ps-design-system-theme/react'
+import themeDecorator from '@pluralsight/ps-design-system-storybook-addon-theme'
+
+import CircularProgress from '../react'
+
+const valueStory = storiesOf('value', module).addDecorator(
+  themeDecorator(addons)
+)
+;[0, 25, 50, 75, 100, 33, 66].forEach(value =>
+  valueStory.add(`value ${value}`, _ => <CircularProgress value={value} />)
+)
+
+const sizeStory = storiesOf('size', module).addDecorator(themeDecorator(addons))
+Object.keys(CircularProgress.sizes).forEach(size =>
+  sizeStory.add(size, _ => <CircularProgress size={size} value={75} />)
+)
+
+const indeterminateStory = storiesOf('indeterminate', module).addDecorator(
+  themeDecorator(addons)
+)
+Object.keys(CircularProgress.sizes).forEach(size =>
+  indeterminateStory.add(size, _ => <CircularProgress size={size} />)
+)
+
+class AnimationDemo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: 0 }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const rando = Math.random() * 115
+      this.setState({ value: rando > 100 ? 100 : rando })
+    }, 1500)
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  render() {
+    return (
+      <div>
+        <CircularProgress value={this.state.value} />
+        <div style={{ color: core.colors.gray02 }}>
+          Value: {this.state.value}
+        </div>
+      </div>
+    )
+  }
+}
+
+const animationStory = storiesOf('animation', module)
+  .addDecorator(themeDecorator(addons))
+  .add('animates to new values', _ => <AnimationDemo />)
