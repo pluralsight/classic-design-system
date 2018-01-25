c33a0d366be91c8f6f221730c3699880e32959f6
jaketrent <trent.jake@gmail.com>
2018-01-25 16:39:42 -0700

    chore(site): regen

--- packages/button/src/react/__specs__/index.spec.js
+++ packages/button/src/react/__specs__/index.spec.js
@@ -10,12 +10,36 @@ test('click on button triggered once', () => {
     callCount += 1
   }
   const button = shallow(
-    <Button onClick={onClick} icon={<Icon id={Icon.ids.check} />}>
+    <Button
+      onClick={onClick}
+      disabled={true}
+      href="foo"
+      icon={<Icon id={Icon.ids.check} />}
+    >
       Clicks once
     </Button>
   )
   button.simulate('click')
   expect(callCount).toBe(1)
+})
+
+test('click on disabled button with href does not trigger onClick', () => {
+  let callCount = 0
+  const onClick = _ => {
+    callCount += 1
+  }
+  const button = mount(
+    <Button
+      disabled={true}
+      onClick={onClick}
+      href="https://foo.com"
+      icon={<Icon id={Icon.ids.check} />}
+    >
+      Can't Be Clicked
+    </Button>
+  )
+  button.simulate('click')
+  expect(callCount).toBe(0)
 })
 
 test('custom innerRef function called', done => {
