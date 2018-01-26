204f62f9cfcecdf8a426037e8de9c90b7617a218
jaketrent <trent.jake@gmail.com>
2018-01-26 10:07:52 -0700

    refactor(drawer): remove border top on first drawer
    To match the similar style of a sequence of rows
    
    Closes #159

--- packages/drawer/stories/index.js
+++ packages/drawer/stories/index.js
@@ -10,25 +10,25 @@ import themeDecorator from '@pluralsight/ps-design-system-storybook-addon-theme'
 import Drawer from '../react'
 
 class ControlledDrawerStory extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = { isOpen: true }
-    }
+  constructor(props) {
+    super(props)
+    this.state = { isOpen: true }
+  }
 
-    toggle = () => {
-      this.setState({ isOpen: !this.state.isOpen })
-    }
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
 
-    render() {
-      return (
-        <div>
-          <Button onClick={this.toggle}>Toggle drawer</Button>
-          <Drawer base={<div>Drawer Base Here</div>} isOpen={this.state.isOpen}>
-            Drawer Content Here
-          </Drawer>
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <Button onClick={this.toggle}>Toggle drawer</Button>
+        <Drawer base={<div>Drawer Base Here</div>} isOpen={this.state.isOpen}>
+          Drawer Content Here
+        </Drawer>
+      </div>
+    )
+  }
 }
 
 class OnToggleDrawerStory extends React.Component {
@@ -44,7 +44,10 @@ class OnToggleDrawerStory extends React.Component {
   render() {
     return (
       <div>
-        <Drawer base={<div>Drawer is {this.state.isOpen ? 'open' : 'closed'}</div>} onToggle={this.handleToggle}>
+        <Drawer
+          base={<div>Drawer is {this.state.isOpen ? 'open' : 'closed'}</div>}
+          onToggle={this.handleToggle}
+        >
           Drawer Content Here
         </Drawer>
       </div>
@@ -54,17 +57,13 @@ class OnToggleDrawerStory extends React.Component {
 
 storiesOf('drawer', module)
   .addDecorator(story => (
-    <div style={{ color: 'white', padding: 48, fontSize: 24 }}>
-      {story()}
-    </div>
+    <div style={{ color: 'white', padding: 48, fontSize: 24 }}>{story()}</div>
   ))
   .addDecorator(themeDecorator(addons))
   .add('default', () => (
-    <Drawer base={<div>Drawer Base Here</div>}>
-      Drawer Content Here
-    </Drawer>
+    <Drawer base={<div>Drawer Base Here</div>}>Drawer Content Here</Drawer>
   ))
-  .add('controlled', () => <ControlledDrawerStory /> )
+  .add('controlled', () => <ControlledDrawerStory />)
   .add('using startOpen prop', () => (
     <Drawer startOpen base={<div>Base here</div>}>
       <div>Panel here</div>
@@ -72,34 +71,69 @@ storiesOf('drawer', module)
   ))
   .add('using onToggle prop', () => <OnToggleDrawerStory />)
   .add('with row component', () => (
-    <Drawer base={
-	    <Row image={<Row.Image src="https://cataas.com/cat"/>}
-		 title="Look at me! I'm a <Row />!"
-		 metadata1={['Kitten McCatbuns', '23 hours of cuteness']}
-		 actionBarVisible
-		 actionBar={[<Row.Action key="iHeartCats" icon={<Icon id="more"/>}/>]}
-	    />
-	}>
+    <Drawer
+      base={
+        <Row
+          image={<Row.Image src="https://cataas.com/cat" />}
+          title="Look at me! I'm a <Row />!"
+          metadata1={['Kitten McCatbuns', '23 hours of cuteness']}
+          actionBarVisible
+          actionBar={[
+            <Row.Action key="iHeartCats" icon={<Icon id="more" />} />
+          ]}
+        />
+      }
+    >
       Drawer Content Here
     </Drawer>
   ))
   .add('row component with actions', () => (
-    <Drawer base={
-	    <Row image={<Row.ImageLink><a href="https://duckduckgo.com?q=image"><img src="https://cataas.com/cat"/></a></Row.ImageLink>}
-		 title={<Row.TextLink><a href="https://duckduckgo.com?q=title">I'm a Row with Actions</a></Row.TextLink>}
-		 metadata1={[<Row.TextLink><a href="https://duckduckgo.com?q=cats">Kitten McCatbuns</a></Row.TextLink>, '23 hours of cuteness']}
-		 fullOverlay={
-		  <Row.FullOverlayLink>
-		    <a href="https://duckduckgo.com?q=overlay">
-		      Overlay
-		    </a>
-		  </Row.FullOverlayLink>
-		 }
-		 actionBarVisible
-		 actionBar={[<Row.Action key="iHeartCats" icon={<Icon id="more"/>} onClick={action('action')}/>]}
-	    />
-	}>
+    <Drawer
+      base={
+        <Row
+          image={
+            <Row.ImageLink>
+              <a href="https://duckduckgo.com?q=image">
+                <img src="https://cataas.com/cat" />
+              </a>
+            </Row.ImageLink>
+          }
+          title={
+            <Row.TextLink>
+              <a href="https://duckduckgo.com?q=title">
+                I'm a Row with Actions
+              </a>
+            </Row.TextLink>
+          }
+          metadata1={[
+            <Row.TextLink>
+              <a href="https://duckduckgo.com?q=cats">Kitten McCatbuns</a>
+            </Row.TextLink>,
+            '23 hours of cuteness'
+          ]}
+          fullOverlay={
+            <Row.FullOverlayLink>
+              <a href="https://duckduckgo.com?q=overlay">Overlay</a>
+            </Row.FullOverlayLink>
+          }
+          actionBarVisible
+          actionBar={[
+            <Row.Action
+              key="iHeartCats"
+              icon={<Icon id="more" />}
+              onClick={action('action')}
+            />
+          ]}
+        />
+      }
+    >
       Drawer Content Here
     </Drawer>
   ))
-
+  .add('stack of drawers', () => (
+    <div>
+      <Drawer base={<div>The Drawer #1</div>} />
+      <Drawer base={<div>The Drawer #2</div>} />
+      <Drawer base={<div>The Drawer #2</div>} />
+    </div>
+  ))
