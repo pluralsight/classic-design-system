2bc8e9d88ebd8356e2e1579c335324a842180890
jaketrent <trent.jake@gmail.com>
2018-02-06 16:53:24 -0700

    feat(site): add in-app example for tooltip

--- packages/site/pages/components/tooltip.js
+++ packages/site/pages/components/tooltip.js
@@ -1,4 +1,6 @@
+import Button from '@pluralsight/ps-design-system-button/react'
 import core from '@pluralsight/ps-design-system-core'
+import React from 'react'
 import Tooltip from '@pluralsight/ps-design-system-tooltip/react'
 
 import {
@@ -7,6 +9,7 @@ import {
   Content,
   Example,
   Heading,
+  Intro,
   Link,
   P,
   PageHeading,
@@ -15,10 +18,177 @@ import {
   withServerProps
 } from '../../src/ui'
 
+const styleUnder = (target, tooltip) => {
+  if (!target || !tooltip) return
+
+  const targetRect = target.getBoundingClientRect()
+  const tooltipRect = tooltip.getBoundingClientRect()
+
+  const scrollLeft = window.pageXOffset
+  const scrollTop = window.pageYOffset
+
+  const bufferHeight = 10
+  const targetCenterLeft = targetRect.left + targetRect.width / 2
+  const tooltipLeft = scrollLeft + targetCenterLeft - tooltipRect.width / 2
+  const tooltipTop =
+    scrollTop + targetRect.top + targetRect.height + bufferHeight
+
+  return {
+    position: 'absolute',
+    top: tooltipTop,
+    left: tooltipLeft
+  }
+}
+
+class TooltipPositioner extends React.Component {
+  constructor(props) {
+    super(props)
+    this.el = null
+    this.state = { rect: { top: 0, left: 0 } }
+  }
+  componentDidMount() {
+    this.setState({ rect: this.el.getBoundingClientRect() })
+  }
+  render() {
+    return React.cloneElement(this.props.children, {
+      innerRef: el => (this.el = el),
+      style: this.props.styleBy(this.el)
+    })
+  }
+}
+
+class InAppExample extends React.Component {
+  constructor(props) {
+    super(props)
+    this.examples = []
+    this.tips = []
+    this.state = {
+      examples: [],
+      tips: [],
+      isHovered: false,
+      isClicked: false
+    }
+    this.handleMouseOver = this.handleMouseOver.bind(this)
+    this.handleMouseOut = this.handleMouseOut.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  componentDidMount() {
+    this.setState({
+      examples: this.examples.map(el => el),
+      tips: this.tips.map(el => el)
+    })
+  }
+  handleClick() {
+    this.setState({ isClicked: !this.state.isClicked })
+  }
+  handleMouseOver() {
+    this.setState({ isHovered: true })
+  }
+  handleMouseOut() {
+    this.setState({ isHovered: false })
+  }
+  render() {
+    return (
+      <div className="examples">
+        <div className="example">
+          <Button
+            appearance={Button.appearances.flat}
+            className="text"
+            innerRef={el => (this.examples[0] = el)}
+          >
+            Look at me
+          </Button>
+          <TooltipPositioner styleBy={styleUnder.bind(null, this.examples[0])}>
+            <Tooltip
+              innerRef={el => (this.tips[0] = el)}
+              tailPosition={Tooltip.tailPositions.topCenter}
+            >
+              Tooltip
+            </Tooltip>
+          </TooltipPositioner>
+        </div>
+
+        <div className="example">
+          <Button
+            appearance={Button.appearances.flat}
+            className="text"
+            innerRef={el => (this.examples[1] = el)}
+            onMouseOver={this.handleMouseOver}
+            onMouseOut={this.handleMouseOut}
+          >
+            Hover me
+          </Button>
+        </div>
+        {this.state.isHovered && (
+          <TooltipPositioner styleBy={styleUnder.bind(null, this.examples[1])}>
+            <Tooltip
+              innerRef={el => (this.tips[1] = el)}
+              style={styleUnder(this.state.examples[1], this.state.tips[1])}
+              tailPosition={Tooltip.tailPositions.topCenter}
+            >
+              Is hovered
+            </Tooltip>
+          </TooltipPositioner>
+        )}
+
+        <div className="example">
+          <Button
+            appearance={Button.appearances.flat}
+            className="text"
+            innerRef={el => (this.examples[2] = el)}
+            onClick={this.handleClick}
+          >
+            Click me
+          </Button>
+        </div>
+        {this.state.isClicked && (
+          <TooltipPositioner styleBy={styleUnder.bind(null, this.examples[2])}>
+            <Tooltip
+              innerRef={el => (this.tips[2] = el)}
+              style={styleUnder(this.state.examples[2], this.state.tips[2])}
+              tailPosition={Tooltip.tailPositions.topCenter}
+            >
+              Was clicked
+            </Tooltip>
+          </TooltipPositioner>
+        )}
+
+        <style jsx>{`
+          .examples {
+            display: flex;
+            padding: ${core.layout.spacingLarge};
+            padding-bottom: 88px;
+            color: ${core.colors.gray02};
+            font-weight: ${core.type.fontWeightMedium};
+            background: ${core.colors.gray06};
+          }
+          .example {
+            margin-right: calc(${core.layout.spacingLarge} * 2);
+          }
+          .text {
+            display: inline-block;
+          }
+          .example:last-child {
+            margin-right: 0;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
 export default withServerProps(_ => (
   <Chrome>
     <Content title="Tooltip">
       <PageHeading packageName="tooltip">Tooltip</PageHeading>
+
+      <Intro>
+        The purpose of a tooltip is to provide context and explain the function
+        of a user interface element or feature. The content of a tooltip is
+        limited to styled text. If more cutomization is necessary, consider the{' '}
+        <Link href="/components/dialog">Dialog</Link> component which builds on
+        the patterns of the tooltip.
+      </Intro>
 
       <P>Install the component dependency:</P>
       <Code language="bash">
@@ -42,6 +212,13 @@ export default withServerProps(_ => (
             </span>
           ]),
           PropTypes.row([
+            'innerRef',
+            'DOM element => ()',
+            null,
+            null,
+            'react ref callback'
+          ]),
+          PropTypes.row([
             'tailPosition',
             PropTypes.union(Tooltip.tailPositions),
             null,
@@ -60,6 +237,12 @@ export default withServerProps(_ => (
         ]}
       />
 
+      <SectionHeading>In-app example</SectionHeading>
+      <P>
+        Tooltips can appear automatically, or be triggered by hover, focus, tap
+        or click.
+      </P>
+      <InAppExample />
       <SectionHeading>Appearance</SectionHeading>
       <P>
         Tooltips come in {Object.keys(Tooltip.appearances).length} styles.
