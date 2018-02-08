9b935e1602347d1e036e9aac6717deb7459bd1ed
jaketrent <trent.jake@gmail.com>
2018-02-07 17:06:37 -0700

    fix(build): dont strip % in keyframe value

--- packages/build/css/js-to-css.js
+++ packages/build/css/js-to-css.js
@@ -1,4 +1,17 @@
-const dashify = require('dashify')
+// based on https://github.com/jonschlinkert/dashify/blob/master/index.js
+// change: ignore non-word characters (eg, %)
+function dashify(str, options) {
+  if (typeof str !== 'string') {
+    throw new TypeError('expected a string')
+  }
+
+  return str
+    .trim()
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/^-+|-+$/g, '')
+    .replace(/-{2,}/g, m => (options && options.condense ? '-' : m))
+    .toLowerCase()
+}
 
 // based on https://raw.githubusercontent.com/tiaanduplessis/obj-to-css/master/index.js
 function toCss(obj = {}) {
