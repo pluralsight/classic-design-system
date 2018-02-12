2188583092c7181121e51b5420e4b03ed1aa2f2c
jaketrent <trent.jake@gmail.com>
2018-02-12 11:16:59 -0700

    feat(site): add guidelines to dialog

--- packages/site/pages/components/dialog.js
+++ packages/site/pages/components/dialog.js
@@ -2,13 +2,16 @@ import core from '@pluralsight/ps-design-system-core'
 import Button from '@pluralsight/ps-design-system-button/react'
 import Dialog from '@pluralsight/ps-design-system-dialog/react'
 import * as Text from '@pluralsight/ps-design-system-text/react'
+import { transparentize } from 'polished'
 
 import {
   Chrome,
   Code,
   Content,
   Example,
+  Guideline,
   Heading,
+  Intro,
   Link,
   P,
   PageHeading,
@@ -16,6 +19,59 @@ import {
   SectionHeading,
   withServerProps
 } from '../../src/ui'
+
+const ContentGridVisual = _ => (
+  <div className="grid">
+    <div className="box top" />
+    <div className="box hero" />
+    <div className="box body" />
+    <div className="box side" />
+    <style jsx>{`
+      .grid {
+        display: grid;
+        grid-gap: ${core.layout.spacingSmall};
+        grid-template-columns: [left] 5fr [right] 1fr [end];
+        grid-template-rows: [row1] 1fr [row2] 2fr [row3] 5fr [end];
+        height: 100%;
+        width: 100%;
+      }
+      .box {
+        border-radius: 2px;
+        background: ${transparentize(0.8, core.colors.gray03)};
+      }
+      .top {
+        grid-column: left / end;
+        grid-row: row1 / row2;
+      }
+      .hero {
+        grid-column: left / right;
+        grid-row: row2 / row3;
+      }
+      .body {
+        grid-column: left / right;
+        grid-row: row3 / end;
+      }
+      .side {
+        grid-column: right / end;
+        grid-row: row2 / end;
+      }
+    `}</style>
+  </div>
+)
+
+const ModalGuidelineExample = props => (
+  <div className="example" style={props.style}>
+    <ContentGridVisual />
+    {props.children}
+    <style jsx>{`
+      .example {
+        position: relative;
+        height: 100%;
+        width: 100%;
+      }
+    `}</style>
+  </div>
+)
 
 const ExampleContent = _ => (
   <div style={{ maxWidth: '300px' }}>
@@ -67,6 +123,14 @@ export default withServerProps(_ => (
   <Chrome>
     <Content title="Dialog">
       <PageHeading packageName="dialog">Dialog</PageHeading>
+
+      <Intro>
+        The purpose of a Dialog is to provide actionable messaging and may
+        appear contextually or as a <Link href="#modal">modal</Link>. The Dialog
+        adapts to various amounts and types of content. For contextual,
+        non-actionable messaging, consider the{' '}
+        <Link href="/components/tooltip">Tooltip</Link> instead.
+      </Intro>
 
       <P>Install the component dependency:</P>
       <Code language="bash">
@@ -196,6 +260,182 @@ export default withServerProps(_ => (
         of that node.
       </P>
       <ModalIframe />
+
+      <SectionHeading>Guidelines</SectionHeading>
+      <P>
+        In most cases, dialogs can assume the user to digest the content
+        following a Z-Pattern. Readers will start in the top/left, move
+        horizontally to the top/right and then diagonally to the bottom/left
+        before finishing with another horizontal movement to the bottom/right.
+        By that measure, place your actions at the bottom/right, with the
+        primary action on the right.{' '}
+      </P>
+      <Guideline
+        do={
+          <Dialog disableFocusOnMount>
+            <Text.Heading>
+              <h3>Title</h3>
+            </Text.Heading>
+            <Text.P>
+              Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation
+            </Text.P>
+            <div style={{ textAlign: 'right' }}>
+              <Button
+                appearance={Button.appearances.stroke}
+                style={{ marginRight: core.layout.spacingMedium }}
+              >
+                Secondary
+              </Button>
+              <Button>Primary</Button>
+            </div>
+          </Dialog>
+        }
+        dont={
+          <Dialog disableFocusOnMount>
+            <Text.Heading>
+              <h3>Title</h3>
+            </Text.Heading>
+            <Text.P>
+              Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation
+            </Text.P>
+            <div>
+              <Button style={{ marginRight: core.layout.spacingMedium }}>
+                Primary
+              </Button>
+              <Button appearance={Button.appearances.stroke}>Secondary</Button>
+            </div>
+          </Dialog>
+        }
+      />
+
+      <P>
+        Don’t use <Link href="#modal">modal</Link> dialogs when displaying
+        non-critical information that doesn’t need to interrupt a workflow.
+        Consider a dialog interaction which would be less intrusive.{' '}
+      </P>
+      <Guideline
+        do={
+          <ModalGuidelineExample>
+            <div className="dialog">
+              <Dialog disableFocusOnMount>
+                Bookmark added. <Link href="">Undo?</Link>
+              </Dialog>
+            </div>
+            <style jsx>{`
+              .dialog {
+                position: absolute;
+                bottom: ${core.layout.spacingSmall};
+                left: 50%;
+                transform: translateX(-50%);
+                white-space: nowrap;
+              }
+            `}</style>
+          </ModalGuidelineExample>
+        }
+        dont={
+          <div className="example">
+            <div className="background">
+              <ContentGridVisual />
+            </div>
+            <div className="fakeOverlay">
+              <Dialog disableFocusOnMount style={{ maxWidth: '70%' }}>
+                <Text.Heading>
+                  <h3>Bookmark added</h3>
+                </Text.Heading>
+                <Text.P>
+                  Oh joy! You have successfully added a new bookmark.
+                </Text.P>
+                <div style={{ textAlign: 'right' }}>
+                  <Button>Ok, got it</Button>
+                </div>
+              </Dialog>
+            </div>
+            <style jsx>{`
+              .example {
+                position: relative;
+                height: 100%;
+                width: 100%;
+                min-height: 400px;
+              }
+              .fakeOverlay {
+                position: absolute;
+                top: 0;
+                left: 0;
+                height: 100%;
+                width: 100%;
+                background-color: ${transparentize(0.5, core.colors.black)};
+                border-radius: 12px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
+              .background {
+                position: relative;
+                height: 100%;
+                width: 100%;
+                padding: ${core.layout.spacingXLarge};
+              }
+            `}</style>
+          </div>
+        }
+        dontStyle={{ padding: 0 }}
+      />
+
+      <P>
+        Be explicit as possible when writing dialog buttons. Use affirimitive
+        action text to clearly indicate the outcome of the decision. [<Link
+          appearance={Link.appearances.subtle}
+          href="https://material.io/guidelines/components/dialogs.html#dialogs-alerts"
+        >
+          Material Design
+        </Link>]
+      </P>
+      <Guideline
+        do={
+          <Dialog disableFocusOnMount>
+            <Text.Heading>
+              <h3>Delete record?</h3>
+            </Text.Heading>
+            <Text.P>
+              This action cannot be reversed. Are you sure you want to delete
+              this record?
+            </Text.P>
+            <div style={{ textAlign: 'right' }}>
+              <Button
+                appearance={Button.appearances.stroke}
+                style={{ marginRight: core.layout.spacingMedium }}
+              >
+                Delete
+              </Button>
+              <Button>Cancel</Button>
+            </div>
+          </Dialog>
+        }
+        dont={
+          <Dialog disableFocusOnMount>
+            <Text.Heading>
+              <h3>Delete record?</h3>
+            </Text.Heading>
+            <Text.P>
+              This action cannot be reversed. Are you sure you want to delete
+              this record?
+            </Text.P>
+            <div style={{ textAlign: 'right' }}>
+              <Button
+                appearance={Button.appearances.stroke}
+                style={{ marginRight: core.layout.spacingMedium }}
+              >
+                Yes
+              </Button>
+              <Button>No</Button>
+            </div>
+          </Dialog>
+        }
+      />
     </Content>
   </Chrome>
 ))
