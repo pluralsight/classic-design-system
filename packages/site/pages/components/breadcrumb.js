1ce099783c75effebe60af39516af9e6250a1ce2
jaketrent <trent.jake@gmail.com>
2018-02-21 13:57:39 -0700

    refactor(site): adjust breadcrumb docs

--- packages/site/pages/components/breadcrumb.js
+++ packages/site/pages/components/breadcrumb.js
@@ -1,6 +1,10 @@
 import core from '@pluralsight/ps-design-system-core'
 import React from 'react'
 import Breadcrumb from '@pluralsight/ps-design-system-breadcrumb/react'
+import { PageHeadingLayout } from '@pluralsight/ps-design-system-layout/react'
+import Tab from '@pluralsight/ps-design-system-tab/react'
+import Theme from '@pluralsight/ps-design-system-theme/react'
+import { transparentize } from 'polished'
 
 import {
   Chrome,
@@ -18,10 +22,66 @@ import {
   withServerProps
 } from '../../src/ui'
 
+const PinkBox = props => (
+  <div className="pink-box">
+    <style jsx>{`
+      .pink-box {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border-top: ${core.layout.spacingMedium} solid
+          ${transparentize(0.5, core.colors.pink)};
+        border-bottom: ${core.layout.spacingMedium} solid
+          ${transparentize(0.5, core.colors.pink)};
+      }
+    `}</style>
+  </div>
+)
+
+const InAppExample = _ => (
+  <div className="example">
+    <Theme name={Theme.names.dark}>
+      <PageHeadingLayout heading={<h2>Page title</h2>}>
+        <Tab.List>
+          <Tab.ListItem id="example1">Menu Item</Tab.ListItem>
+          <Tab.ListItem id="example2">Menu Item</Tab.ListItem>
+          <Tab.ListItem id="example3">Menu Item</Tab.ListItem>
+          <Tab.ListItem id="example4">Menu Item</Tab.ListItem>
+        </Tab.List>
+        <div className="boxContainer">
+          <PinkBox />
+          <Breadcrumb className="breadcrumb">All the things</Breadcrumb>
+        </div>
+      </PageHeadingLayout>
+      <Code language="javascript">
+        {`<Breadcrumb>All the things</Breadcrumb>`}
+      </Code>
+    </Theme>
+    <style jsx>{`
+      .example {
+        background: ${core.colors.gray06};
+      }
+      .boxContainer {
+        position: relative;
+        display: inline-block;
+      }
+      .breadcrumb {
+      }
+    `}</style>
+  </div>
+)
+
 export default withServerProps(_ => (
   <Chrome>
     <Content title="Breadcrumb">
       <PageHeading packageName="breadcrumb">Breadcrumb</PageHeading>
+
+      <Intro>
+        The breadcrumb is to provide a consistently-placed and styled affordance
+        to orient and move up a hierarchy.
+      </Intro>
 
       <P>Install the component dependency:</P>
       <Code language="bash">
@@ -52,30 +112,30 @@ export default withServerProps(_ => (
         ]}
       />
 
-      <SectionHeading>Placement</SectionHeading>
+      <SectionHeading>In-app example</SectionHeading>
       <P>
-        The breadcrumb may be placed either above or below tabs in the top-left
-        region of the page.
+        The breadcrumb should be placed toward the top left of the page. Below
+        any persistent heading or navigation elements.
       </P>
-      <Example.React
-        includes={{ Breadcrumb }}
-        codes={[`<Breadcrumb>All channels</Breadcrumb>`]}
-      />
+      <InAppExample />
 
       <SectionHeading>Guidelines</SectionHeading>
       <P>
-        No mention of "back to" should be used. Instead name the location that
-        the breadcrumb is linked to.
+        Reference the destination screen or page directly. Don't add text like
+        "Back to..."
       </P>
       <Guideline
-        do={<Breadcrumb>All channels</Breadcrumb>}
-        dont={<Breadcrumb>Back to channels</Breadcrumb>}
+        do={<Breadcrumb>All things</Breadcrumb>}
+        dont={<Breadcrumb>Back to all things</Breadcrumb>}
       />
 
-      <P>Breadcrumb labels should be written in sentence case.</P>
+      <P>
+        Breadcrumbs should be written in sentece case, except for proper nouns
+        or product titles.
+      </P>
       <Guideline
-        do={<Breadcrumb>All channels</Breadcrumb>}
-        dont={<Breadcrumb>All Channels</Breadcrumb>}
+        do={<Breadcrumb>All the things</Breadcrumb>}
+        dont={<Breadcrumb>All The Things</Breadcrumb>}
       />
     </Content>
   </Chrome>
