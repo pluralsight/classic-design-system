47bf58ad6c1c0249bbd891c2608ae6004e2b5e6b
jaketrent <trent.jake@gmail.com>
2018-04-23 14:15:15 -0600

    refactor(api): use child_process instead of npm cli to search
    Relies on console.log in stdout; npm@3 was broken with backend; npm@5 would not
    do parseable json (only console log);  therefore, why use the cli lib at all.
    If I have node, I should have npm.

--- packages/packages-api/src/packages/repo.js
+++ packages/packages-api/src/packages/repo.js
@@ -1,5 +1,5 @@
 // @flow
-import npm from 'npm'
+import child_process from 'child_process'
 
 type Packages = {
   [name: string]: string
@@ -11,25 +11,54 @@ type NpmResult = {
   [packageName: string]: NpmPackage
 }
 
-export const findAll = (): Promise<Packages> =>
+type NpmSearchResult = {
+  name: string,
+  version: string
+}
+const searchNpm = (): Promise<NpmSearchResult[]> =>
   new Promise((resolve, reject) => {
-    npm.load({}, _ => {
-      npm.commands.search(
-        'ps-design-system',
-        true,
-        (err, result: NpmResult) => {
-          if (err) {
-            console.log('Error searching packages', err)
-            reject(err)
-          }
-
-          const packages = Object.keys(result).reduce((acc, pkgName) => {
-            acc[pkgName] = result[pkgName].version
-            return acc
-          }, {})
-
-          resolve(packages)
+    child_process.exec(
+      'npm search ps-design-system --json --parseable',
+      {
+        cwd: __dirname,
+        encoding: 'utf8',
+        timeout: 5000
+      },
+      (err, stdout, stderr) => {
+        if (err) {
+          console.log('Error searching packages', err)
+          return reject(err)
         }
-      )
-    })
+
+        if (stderr) {
+          console.log('Error output when searching packages', stderr)
+          return reject(new Error(stderr))
+        }
+
+        let json
+        try {
+          if (typeof stdout === 'string') {
+            json = JSON.parse(stdout)
+            return resolve(json)
+          } else {
+            return reject(new Error('Child process output must be a string'))
+          }
+        } catch (err) {
+          return reject(err)
+        }
+      }
+    )
   })
+
+export const findAll = async (): Promise<Packages> => {
+  try {
+    const list = await searchNpm()
+    return list.reduce((acc, searchResult) => {
+      acc[searchResult.name] = searchResult.version
+      return acc
+    }, {})
+  } catch (err) {
+    console.log('findAll repo error', err)
+    return {}
+  }
+}
