ae9e554935e34808c7970554074d7cf0231818c5
jaketrent <trent.jake@gmail.com>
2018-06-19 15:22:26 -0600

    feat(site): add live example for form

--- packages/site/pages/components/form.js
+++ packages/site/pages/components/form.js
@@ -1,14 +1,17 @@
 import ActionMenu from '@pluralsight/ps-design-system-actionmenu/react'
+import Banner from '@pluralsight/ps-design-system-banner/react'
 import Button from '@pluralsight/ps-design-system-button/react'
+import Checkbox from '@pluralsight/ps-design-system-checkbox/react'
 import core from '@pluralsight/ps-design-system-core'
+import Dropdown from '@pluralsight/ps-design-system-dropdown/react'
 import Form from '@pluralsight/ps-design-system-form/react'
 import Icon from '@pluralsight/ps-design-system-icon/react'
 import Text from '@pluralsight/ps-design-system-text/react'
 import TextInput from '@pluralsight/ps-design-system-textinput/react'
 import TextArea from '@pluralsight/ps-design-system-textarea/react'
 import Radio from '@pluralsight/ps-design-system-radio/react'
-import Checkbox from '@pluralsight/ps-design-system-checkbox/react'
-import Dropdown from '@pluralsight/ps-design-system-dropdown/react'
+import Theme from '@pluralsight/ps-design-system-theme/react'
+import { string as stringUtil } from '@pluralsight/ps-design-system-util'
 
 import {
   Chrome,
@@ -118,6 +121,196 @@ const Comp = props => (
   </div>
 )
 
+const validate = state => {
+  const rules = {
+    name: { rule: /.+/, message: 'Required' },
+    level: {
+      rule: /beginner|intermediate|advanced/,
+      message: 'Select a valid option'
+    },
+    slides: {
+      rule: /false|true/,
+      message: 'Turn on or off'
+    },
+    desc: { rule: /.+/, message: 'Required' }
+  }
+  return Object.keys(rules).reduce(
+    (errors, ruleName) => {
+      if (rules[ruleName].rule.test(state[ruleName])) {
+        delete errors[ruleName]
+      } else {
+        errors[ruleName] = rules[ruleName].message
+      }
+      return errors
+    },
+    { ...state.errors }
+  )
+}
+
+const initialState = {
+  errors: {},
+  isSubmitting: false,
+  isSubmitted: false,
+  name: '',
+  level: null,
+  slides: false,
+  slidestech: null,
+  desc: ''
+}
+
+class InAppExample extends React.Component {
+  constructor() {
+    super()
+    this.state = initialState
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.reset = this.reset.bind(this)
+  }
+  handleChange(evt) {
+    const { name, value } = evt.target
+    console.log('target', evt.target, 'name', name, 'value', value)
+    this.setState({
+      [name]: value
+    })
+  }
+  handleSubmit(evt) {
+    evt.preventDefault()
+    const errors = validate(this.state)
+    const hasErrors = Object.keys(errors).length > 0
+    this.setState({ errors })
+    if (hasErrors) {
+    } else {
+      this.setState({ isSubmitting: true }, _ => {
+        setTimeout(
+          _ =>
+            this.setState({ isSubmitting: false, isSubmitted: true }, _ =>
+              setTimeout(_ => this.reset(), 800)
+            ),
+          1500
+        )
+      })
+    }
+  }
+  reset(evt) {
+    this.setState(initialState)
+  }
+  render() {
+    const { props, state } = this
+    const errorMsg = name => state.errors[name]
+    const isError = name => !!errorMsg(name)
+    const hasErrors = Object.keys(state.errors).length > 0
+    return (
+      <div className="example">
+        {state.isSubmitted && (
+          <Banner
+            color={Banner.colors.green}
+            style={{ position: 'absolute', top: '0', left: '0' }}
+          >
+            Course created!
+          </Banner>
+        )}
+        {hasErrors && (
+          <Banner
+            color={Banner.colors.red}
+            style={{ position: 'absolute', top: '0', left: '0' }}
+          >
+            Failed: Sample course form has errors
+          </Banner>
+        )}
+        <Theme>
+          <form onSubmit={this.handleSubmit}>
+            <Form.VerticalLayout>
+              <Text.Heading>
+                <h2>Course sample form</h2>
+              </Text.Heading>
+              <TextInput
+                error={isError('name')}
+                onChange={this.handleChange}
+                name="name"
+                label="Course name"
+                placeholder="Title"
+                subLabel={errorMsg('name') || 'Use Title Case'}
+                value={state.name}
+              />
+              <Dropdown
+                error={isError('level')}
+                label="Course difficulty level"
+                placeholder="Choose one"
+                subLabel={errorMsg('level')}
+                menu={
+                  <ActionMenu>
+                    {['beginner', 'intermediate', 'advanced'].map(level => (
+                      <ActionMenu.Item
+                        name="level"
+                        onClick={_ => this.setState({ level })}
+                      >
+                        {stringUtil.capitalize(level)}
+                      </ActionMenu.Item>
+                    ))}
+                  </ActionMenu>
+                }
+              />
+              <Checkbox
+                error={isError('slides')}
+                checked={state.slides}
+                name="slides"
+                label="Has slides?"
+                onCheck={(_, checked, __, name) =>
+                  this.setState({ [name]: checked })
+                }
+              />
+              <Form.Divider />
+              <Radio.Group
+                error={isError('slidestech')}
+                disabled={!state.slides}
+                name="slidestech"
+                onSelect={(_, slidestech) => this.setState({ slidestech })}
+                value={state.slidestech}
+              >
+                <Radio.Button value="key" label="Keynote" />
+                <Radio.Button value="pptx" label="Powerpoint" />
+              </Radio.Group>
+              <Form.Divider />
+              <TextArea
+                error={isError('desc')}
+                label="Description"
+                subLabel={errorMsg('desc')}
+                placeholder="What is your course about?"
+                onChange={this.handleChange}
+                name="desc"
+                value={state.desc}
+              />
+              <Form.ButtonRow>
+                <Button
+                  loading={state.isSubmitting}
+                  onClick={this.handleSubmit}
+                >
+                  Save
+                </Button>
+                <Button
+                  onClick={_ => {
+                    if (confirm('Confirm reset?')) this.reset()
+                  }}
+                  appearance={Button.appearances.flat}
+                >
+                  Cancel
+                </Button>
+              </Form.ButtonRow>
+            </Form.VerticalLayout>
+          </form>
+        </Theme>
+        <style jsx>{`
+          .example {
+            position: relative;
+            background: ${core.colors.gray04};
+            padding: ${core.layout.spacingLarge} ${core.layout.spacingXLarge};
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
 export default withServerProps(_ => (
   <Chrome>
     <Content title="Form">
@@ -137,6 +330,13 @@ export default withServerProps(_ => (
       <Code language="javascript">
         {`import Form from '@pluralsight/ps-design-system-form/react'`}
       </Code>
+
+      <SectionHeading>In-app example</SectionHeading>
+      <P>
+        Here's a fully functioning form that does everything short of submitting
+        to a server.
+      </P>
+      <InAppExample />
 
       <SectionHeading>Form controls</SectionHeading>
       <P>
