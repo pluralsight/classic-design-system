6c7cbf2a697a474fc8fe74a82eb64f812b9df2da
Dane Thurber <dane.thurber@gmail.com>
2018-10-19 12:16:54 -0600

    test(button): add storybook decorator to center story
    it was difficult to view the entire focus state of the <Button /> so i added a new decorator to
    center the stories. I also moved the theme decorator to be configured globally to DRY up the stories
    a little

---
+++ packages/button/.storybook/center-decorator.js
@@ -0,0 +1,34 @@
+// TODO: move to separate package
+
+import React from 'react'
+import { css } from 'glamor'
+
+const styles = {
+  outer: css({
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    width: '100vw',
+    overflow: 'auto'
+  }),
+  inner: css({
+    margin: 'auto',
+    maxHeight: '100%' // IE Hack
+  })
+}
+
+const Center = props => (
+  <div {...styles.outer}>
+    <div {...styles.inner}>{props.children}</div>
+  </div>
+)
+
+const centerDecorator = storyFn => <Center>{storyFn()}</Center>
+
+export default centerDecorator
--- packages/button/src/react/__specs__/storyshots.spec.js
+++ packages/button/src/react/__specs__/storyshots.spec.js
@@ -1,3 +1,11 @@
+import React from 'react'
 import initStoryshots from '@storybook/addon-storyshots'
+
+jest.mock('../../../.storybook/center-decorator.js', () => {
+  const CenterDecorator = props => (
+    <div data-storybook-center-decorator {...props} />
+  )
+  return CenterDecorator
+})
 
 initStoryshots()
