1db59251eabef5b2abdc10c924ff69741b7c98cf
Dane Thurber <dane.thurber@gmail.com>
2018-10-24 16:04:34 -0600

    refactor(switch): use <Halo /> comp

--- packages/switch/src/react/index.js
+++ packages/switch/src/react/index.js
@@ -1,35 +1,12 @@
-import core from '@pluralsight/ps-design-system-core'
 import * as glamor from 'glamor'
 import PropTypes from 'prop-types'
 import React from 'react'
-import {
-  names as themeNames,
-  defaultName as themeDefaultName
-} from '@pluralsight/ps-design-system-theme/react'
-import { transparentize } from 'polished'
+
+import { defaultName as themeDefaultName } from '@pluralsight/ps-design-system-theme/react'
+import Halo from '@pluralsight/ps-design-system-halo/react'
 
 import css from '../css'
 import * as vars from '../vars'
-
-const styleFocusRingGap = ({ size, themeName }) => ({
-  ...css['.psds-switch__track:focus:before, .psds-switch__track--error:before'],
-  ...css[
-    `.psds-switch__track--size-${size}:focus:before, .psds-switch__track--error.psds-switch__track--size-${size}:before`
-  ],
-  ...css[
-    `.psds-switch__track.psds-theme--${themeName}:focus:before, .psds-switch__track--error.psds-theme--${themeName}:before`
-  ]
-})
-
-const styleFocusRingBorder = ({ size, themeName }) => ({
-  ...css['.psds-switch__track:focus:after, .psds-switch__track--error:after'],
-  ...css[
-    `.psds-switch__track--size-${size}:focus:after, .psds-switch__track--error.psds-switch__track--size-${size}:after`
-  ],
-  ...css[
-    `.psds-switch__track.psds-theme--${themeName}:focus:after, .psds-switch__track--error.psds-theme--${themeName}:after`
-  ]
-})
 
 const styles = {
   switch: ({ disabled, labelAlign }) =>
@@ -46,22 +23,7 @@ const styles = {
       css['.psds-switch__track'],
       checked &&
         css[`.psds-switch__track--checked.psds-switch__track--color-${color}`],
-      css[`.psds-switch__track.psds-switch__track--size-${size}`],
-      error && {
-        ':before': styleFocusRingGap({ size, themeName }),
-        ':after': {
-          ...styleFocusRingBorder({ size, themeName }),
-          ...css['.psds-switch__track--error:after']
-        }
-      },
-      isFocused &&
-        !disabled && {
-          ':before': styleFocusRingGap({ size, themeName }),
-          ':after': {
-            ...styleFocusRingBorder({ size, themeName }),
-            ...css['.psds-switch__track:focus:after']
-          }
-        }
+      css[`.psds-switch__track.psds-switch__track--size-${size}`]
     ),
   thumb: ({ checked, size }) =>
     glamor.css({
@@ -71,69 +33,84 @@ const styles = {
         ? css[`.psds-switch__thumb--checked.psds-switch__thumb--size-${size}`]
         : null)
     }),
-  label: ({ labelAlign, size, themeName }) =>
-    glamor.css({
+  label: ({ labelAlign, size, themeName }) => {
+    return glamor.css({
       ...css['.psds-switch__label'],
       ...css[`.psds-switch__label--size-${size}`],
       ...css[
         `.psds-switch__label--size-${size}.psds-switch__label--labelAlign-${labelAlign}`
       ],
       ...css[`.psds-switch__label.psds-theme--${themeName}`]
-    }),
+    })
+  },
   checkbox: _ => glamor.css(css['.psds-switch__checkbox'])
 }
 
 class Switch extends React.Component {
-  constructor() {
-    super()
-    this.state = { isFocused: false }
+  constructor (props) {
+    super(props)
+
     this.handleFocus = this.handleFocus.bind(this)
     this.handleBlur = this.handleBlur.bind(this)
+
+    this.state = { isFocused: false }
   }
-  handleFocus() {
-    this.setState({ isFocused: true })
+
+  handleFocus () {
+    this.setState(_ => ({ isFocused: true }))
   }
-  handleBlur() {
-    this.setState({ isFocused: false })
+
+  handleBlur () {
+    this.setState(_ => ({ isFocused: false }))
   }
-  render() {
-    const { context, props, state } = this
-    const allProps = {
-      ...props,
-      isFocused: state.isFocused,
-      themeName: context.themeName || themeDefaultName
-    }
+
+  render () {
+    const { isFocused } = this.state
+    const { children, error } = this.props
+    const themeName = this.context.themeName || themeDefaultName
+    const allProps = { ...this.props, themeName }
+
     const switchProps = {
       ...styles.switch(allProps),
-      ...(props.onClick && !props.disabled
-        ? { onClick: _ => props.onClick(!allProps.checked) }
+      ...(allProps.onClick && !allProps.disabled
+        ? { onClick: _ => allProps.onClick(!allProps.checked) }
         : null),
-      ...(props.style ? { style: props.style } : null),
-      ...(props.className ? { className: props.className } : null)
+      ...(allProps.style ? { style: allProps.style } : null),
+      ...(allProps.className ? { className: allProps.className } : null)
     }
 
     return (
       <button
         {...switchProps}
-        tabIndex={allProps.tabIndex || '0'}
-        role="checkbox"
         aria-checked={allProps.checked}
-        onFocus={this.handleFocus}
         onBlur={this.handleBlur}
+        onFocus={this.handleFocus}
+        role='checkbox'
+        tabIndex={allProps.tabIndex || '0'}
       >
-        <div {...styles.track(allProps)}>
-          <div {...styles.thumb(allProps)} />
-        </div>
-        <input
-          tabIndex="-1"
-          type="checkbox"
-          readOnly
-          checked={allProps.checked}
-          {...styles.checkbox(allProps)}
-        />
-        {allProps.children && (
-          <label {...styles.label(allProps)}>{allProps.children}</label>
-        )}
+        <Halo
+          appearance={
+            !isFocused && error
+              ? Halo.appearances.error
+              : Halo.appearances.default
+          }
+          shape={Halo.shapes.pill}
+          visible={error || isFocused}
+        >
+          <div {...styles.track(allProps)}>
+            <div {...styles.thumb(allProps)} />
+          </div>
+
+          <input
+            tabIndex='-1'
+            type='checkbox'
+            readOnly
+            checked={allProps.checked}
+            {...styles.checkbox(allProps)}
+          />
+        </Halo>
+
+        {children && <label {...styles.label(allProps)}>{children}</label>}
       </button>
     )
   }
