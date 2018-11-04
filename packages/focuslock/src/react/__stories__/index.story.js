272764c12b5673a7542c4ad84e70be4bff526dbe
Dane Thurber <dane.thurber@gmail.com>
2018-11-04 14:09:02 -0700

    test(focuslock): cleanup story

--- packages/focuslock/src/react/__stories__/index.story.js
+++ packages/focuslock/src/react/__stories__/index.story.js
@@ -1,28 +1,20 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 
+import Text from '@pluralsight/ps-design-system-text/react'
+
 import FocusLock from '..'
 
-const Story = () => {
-  return (
-    <div style={{ color: 'white', textAlign: 'center' }}>
-      <FocusLock>
-        <p>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-          <a href="#">a link</a>
-        </p>
-      </FocusLock>
-    </div>
-  )
-}
-storiesOf('FocusLock', module).add('default', _ => <Story />)
+storiesOf('FocusLock', module).add('default', _ => (
+  <FocusLock style={{ color: 'white', textAlign: 'center', margin: 100 }}>
+    <Text.P>
+      <a href="#">Tri-tip pork chop jowl</a>, spare ribs shankle sirloin kevin.
+      Ham hock tri-tip jowl, <a href="#">shank venison ham</a> andouille rump
+      salami swine sausage turducken beef ribs cupim jerky. Chuck swine shankle
+      short loin tri-tip beef ribs leberkas andouille spare ribs shoulder
+      buffalo ground round tongue <a href="#">sirloin bresaola</a>. Kielbasa
+      rump buffalo shoulder chicken beef brisket. Spare ribs drumstick tail
+      cupim bacon filet mignon.
+    </Text.P>
+  </FocusLock>
+))
