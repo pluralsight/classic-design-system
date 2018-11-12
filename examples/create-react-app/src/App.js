6403f86e4251725b98e4171afe528139c679c101
jaketrent <trent.jake@gmail.com>
2018-11-12 11:14:22 -0700

    refactor(examples): create-react-app to use ds css and js

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,5 +1,8 @@
+// DS: import and used react components
+import Button from '@pluralsight/ps-design-system-button/react'
+import Icon from '@pluralsight/ps-design-system-icon/react'
+import Text from '@pluralsight/ps-design-system-text/react'
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +10,17 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
+          <Icon size={Icon.sizes.large} id={Icon.ids.code} className="App-logo" />
+          <Text.P>
+            Edit <Text.Code>src/App.js</Text.Code> and save to reload.
+          </Text.P>
+          <Button
+            href="https://design-system.pluralsight.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
-          </a>
+            Learn the Design System
+          </Button>
         </header>
       </div>
     );
