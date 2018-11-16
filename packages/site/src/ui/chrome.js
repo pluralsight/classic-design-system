50f3438d11188c274a1fef63b052aa3863213f84
Dane Thurber <dane.thurber@gmail.com>
2018-11-16 10:43:25 -0700

    style(site): format/link Chrome

--- packages/site/src/ui/chrome.js
+++ packages/site/src/ui/chrome.js
@@ -1,30 +1,44 @@
 import core from '@pluralsight/ps-design-system-core'
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import { Head, GlobalStyles, MobileMenuBar, SideNav } from './index'
 
-export default class extends React.Component {
+class Chrome extends React.Component {
   constructor(props) {
     super(props)
+
+    this.closeSideNav = this.closeSideNav.bind(this)
+    this.openSideNav = this.openSideNav.bind(this)
+
     this.state = { isSideNavOpen: false }
   }
+
+  closeSideNav() {
+    this.setState(() => ({ isSideNavOpen: false }))
+  }
+
+  openSideNav() {
+    this.setState(() => ({ isSideNavOpen: true }))
+  }
+
   render() {
     return (
       <div>
         <Head />
         <GlobalStyles />
-        <MobileMenuBar
-          onBurgerClick={_ => this.setState({ isSideNavOpen: true })}
-        />
+        <MobileMenuBar onBurgerClick={this.openSideNav} />
+
         <div className="page">
           <div className="side">
             <SideNav
               isOpen={this.state.isSideNavOpen}
-              onCloseClick={_ => this.setState({ isSideNavOpen: false })}
+              onCloseClick={this.closeSideNav}
             />
           </div>
           <div className="main">{this.props.children}</div>
         </div>
+
         <style jsx>{`
           .main {
             background: ${core.colors.white};
@@ -44,3 +58,9 @@ export default class extends React.Component {
     )
   }
 }
+
+Chrome.propTypes = {
+  children: PropTypes.node
+}
+
+export default Chrome
