fb68de0155862c90772e774f36a0e20a6b33d3dd
Dane Thurber <dane.thurber@gmail.com>
2018-11-30 13:09:39 -0700

    feat(avatar): allow alt prop. better screen reader support
    BREAKING CHANGE: if no alt attr is provided the image will be marked as aria-hidden
    
    fixes: #365

--- packages/avatar/src/react/__specs__/index.spec.js
+++ packages/avatar/src/react/__specs__/index.spec.js
@@ -30,4 +30,51 @@ describe('Avatar', () => {
       expect(wrapper.prop(propName)).toBeDefined()
     })
   })
+
+  describe('with an image src and an alt attr', () => {
+    let image, wrapper
+
+    beforeEach(() => {
+      wrapper = mount(<Avatar alt="alt text" src="//something.png" />)
+      image = wrapper.find('img')
+    })
+
+    it('should passes the alt attr to the image', () => {
+      expect(image.prop('alt')).toBeDefined()
+    })
+
+    it('should be visible to screen readers', () => {
+      expect(wrapper.childAt(0).prop('aria-hidden')).toBeFalsy()
+    })
+  })
+
+  describe('with an image src and NO alt attr', () => {
+    it('should be hidden from screen readers', () => {
+      const wrapper = mount(<Avatar src="//something.png" />)
+      expect(wrapper.childAt(0).prop('aria-hidden')).toBeTruthy()
+    })
+  })
+
+  describe('WITHOUT an image src', () => {
+    const name = 'Benedict Cumberbatch'
+
+    let div, wrapper
+
+    beforeEach(() => {
+      wrapper = mount(<Avatar name={name} />)
+      div = wrapper.find('div > div')
+    })
+
+    it('displays initial', () => {
+      expect(div.text()).toEqual('BC')
+    })
+
+    it('add the name as an aria-label', () => {
+      expect(div.prop('aria-label')).toEqual(name)
+    })
+
+    it('should NOT be hidden from screen readers', () => {
+      expect(wrapper.childAt(0).prop('aria-hidden')).toBeFalsy()
+    })
+  })
 })
