b8f3566d5cc96ef74bc4b6742f96fe4c0f692ac2
jaketrent <trent.jake@gmail.com>
2018-12-07 16:24:54 -0700

    refactor(tab): remove glamorous; add react filter

--- packages/tab/src/react/list-item.js
+++ packages/tab/src/react/list-item.js
@@ -1,93 +1,92 @@
-import core from '@pluralsight/ps-design-system-core'
+import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
 import * as glamor from 'glamor'
-import glamorous from 'glamorous'
 import PropTypes from 'prop-types'
 import React from 'react'
-import {
-  defaultName as themeDefaultName,
-  names as themeNames
-} from '@pluralsight/ps-design-system-theme/react'
+import { defaultName as themeDefaultName } from '@pluralsight/ps-design-system-theme/react'
 
 import css from '../css'
 
-const TextWidth = glamorous.div(css['.psds-tab__list-item__text'])
+const styles = {
+  bar: _ => glamor.css(css['.psds-tab__list-item__bar']),
+  listItem: props =>
+    glamor.css(
+      css['.psds-tab__list-item'],
+      {
+        ':focus': css['.psds-tab__list-item:focus'],
+        ':first-child': css['.psds-tab__list-item:first-child'],
 
-const Bar = glamorous.span(css['.psds-tab__list-item__bar'])
-
-const ListItem = glamorous.button(
-  {
-    ...css['.psds-tab__list-item'],
-    ':focus': css['.psds-tab__list-item:focus'],
-    ':first-child': css['.psds-tab__list-item:first-child']
-  },
-  props => ({
-    // TextWidth
-    ':hover div':
-      css[
-        `.psds-tab__list-item.psds-theme--${
-          props.themeName
-        }:hover .psds-tab__list-item__text`
-      ],
-    ':focus div':
-      css[
-        `.psds-tab__list-item.psds-theme--${
-          props.themeName
-        }:focus .psds-tab__list-item__text`
-      ],
-    ':active div':
-      css[
-        `.psds-tab__list-item.psds-theme--${
-          props.themeName
-        }:active .psds-tab__list-item__text`
-      ],
-    // Bar
-    ':hover span': css[`.psds-tab__list-item:hover .psds-tab__list-item__bar`],
-    ':focus span': css[`.psds-tab__list-item:focus .psds-tab__list-item__bar`],
-    ':active span': props.active
-      ? css[
-          `.psds-tab__list-item.psds-tab__list-item--active:active .psds-tab__list-item__bar`
-        ]
-      : css[`.psds-tab__list-item:active .psds-tab__list-item__bar`]
-  }),
-  ({ themeName }) => css[`.psds-tab__list-item.psds-theme--${themeName}`],
-  props =>
-    props.active
-      ? {
-          // TextWidth
-          ':hover div':
-            css[
-              `.psds-tab__list-item.psds-tab__list-item--active.psds-theme--${
-                props.themeName
-              }:hover .psds-tab__list-item__text`
-            ],
-          ':focus div':
-            css[
-              `.psds-tab__list-item.psds-tab__list-item--active.psds-theme--${
-                props.themeName
-              }:focus .psds-tab__list-item__text`
-            ],
-          '& div':
-            css[
-              `.psds-tab__list-item.psds-tab__list-item--active.psds-theme--${
-                props.themeName
-              } .psds-tab__list-item__text`
-            ],
-          // Bar
-          ':hover span':
-            css[
-              `.psds-tab__list-item.psds-tab__list-item--active:hover .psds-tab__list-item__bar`
-            ],
-          ':focus span':
-            css[
-              `.psds-tab__list-item.psds-tab__list-item--active:focus .psds-tab__list-item__bar`
-            ],
-          '& span':
-            css[
-              `.psds-tab__list-item.psds-tab__list-item--active .psds-tab__list-item__bar`
+        // TextWidth
+        ':hover div':
+          css[
+            `.psds-tab__list-item.psds-theme--${
+              props.themeName
+            }:hover .psds-tab__list-item__text`
+          ],
+        ':focus div':
+          css[
+            `.psds-tab__list-item.psds-theme--${
+              props.themeName
+            }:focus .psds-tab__list-item__text`
+          ],
+        ':active div':
+          css[
+            `.psds-tab__list-item.psds-theme--${
+              props.themeName
+            }:active .psds-tab__list-item__text`
+          ],
+        // Bar
+        ':hover span':
+          css[`.psds-tab__list-item:hover .psds-tab__list-item__bar`],
+        ':focus span':
+          css[`.psds-tab__list-item:focus .psds-tab__list-item__bar`],
+        ':active span': props.active
+          ? css[
+              `.psds-tab__list-item.psds-tab__list-item--active:active .psds-tab__list-item__bar`
             ]
-        }
-      : null
-)
+          : css[`.psds-tab__list-item:active .psds-tab__list-item__bar`]
+      },
+      css[`.psds-tab__list-item.psds-theme--${props.themeName}`],
+      props.active && {
+        // TextWidth
+        ':hover div':
+          css[
+            `.psds-tab__list-item.psds-tab__list-item--active.psds-theme--${
+              props.themeName
+            }:hover .psds-tab__list-item__text`
+          ],
+        ':focus div':
+          css[
+            `.psds-tab__list-item.psds-tab__list-item--active.psds-theme--${
+              props.themeName
+            }:focus .psds-tab__list-item__text`
+          ],
+        '& div':
+          css[
+            `.psds-tab__list-item.psds-tab__list-item--active.psds-theme--${
+              props.themeName
+            } .psds-tab__list-item__text`
+          ],
+        // Bar
+        ':hover span':
+          css[
+            `.psds-tab__list-item.psds-tab__list-item--active:hover .psds-tab__list-item__bar`
+          ],
+        ':focus span':
+          css[
+            `.psds-tab__list-item.psds-tab__list-item--active:focus .psds-tab__list-item__bar`
+          ],
+        '& span':
+          css[
+            `.psds-tab__list-item.psds-tab__list-item--active .psds-tab__list-item__bar`
+          ]
+      }
+    ),
+  textWidth: _ => glamor.css(css['.psds-tab__list-item__text'])
+}
+
+const ListItem = props => {
+  return <button {...filterReactProps(props)} {...styles.listItem(props)} />
+}
 
 const ListItemComponent = (props, context) => {
   const listItemProps = {
@@ -104,10 +103,10 @@ const ListItemComponent = (props, context) => {
   }
   return (
     <ListItem {...listItemProps}>
-      <TextWidth>
+      <div {...styles.textWidth(props)}>
         {props.children}
-        <Bar />
-      </TextWidth>
+        <span {...styles.bar(props)} />
+      </div>
     </ListItem>
   )
 }
