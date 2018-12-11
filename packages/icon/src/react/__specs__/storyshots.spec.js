f7d4cb5b5c9b75f9a8420c68155c17b79e765a9e
Dane Thurber <dane.thurber@gmail.com>
2018-12-11 09:53:57 -0700

    test(icon): mock theme decorator

--- packages/icon/src/react/__specs__/storyshots.spec.js
+++ packages/icon/src/react/__specs__/storyshots.spec.js
@@ -1,5 +1,6 @@
 import initStoryshots from '@storybook/addon-storyshots'
 
 jest.mock('@pluralsight/ps-design-system-storybook-addon-center')
+jest.mock('@pluralsight/ps-design-system-storybook-addon-theme')
 
 initStoryshots()
