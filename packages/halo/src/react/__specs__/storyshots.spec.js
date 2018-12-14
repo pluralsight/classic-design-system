b607f0af574024f05bda006e74ffeb7b38dc60df
Dane Thurber <dane.thurber@gmail.com>
2018-12-14 12:21:45 -0700

    test(halo): migrate to shared test config

--- packages/halo/src/react/__specs__/storyshots.spec.js
+++ packages/halo/src/react/__specs__/storyshots.spec.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import React from 'react'
 import initStoryshots from '@storybook/addon-storyshots'
 
@@ -5,4 +6,6 @@ jest.mock('@pluralsight/ps-design-system-storybook-addon-center')
 
 jest.mock('../../../stories/focusable', () => props => <div {...props} />)
 
-initStoryshots()
+initStoryshots({
+  configPath: path.resolve(__dirname, '..', '..', '..', '.storybook')
+})
