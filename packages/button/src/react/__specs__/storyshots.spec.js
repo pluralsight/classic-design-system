becc30922f0e0ef8c00eee7f75736a3ad4914e83
Dane Thurber <dane.thurber@gmail.com>
2018-12-14 12:18:20 -0700

    test(button): migrate to shared test config

--- packages/button/src/react/__specs__/storyshots.spec.js
+++ packages/button/src/react/__specs__/storyshots.spec.js
@@ -1,5 +1,8 @@
+import path from 'path'
 import initStoryshots from '@storybook/addon-storyshots'
 
 jest.mock('@pluralsight/ps-design-system-storybook-addon-center')
 
-initStoryshots()
+initStoryshots({
+  configPath: path.resolve(__dirname, '..', '..', '..', '.storybook')
+})
