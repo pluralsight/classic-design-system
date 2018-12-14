2a5299e77525388f1a2dc0ef16e39c4abc873b22
Dane Thurber <dane.thurber@gmail.com>
2018-12-14 12:29:57 -0700

    chore: update create-component template with new test config

--- scripts/create-component/package-template/src/react/__specs__/storyshots.spec.js
+++ scripts/create-component/package-template/src/react/__specs__/storyshots.spec.js
@@ -1,6 +1,14 @@
-import initStoryshots from '@storybook/addon-storyshots'
+import path from 'path'
+import initStoryshots, {
+  snapshotWithOptions
+} from '@storybook/addon-storyshots'
 
 jest.mock('@pluralsight/ps-design-system-storybook-addon-center')
 jest.mock('@pluralsight/ps-design-system-storybook-addon-theme')
 
-initStoryshots()
+const createNodeMock = el => document.createElement('div')
+
+initStoryshots({
+  configPath: path.resolve(__dirname, '..', '..', '..', '.storybook'),
+  test: snapshotWithOptions({ createNodeMock })
+})
