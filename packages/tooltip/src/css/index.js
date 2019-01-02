8b4991f200c906747a64e76a60cc6d5aaf9bc50e
jaketrent <trent.jake@gmail.com>
2019-01-02 15:58:33 -0700

    refactor(tooltip): support light theme and fix tail shadow bleed
    Fixes #396, #397
    
    BREAKING CHANGE put tail styles in __tail css element selector

--- packages/tooltip/src/css/index.js
+++ packages/tooltip/src/css/index.js
@@ -4,6 +4,11 @@ import { transparentize } from 'polished'
 import * as vars from '../vars'
 
 const tailDimension = '14px'
+const diamondHeight = getDiamondHeight(tailDimension, tailDimension) + 'px'
+const tailTransformTop = `translate(calc(((${diamondHeight} - ${tailDimension})) / 2), calc(50% + (${diamondHeight} - ${tailDimension}))) rotate(45deg)`
+const tailTransformBottom = `translate(calc(((${diamondHeight} - ${tailDimension})) / 2), -50%) rotate(45deg)`
+
+const boxShadow = `0px 1px 4px ${transparentize(0.5, core.colors.black)}`
 
 export default {
   [`@keyframes psds-tooltip__keyframes__fade`]: {
@@ -18,7 +23,7 @@ export default {
     maxWidth: '320px',
     padding: `${core.layout.spacingXSmall} ${core.layout.spacingMedium}`,
     borderRadius: '2px',
-    boxShadow: `0 1px 2px ${transparentize(0.5, core.colors.black)}`,
+    boxShadow,
     fontSize: core.type.fontSizeSmall,
     lineHeight: core.type.lineHeightTight,
     fontWeight: core.type.fontWeightMedium,
@@ -36,51 +41,87 @@ export default {
     color: core.colors.white,
     backgroundColor: core.colors.blue
   },
-  // --tailPosition
-  [`.psds-tooltip:after`]: {
+
+  [`.psds-tooltip__tail`]: {
     position: 'absolute',
+    height: diamondHeight,
+    width: diamondHeight,
+    overflow: 'hidden'
+  },
+  [`.psds-tooltip__tail:after`]: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
     display: 'block',
     content: ' ',
     whiteSpace: 'pre',
     height: tailDimension,
     width: tailDimension,
     lineHeight: '0',
-    transform: 'rotate(45deg)'
+    transform: 'rotate(45deg)',
+    boxShadow
   },
-  [`.psds-tooltip--appearance-${vars.appearances.basic}:after`]: {
+  [`.psds-tooltip__tail--appearance-${vars.appearances.basic}:after`]: {
     backgroundColor: core.colors.white
   },
-  [`.psds-tooltip--appearance-${vars.appearances.accent}:after`]: {
+  [`.psds-tooltip__tail--appearance-${vars.appearances.accent}:after`]: {
     backgroundColor: core.colors.blue
   },
-  [`.psds-tooltip--tailPosition-${vars.tailPositions.topLeft}:after`]: {
-    top: `calc(${tailDimension} / -2)`,
+  [`.psds-tooltip__tail--tailPosition-${vars.tailPositions.topLeft}`]: {
+    top: `calc(${diamondHeight} * -1)`,
     left: tailDimension
   },
-  [`.psds-tooltip--tailPosition-${vars.tailPositions.topCenter}:after`]: {
-    top: `calc(${tailDimension} / -2)`,
-    left: '50%',
-    transform: 'translateX(-50%) rotate(45deg)'
+  [`.psds-tooltip__tail--tailPosition-${vars.tailPositions.topLeft}:after`]: {
+    transform: tailTransformTop,
+    boxShadow
   },
-  [`.psds-tooltip--tailPosition-${vars.tailPositions.topRight}:after`]: {
-    top: `calc(${tailDimension} / -2)`,
+  [`.psds-tooltip__tail--tailPosition-${vars.tailPositions.topCenter}`]: {
+    top: `calc(${diamondHeight} * -1)`,
+    left: '50%',
+    transform: 'translateX(-50%)'
+  },
+  [`.psds-tooltip__tail--tailPosition-${vars.tailPositions.topCenter}:after`]: {
+    transform: tailTransformTop,
+    boxShadow
+  },
+  [`.psds-tooltip__tail--tailPosition-${vars.tailPositions.topRight}`]: {
+    top: `calc(${diamondHeight} * -1)`,
     right: tailDimension
   },
-  [`.psds-tooltip--tailPosition-${vars.tailPositions.bottomLeft}:after`]: {
-    bottom: `calc(${tailDimension} / -2)`,
-    left: tailDimension,
-    boxShadow: `1px 1px 1px ${transparentize(0.5, core.colors.black)}`
+  [`.psds-tooltip__tail--tailPosition-${vars.tailPositions.topRight}:after`]: {
+    transform: tailTransformTop,
+    boxShadow
   },
-  [`.psds-tooltip--tailPosition-${vars.tailPositions.bottomCenter}:after`]: {
-    bottom: `calc(${tailDimension} / -2)`,
+  [`.psds-tooltip__tail--tailPosition-${vars.tailPositions.bottomLeft}`]: {
+    bottom: `calc(${diamondHeight} * -1)`,
+    left: tailDimension
+  },
+  [`.psds-tooltip__tail--tailPosition-${
+    vars.tailPositions.bottomLeft
+  }:after`]: {
+    transform: tailTransformBottom,
+    boxShadow
+  },
+  [`.psds-tooltip__tail--tailPosition-${vars.tailPositions.bottomCenter}`]: {
+    bottom: `calc(${diamondHeight} * -1)`,
     left: '50%',
-    transform: 'translateX(-50%) rotate(45deg)',
-    boxShadow: `1px 1px 1px ${transparentize(0.5, core.colors.black)}`
+    transform: 'translateX(-50%)'
   },
-  [`.psds-tooltip--tailPosition-${vars.tailPositions.bottomRight}:after`]: {
-    bottom: `calc(${tailDimension} / -2)`,
-    right: tailDimension,
-    boxShadow: `1px 1px 1px ${transparentize(0.5, core.colors.black)}`
+  [`.psds-tooltip__tail--tailPosition-${
+    vars.tailPositions.bottomCenter
+  }:after`]: {
+    transform: tailTransformBottom,
+    boxShadow
+  },
+  [`.psds-tooltip__tail--tailPosition-${vars.tailPositions.bottomRight}`]: {
+    bottom: `calc(${diamondHeight} * -1)`,
+    right: tailDimension
+  },
+  [`.psds-tooltip__tail--tailPosition-${
+    vars.tailPositions.bottomRight
+  }:after`]: {
+    transform: tailTransformBottom,
+    boxShadow
   },
   // --closeable
   [`.psds-tooltip--closeable`]: {
@@ -110,4 +151,15 @@ export default {
   [`.psds-tooltip__close--appearance-${vars.appearances.accent} > svg`]: {
     fill: core.colors.bone
   }
+}
+
+function getDiamondHeight(height, width) {
+  height = parseInt(height, 10)
+  width = parseInt(width, 10)
+  return (
+    Math.sqrt(
+      Math.pow(height, 2) -
+        Math.pow(Math.sqrt(Math.pow(height, 2) + Math.pow(width, 2)) / 2, 2)
+    ) * 2
+  )
 }
