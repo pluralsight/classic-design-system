058290a9942f582a5eb65ef391649f926f15d382
jaketrent <trent.jake@gmail.com>
2019-01-14 15:46:22 -0700

    feat(util): add color#transparentize

--- packages/util/index.js
+++ packages/util/index.js
@@ -1,5 +1,11 @@
+const color = require('./color')
 const propDefs = require('./prop-defs')
 const props = require('./props')
 const string = require('./string')
 
-module.exports = { propDefs: propDefs, props: props, string: string }
+module.exports = {
+  color: color,
+  propDefs: propDefs,
+  props: props,
+  string: string
+}
