59467066a453ad92f7c7b842272cb7258a6d0484
Dane Thurber <dane.thurber@gmail.com>
2019-02-27 10:51:53 -0700

    feat(theme): forward refs
    use React.forwardRefs to ensure child components have refs
    
    BREAKING CHANGE: bump to react 16.3 to use forwardrefs api

--- packages/theme/src/react/with-theme.js
+++ packages/theme/src/react/with-theme.js
@@ -12,17 +12,29 @@ const getDisplayName = Component => {
   return Component.displayName || Component.name || 'Component'
 }
 
-export default function withTheme(BaseComponent) {
+export default function withTheme(BaseComponent, options = {}) {
+  const { forwardRef = true } = options
   const name = getDisplayName(BaseComponent)
 
-  const EnhancedComponent = (props, context = {}) => {
+  const EnhancedComponent = ({ forwardedRef, ...props }, context) => {
     const themeName = context.themeName || themeDefaultName
-    return <BaseComponent {...props} themeName={themeName} />
+    return <BaseComponent {...props} ref={forwardedRef} themeName={themeName} />
+  }
+
+  EnhancedComponent.contextTypes = { themeName: PropTypes.string }
+  EnhancedComponent.propTypes = { forwardedRef: PropTypes.any }
+
+  if (forwardRef) {
+    const Forwarded = React.forwardRef(function forwardThemeRef(props, ref) {
+      return <EnhancedComponent {...props} forwardedRef={ref} />
+    })
+    Forwarded.BaseComponent = BaseComponent
+    Forwarded.displayName = `withTheme(${name})`
+
+    return hoistNonReactStatics(Forwarded, BaseComponent)
   }
 
   EnhancedComponent.BaseComponent = BaseComponent
-  EnhancedComponent.contextTypes = { themeName: PropTypes.string }
   EnhancedComponent.displayName = `withTheme(${name})`
-
   return hoistNonReactStatics(EnhancedComponent, BaseComponent)
 }
