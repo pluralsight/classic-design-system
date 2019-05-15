8b405e8bb6e2901e8a52cbab24ed2c7fe6a0f50f
jaketrent <trent.jake@gmail.com>
2019-05-15 15:30:59 -0600

    feat(position): add react component

--- packages/position/src/react/__stories__/index.story.js
+++ packages/position/src/react/__stories__/index.story.js
@@ -1,66 +1,68 @@
 import core from '@pluralsight/ps-design-system-core'
-import { storiesOf } from '@storybook/react'
+import PropTypes from 'prop-types'
 import React from 'react'
+import { storiesOf } from '@storybook/react'
 import Tooltip from '@pluralsight/ps-design-system-tooltip/react'
 
+import { Above, Below, LeftOf, RightOf } from '../index.js'
 import * as position from '../../js/index.js'
 
+const Box = React.forwardRef((props, ref) => (
+  <div
+    ref={ref}
+    style={{
+      height: '200px',
+      width: '200px',
+      border: `4px dashed ${core.colors.pink}`,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: core.colors.pink,
+      fontSize: core.type.fontSizeMedium,
+      fontWeight: core.type.fontWeightBold,
+      ...props.style
+    }}
+  >
+    {props.children}
+  </div>
+))
+Box.propTypes = {
+  children: PropTypes.node,
+  style: PropTypes.object
+}
+
 // NOTE: old style refs to make storybook/storyshots happy
-class StyleStory2 extends React.Component {
+class JsStory extends React.Component {
   constructor() {
     super()
     this.state = { style: {} }
+    this.tooltipRef = React.createRef()
   }
   componentDidMount() {
     const { props } = this
-    if (this.boxEl && this.tooltipEl)
+    if (this.boxEl && this.tooltipRef.current)
       this.setState({
         styles: position[props.styleFunction](this.boxEl).styleFor(
-          this.tooltipEl
+          this.tooltipRef.current
         )
       })
   }
   render() {
     const { props } = this
     const tailPositions = {
-      styleAbove: Tooltip.tailPositions.bottomCenter,
-      styleRightOf: null,
-      styleBelow: Tooltip.tailPositions.topCenter,
-      styleLeftOf: null
-    }
-
-    class Box extends React.Component {
-      render() {
-        const { props } = this
-        return (
-          <div
-            ref={props.innerRef}
-            style={{
-              height: '200px',
-              width: '200px',
-              border: `4px dashed ${core.colors.pink}`,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              color: core.colors.pink,
-              fontSize: core.type.fontSizeMedium,
-              fontWeight: core.type.fontWeightBold,
-              ...props.style
-            }}
-          >
-            {props.children}
-          </div>
-        )
-      }
+      above: Tooltip.tailPositions.bottomCenter,
+      rightOf: null,
+      below: Tooltip.tailPositions.topCenter,
+      leftOf: null
     }
 
     return (
       <div>
-        <Box innerRef={el => (this.boxEl = el)}>Below</Box>
+        <Box ref={el => (this.boxEl = el)}>{props.styleFunction}</Box>
         <Tooltip
-          innerRef={el => (this.tooltipEl = el)}
-          style={this.state.styles}
-          tailPositio={tailPositions[props.styleFunction]}
+          ref={this.tooltipRef}
+          style={{ position: 'absolute', ...this.state.styles }}
+          tailPosition={tailPositions[props.styleFunction]}
         >
           The tip
         </Tooltip>
@@ -69,9 +71,18 @@ class StyleStory2 extends React.Component {
   }
 }
 
-const styleStory = storiesOf('Position', module)
+const jsStory = storiesOf('js', module)
 ;['above', 'rightOf', 'below', 'leftOf'].forEach(styleFunction =>
-  styleStory.add(styleFunction, _ => {
-    return <StyleStory2 styleFunction={styleFunction} />
+  jsStory.add(styleFunction, _ => {
+    return <JsStory styleFunction={styleFunction} />
   })
+)
+
+const reactStory = storiesOf('react', module)
+;[Above, RightOf, Below, LeftOf].forEach(Position =>
+  reactStory.add(Position.displayName, () => (
+    <Position show={<Tooltip>The tip</Tooltip>}>
+      <Box>{'<' + Position.displayName + '/>'}</Box>
+    </Position>
+  ))
 )
--- packages/position/src/react/index.js
+++ packages/position/src/react/index.js
@@ -1,10 +1,59 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
-import css from '../css'
-import * as vars from '../vars'
+import { above, below, leftOf, rightOf } from '../js/index.js'
 
-const Position = props => <div>Position</div>
+export function Above(props) {
+  return <Position position={above} {...props} />
+}
+Above.displayName = 'Above'
 
-Position.propTypes = {}
+export function Below(props) {
+  return <Position position={below} {...props} />
+}
+Below.displayName = 'Below'
 
-export default Position
+export function RightOf(props) {
+  return <Position position={rightOf} {...props} />
+}
+RightOf.displayName = 'RightOf'
+
+export function LeftOf(props) {
+  return <Position position={leftOf} {...props} />
+}
+LeftOf.displayName = 'LeftOf'
+
+function Position(props) {
+  const targetRef = React.useRef()
+  const elRef = React.useRef()
+  const child = React.Children.only(props.children)
+  const [style, setStyle] = React.useState({ position: 'absolute' })
+
+  React.useLayoutEffect(
+    () => {
+      if (targetRef.current && elRef.current)
+        setStyle(props.position(targetRef.current).styleFor(elRef.current))
+    },
+    [props]
+  )
+
+  return (
+    <React.Fragment>
+      {React.cloneElement(child, {
+        ref: targetRef
+      })}
+      {React.cloneElement(props.show, {
+        ref: elRef,
+        style: {
+          ...child.props.style,
+          ...style
+        }
+      })}
+    </React.Fragment>
+  )
+}
+Position.propTypes = {
+  children: PropTypes.element,
+  position: PropTypes.func,
+  show: PropTypes.element
+}
