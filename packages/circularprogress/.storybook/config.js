e60bbc8c6972a288b0863c495962fdc8dfb05a5a
Dane Thurber <dane.thurber@gmail.com>
2019-05-24 09:56:29 -0600

    test(circularprogress): center stories; mock decorators in snapshots

--- packages/circularprogress/.storybook/config.js
+++ packages/circularprogress/.storybook/config.js
@@ -1,8 +1,10 @@
 import addons from '@storybook/addons'
 import { addDecorator, configure } from '@storybook/react'
 
+import centerDecorator from '@pluralsight/ps-design-system-storybook-addon-center'
 import themeDecorator from '@pluralsight/ps-design-system-storybook-addon-theme'
 
+addDecorator(centerDecorator)
 addDecorator(themeDecorator(addons))
 
 function loadStory() {
