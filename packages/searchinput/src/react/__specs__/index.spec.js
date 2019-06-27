4b881ead122eb32dbcc45eed4124bec23714467e
jaketrent <trent.jake@gmail.com>
2019-06-27 16:46:34 -0600

    feat(site): add label props to searchinput doc

--- packages/searchinput/src/react/__specs__/index.spec.js
+++ packages/searchinput/src/react/__specs__/index.spec.js
@@ -4,16 +4,6 @@ import { fireEvent, render } from 'react-testing-library'
 import SearchInput from '../index.js'
 
 describe('SearchInput', () => {
-  it('ignores the label prop', async () => {
-    const { container } = render(<SearchInput label="unsupported" />)
-    expect(container).not.toHaveTextContent(/unsupported/)
-  })
-
-  it('ignores the subLabel prop', async () => {
-    const { container } = render(<SearchInput subLabel="unsupported" />)
-    expect(container).not.toHaveTextContent(/unsupported/)
-  })
-
   it('focuses input when clear button clicked', () => {
     const noop = () => {}
     const { container } = render(<SearchInput onClear={noop} />)
