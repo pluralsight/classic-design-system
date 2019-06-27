295b603540c2d8824a30e95a63a1bccf707a1c97
Brian Sweeting <brian-sweeting@pluralsight.com>
2019-06-27 10:14:14 -0600

    docs(datawell): updated data well docs
    Updated examples and added some guidelines.

--- packages/site/pages/components/datawell.js
+++ packages/site/pages/components/datawell.js
@@ -11,45 +11,46 @@ import {
   Content,
   Example,
   P,
+  Guideline,
   PageHeading,
   PropTypes,
   SectionHeading,
   withServerProps
 } from '../../src/ui/index.js'
 
-function InAppExample(props) {
-  return (
-    <Theme>
-      <div className="example">
-        <div style={{ display: 'flex' }}>
-          <DataWell label="Active learners">7.2 Billion</DataWell>
-          <DataWell label="Pluralsight ROI">Vast</DataWell>
-          <DataWell label="Lives changed" subLabel="Up to 11">
-            11/10
-          </DataWell>
-        </div>
-      </div>
+// function InAppExample(props) {
+//   return (
+//     <Theme>
+//       <div className="example">
+//         <div style={{ display: 'flex' }}>
+//           <DataWell label="Active learners">7.2 Billion</DataWell>
+//           <DataWell label="Pluralsight ROI">Vast</DataWell>
+//           <DataWell label="Lives changed" subLabel="Up to 11">
+//             11/10
+//           </DataWell>
+//         </div>
+//       </div>
 
-      <Code
-        collapsible
-        language="javascript"
-      >{`<div style={{ display: 'flex' }}>
-  <DataWell label="Active learners">7.2 Billion</DataWell>
-  <DataWell label="Pluralsight ROI">Vast</DataWell>
-  <DataWell label="Lives changed" subLabel="Up to 11">11/10</DataWell>
-</div>`}</Code>
+//       <Code
+//         collapsible
+//         language="javascript"
+//       >{`<div style={{ display: 'flex' }}>
+//   <DataWell label="Active learners">7.2 Billion</DataWell>
+//   <DataWell label="Pluralsight ROI">Vast</DataWell>
+//   <DataWell label="Lives changed" subLabel="Up to 11">11/10</DataWell>
+// </div>`}</Code>
 
-      <style jsx>{`
-        .example {
-          padding: ${core.layout.spacingLarge};
-          background: ${core.colors.gray06};
-          color: ${core.colors.white};
-          min-height: 200px;
-        }
-      `}</style>
-    </Theme>
-  )
-}
+//       <style jsx>{`
+//         .example {
+//           padding: ${core.layout.spacingLarge};
+//           background: ${core.colors.gray06};
+//           color: ${core.colors.white};
+//           min-height: 200px;
+//         }
+//       `}</style>
+//     </Theme>
+//   )
+// }
 
 export default withServerProps(_ => (
   <Chrome>
@@ -92,30 +93,66 @@ export default withServerProps(_ => (
         ]}
       />
 
-      <SectionHeading>In-app example</SectionHeading>
+      <SectionHeading>Example</SectionHeading>
       <P>
         Often Data Wells are shown in a row. Data Well is designed to fit inside
-        this experience. Simply wrap them in a flex container.
+        this experience. Simply wrap them in a flex container. Labels should
+        provide context into the main meaning of the data shown.
       </P>
-      <InAppExample />
-
-      <SectionHeading>Labels</SectionHeading>
       <P>
-        Labels should provide context into the main meaning of the data shown.
         <Text.Code>label</Text.Code> is always required.{' '}
-        <Text.Code>subLabel</Text.Code> is optional
+        <Text.Code>subLabel</Text.Code> is optional.
       </P>
       <Example.React
         themeToggle
         includes={{ DataWell }}
         codes={[
-          `<DataWell 
-  label="Design System efficiency multiplier" 
-  subLabel="Rough estimate"
->
-  ~123.11451
-</DataWell>`
+          `<div style={{ display: 'flex' }}>
+  <DataWell label="Active learners">7.2 Billion</DataWell>
+  <DataWell label="Pluralsight ROI">Vast</DataWell>
+  <DataWell label="Lives changed" subLabel="Up to 11">
+    11/10
+  </DataWell>
+</div>`
         ]}
+      />
+
+      <SectionHeading>Guidelines</SectionHeading>
+      <P>Keep labels, sublabels, and values short as possible.</P>
+      <Guideline
+        do={
+          <div
+            style={{
+              background: core.colors.gray04,
+              borderRadius: '4px',
+              padding: core.layout.spacingLarge
+            }}
+          >
+            <Theme>
+              <DataWell label="Royalty Payments" subLabel="Last 30 days">
+                $123M
+              </DataWell>
+            </Theme>
+          </div>
+        }
+        dont={
+          <div
+            style={{
+              background: core.colors.gray04,
+              borderRadius: '4px',
+              padding: core.layout.spacingLarge
+            }}
+          >
+            <Theme>
+              <DataWell
+                label="Royalties Paid to Authors"
+                subLabel="For the period of the last thirty days"
+              >
+                $123,456,789,123.03
+              </DataWell>
+            </Theme>
+          </div>
+        }
       />
     </Content>
   </Chrome>
