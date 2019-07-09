e9c8ac0804e6a294ba1e22fb1a4cbb53cec9c673
jaketrent <trent.jake@gmail.com>
2019-07-09 10:04:05 -0600

    refactor(avatar): convert to function component

--- packages/avatar/src/react/index.js
+++ packages/avatar/src/react/index.js
@@ -17,62 +17,54 @@ const styles = {
   initials: ({ name }) => css(stylesheet['.psds-avatar__initials'])
 }
 
-class Avatar extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { imageState: 'loading' }
-    this.handleImageLoadSuccess = this.handleImageLoadSuccess.bind(this)
-    this.handleImageLoadError = this.handleImageLoadError.bind(this)
+const Avatar = props => {
+  const [imageState, setImageState] = React.useState('loading')
+
+  function handleImageLoadSuccess(evt) {
+    const isFallbackPixel = evt.target.naturalWidth === 1
+    setImageState(isFallbackPixel ? 'error' : 'success')
   }
 
-  handleImageLoadSuccess(event) {
-    const isFallbackPixel = event.target.naturalWidth === 1
-    this.setState({ imageState: isFallbackPixel ? 'error' : 'success' })
+  function handleImageLoadError() {
+    setImageState('error')
   }
 
-  handleImageLoadError() {
-    this.setState({ imageState: 'error' })
+  const { alt, name, size, src } = props
+
+  const shouldShowImg = src && imageState !== 'error'
+  const shouldShowInitials =
+    name && (imageState === 'error' || imageState === 'loading')
+
+  const hideFromScreenReaders = shouldShowImg && !alt
+
+  const avatarProps = {
+    ...filterReactProps(props),
+    ...styles.avatar({ size }),
+    ...(hideFromScreenReaders ? { 'aria-hidden': true } : null)
   }
 
-  render() {
-    const { alt, name, size, src } = this.props
-    const { imageState } = this.state
-
-    const shouldShowImg = src && imageState !== 'error'
-    const shouldShowInitials =
-      name && (imageState === 'error' || imageState === 'loading')
-
-    const hideFromScreenReaders = shouldShowImg && !alt
-
-    const avatarProps = {
-      ...filterReactProps(this.props),
-      ...styles.avatar({ size }),
-      ...(hideFromScreenReaders ? { 'aria-hidden': true } : null)
-    }
-
-    return (
-      <div {...avatarProps}>
-        {shouldShowImg && (
-          <img
-            alt={alt}
-            onError={this.handleImageLoadError}
-            onLoad={this.handleImageLoadSuccess}
-            {...styles.image()}
-            src={transformSrc(src)}
-          />
-        )}
-        {shouldShowInitials && (
-          <div
-            {...styles.initials({ name })}
-            aria-label={name}
-            style={{ backgroundColor: getColorByName(name) }}
-          >
-            {getInitials(name)}
-          </div>
-        )}
-      </div>
-    )
-  }
+  return (
+    <div {...avatarProps}>
+      {shouldShowImg && (
+        <img
+          alt={alt}
+          onError={handleImageLoadError}
+          onLoad={handleImageLoadSuccess}
+          {...styles.image()}
+          src={transformSrc(src)}
+        />
+      )}
+      {shouldShowInitials && (
+        <div
+          {...styles.initials({ name })}
+          aria-label={name}
+          style={{ backgroundColor: getColorByName(name) }}
+        >
+          {getInitials(name)}
+        </div>
+      )}
+    </div>
+  )
 }
 
 Avatar.defaultProps = {
