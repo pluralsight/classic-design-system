1a27a8d27fd21a68b1c404c17861f126f6eeac7f
Dane Thurber <dane.thurber@gmail.com>
2019-07-19 13:06:40 -0600

    refactor(table): update to use new babel7 and related deps

--- packages/table/.storybook/addons.js
+++ packages/table/.storybook/addons.js
@@ -1,6 +1,1 @@
-import '@storybook/addon-actions/register'
-
-import addons from '@storybook/addons'
 import register from '@pluralsight/ps-design-system-storybook-addon-theme/register'
-
-register(addons)
