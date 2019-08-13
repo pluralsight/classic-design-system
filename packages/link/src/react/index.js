ce30cf905e4e2bc457fee8829ddecca08369ea1e
jaketrent <trent.jake@gmail.com>
2019-08-13 15:07:16 -0600

    feat(link): enable React.forwardRef
    Update style.  Attempt to make compatible with next@9 for site package
    
    BREAKING CHANGE enable React.forwardRef

--- packages/link/src/react/index.js
+++ packages/link/src/react/index.js
@@ -1,31 +1,39 @@
-import * as glamor from 'glamor'
+import { css } from 'glamor'
+import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import css from '../css'
-import * as vars from '../vars'
+import stylesheet from '../css/index.js'
+import * as vars from '../vars/index.js'
 
 const style = props =>
-  glamor.css({
-    ':hover': css['.psds-link:hover'],
-    ...(({ appearance }) => css[`.psds-link--appearance-${appearance}`])(props)
+  css(
+    {
+      ':hover': stylesheet['.psds-link:hover']
+    },
+    stylesheet[`.psds-link--appearance-${props.appearance}`]
+  )
+
+const Link = React.forwardRef((props, forwardedRef) => {
+  const ref = forwardedRef || React.useRef()
+
+  let tagName = 'a'
+  React.useEffect(_ => {
+    if (ref.current && ref.current.tagName) tagName = ref.current.tagName
   })
 
-const rmNonHtmlProps = props => {
-  const { appearance, children, ...rest } = props
-  return rest
-}
-
-const Link = props =>
-  React.cloneElement(React.Children.only(props.children), {
+  const { children, ...rest } = props
+  return React.cloneElement(React.Children.only(props.children), {
     ...style(props),
-    ...rmNonHtmlProps(props)
+    ...filterReactProps(rest, { tagName })
   })
+})
 
 Link.appearances = vars.appearances
 
 Link.propTypes = {
-  appearances: PropTypes.oneOf(Object.keys(vars.appearances))
+  appearance: PropTypes.oneOf(Object.keys(vars.appearances)),
+  children: PropTypes.element.isRequired
 }
 Link.defaultProps = {
   appearance: vars.appearances.default
