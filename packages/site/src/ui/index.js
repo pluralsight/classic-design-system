55069d0a109ff5e6b073dfbccf6c00bd7c9c002a
jaketrent <trent.jake@gmail.com>
2019-08-13 10:30:38 -0600

    refactor(site): upgrade to nextjs@9

--- packages/site/src/ui/index.js
+++ packages/site/src/ui/index.js
@@ -1,25 +1,51 @@
-export ChangeLog from './change-log'
-export Chrome from './chrome'
-export Code from './code'
-export Content from './content'
-export Doc from './doc'
-export Example from './example'
-export GithubIcon from './github-icon'
-export GlobalStyles from './global-styles'
-export Guideline from './guideline'
-export Head from './head'
-export Heading from './heading'
-export Intro from './intro'
-export Link from './link'
-export MobileMenuBar from './mobile-menu-bar'
-export OpenIssuePrompt from './open-issue-prompt'
-export P from './p'
-export PageHeading from './page-heading'
-export PropTypes from './prop-types'
-export SectionHeading from './section-heading'
-export SideNav from './side-nav'
-export TextLink from './text-link'
-export ThemeToggle from './theme-toggle'
-export TitleLogo from './title-logo'
-export TopBar from './top-bar'
-export withServerProps from './with-server-props'
+import AChangeLog from './change-log.js'
+import AChrome from './chrome.js'
+import ACode from './code.js'
+import AContent from './content.js'
+import ADoc from './doc.js'
+import AExample from './example/index.js'
+import AGithubIcon from './github-icon.js'
+import AGlobalStyles from './global-styles.js'
+import AGuideline from './guideline.js'
+import AHead from './head.js'
+import AHeading from './heading.js'
+import AIntro from './intro.js'
+import ALink from './link.js'
+import AMobileMenuBar from './mobile-menu-bar.js'
+import AOpenIssuePrompt from './open-issue-prompt.js'
+import AP from './p.js'
+import APageHeading from './page-heading.js'
+import APropTypes from './prop-types.js'
+import ASectionHeading from './section-heading.js'
+import ASideNav from './side-nav.js'
+import ATextLink from './text-link.js'
+import AThemeToggle from './theme-toggle.js'
+import ATitleLogo from './title-logo.js'
+import ATopBar from './top-bar.js'
+import AwithServerProps from './with-server-props.js'
+
+export const ChangeLog = AChangeLog
+export const Chrome = AChrome
+export const Code = ACode
+export const Content = AContent
+export const Doc = ADoc
+export const Example = AExample
+export const GithubIcon = AGithubIcon
+export const GlobalStyles = AGlobalStyles
+export const Guideline = AGuideline
+export const Head = AHead
+export const Heading = AHeading
+export const Intro = AIntro
+export const Link = ALink
+export const MobileMenuBar = AMobileMenuBar
+export const OpenIssuePrompt = AOpenIssuePrompt
+export const P = AP
+export const PageHeading = APageHeading
+export const PropTypes = APropTypes
+export const SectionHeading = ASectionHeading
+export const SideNav = ASideNav
+export const TextLink = ATextLink
+export const ThemeToggle = AThemeToggle
+export const TitleLogo = ATitleLogo
+export const TopBar = ATopBar
+export const withServerProps = AwithServerProps
