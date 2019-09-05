64b46ffd460d508ab83dd2aaab0b04f24e695898
Dane Thurber <dane.thurber@gmail.com>
2019-09-05 12:12:55 -0600

    test: update deps

--- jest/setup/index.js
+++ jest/setup/index.js
@@ -1,7 +1,6 @@
 import 'raf/polyfill.js'
 import 'jest-prop-type-error'
 
-import '@testing-library/react/cleanup-after-each.js'
 import '@testing-library/jest-dom/extend-expect.js'
 
 import './match-media.js'
