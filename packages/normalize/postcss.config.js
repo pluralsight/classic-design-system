e93f5230f2508d2cc30a98180ec28863794aabf5
Dane Thurber <dane.thurber@gmail.com>
2019-09-23 15:45:15 -0600

    fix(button): fix css output that should not contain nested selectors

--- packages/normalize/postcss.config.js
+++ packages/normalize/postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-preset-env')({
-      browsers: 'last 2 versions, IE <= 10',
       stage: '2',
       preserve: true,
       features: { 'nesting-rules': true }
