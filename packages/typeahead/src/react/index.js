e1312db58a24a383efb1f2396a976dc0c067f1f4
Dane Thurber <dane.thurber@gmail.com>
2019-10-07 16:17:42 -0600

    feat(typeahead): remove ActionMenu and implement custom menu components

--- packages/typeahead/src/react/index.js
+++ packages/typeahead/src/react/index.js
@@ -1,8 +1,6 @@
 import React, { Children } from 'react'
 import PropTypes from 'prop-types'
 
-import ActionMenu from '@pluralsight/ps-design-system-actionmenu/react.js'
-import core from '@pluralsight/ps-design-system-core'
 import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
 import Icon from '@pluralsight/ps-design-system-icon/react.js'
 import { BelowLeft } from '@pluralsight/ps-design-system-position/react.js'
@@ -11,6 +9,7 @@ import TextInput from '@pluralsight/ps-design-system-textinput/react.js'
 
 import * as vars from '../vars/index.js'
 
+import SuggestionsMenu from './menu.js'
 import useOnDocumentClick from './use-on-document-click.js'
 import usePortal from './use-portal.js'
 import { combineFns, omit, pick } from './utils.js'
@@ -133,60 +132,6 @@ const Typeahead = React.forwardRef((props, forwardedRef) => {
     </div>
   )
 })
-
-function CheckIcon({ visible, ...props }) {
-  const style = { color: visible ? core.colors.blue : 'transparent' }
-  return <Icon id={Icon.ids.check} style={style} />
-}
-CheckIcon.propTypes = {
-  visible: PropTypes.bool.isRequired
-}
-
-const SuggestionsMenu = React.forwardRef((props, forwardedRef) => {
-  const { activeValue, suggestions, ...rest } = props
-
-  const items = React.useMemo(() => {
-    if (suggestions.length <= 0)
-      return (
-        <ActionMenu.Item disabled value="">
-          no results found
-        </ActionMenu.Item>
-      )
-
-    return suggestions.map(sug => (
-      <ActionMenu.Item
-        key={sug.index}
-        icon={<CheckIcon visible={sug.value === activeValue} />}
-        value={sug.value}
-      >
-        {sug.label}
-      </ActionMenu.Item>
-    ))
-  }, [activeValue, suggestions])
-
-  return (
-    <ActionMenu
-      {...rest}
-      ref={forwardedRef}
-      origin={ActionMenu.origins.topLeft}
-      shouldFocusOnMount={false}
-    >
-      {items}
-    </ActionMenu>
-  )
-})
-
-SuggestionsMenu.propTypes = {
-  activeValue: PropTypes.string.isRequired,
-  onChange: PropTypes.func,
-  suggestions: PropTypes.arrayOf(
-    PropTypes.shape({
-      index: PropTypes.number.isRequired,
-      label: PropTypes.string.isRequired,
-      value: PropTypes.string
-    })
-  ).isRequired
-}
 
 const Suggestion = React.forwardRef((props, forwardedRef) => {
   return <div ref={forwardedRef} {...props} />
