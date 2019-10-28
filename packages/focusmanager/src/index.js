b7024dbcc4a5e66ad74a433dd553f2dc00903279
Edward Irby <edward-irby@pluralsight.com>
2019-10-28 04:20:04 -0700

    refactor(focusmanager): fix aggregating modules syntax
    update cjs pahs

--- packages/focusmanager/src/index.js
+++ packages/focusmanager/src/index.js
@@ -1,6 +1,1 @@
-import FocusManager from './react/index.js'
-import * as vars from './vars/index.js'
-import css from './css/index.js'
-
-export default FocusManager
-export { vars, css }
+export { default } from './react/index.js'
