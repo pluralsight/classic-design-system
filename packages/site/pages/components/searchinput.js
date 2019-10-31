82f18601bea0e5576fd435e46bf5d91baf5800e8
Edward Irby <edward-irby@pluralsight.com>
2019-10-31 10:40:33 -0700

    refactor(searchinput): esm export refactor
    BREAKING CHANGE: import pattern

--- packages/site/pages/components/searchinput.js
+++ packages/site/pages/components/searchinput.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import core from '@pluralsight/ps-design-system-core'
-import SearchInput from '@pluralsight/ps-design-system-searchinput/react.js'
+import SearchInput from '@pluralsight/ps-design-system-searchinput'
 import Text from '@pluralsight/ps-design-system-text/react.js'
 import Theme from '@pluralsight/ps-design-system-theme/react.js'
 
@@ -29,8 +29,7 @@ export default _ => (
 
       <P>Include a React component in your project:</P>
       <Code language="javascript">
-        import SearchInput from
-        '@pluralsight/ps-design-system-searchinput/react'
+        import SearchInput from '@pluralsight/ps-design-system-searchinput'
       </Code>
 
       <PropTypes
