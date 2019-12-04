bbc86bf940f27c17a8a7322e7dd31af1b00e9fcb
Dane Thurber <dane.thurber@gmail.com>
2019-12-04 16:06:55 -0700

    refactor(row): update to use new glamor coding styles

--- packages/row/src/react/conditional-wrap.js
+++ packages/row/src/react/conditional-wrap.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const ConditionalWrap = ({ shouldWrap, children, wrapper }) =>
-  shouldWrap ? wrapper(children) : <React.Fragment>{children}</React.Fragment>
+  shouldWrap ? wrapper(children) : <>{children}</>
 
 ConditionalWrap.propTypes = {
   shouldWrap: PropTypes.bool.isRequired,
