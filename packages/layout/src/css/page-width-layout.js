71485e76ba5156dd24f822b0169c54aadbaddf66
jaketrent <trent.jake@gmail.com>
2019-12-16 16:36:29 -0700

    refactor(layout): use named export instead of star import

--- packages/layout/src/css/page-width-layout.js
+++ packages/layout/src/css/page-width-layout.js
@@ -1,17 +1,17 @@
-import * as core from '@pluralsight/ps-design-system-core'
+import { layout } from '@pluralsight/ps-design-system-core'
 
 export default {
   '.psds-page-width-layout': {
     marginLeft: 'auto',
     marginRight: 'auto',
     maxWidth: '1600px',
-    paddingLeft: core.layout.spacingLarge,
-    paddingRight: core.layout.spacingLarge
+    paddingLeft: layout.spacingLarge,
+    paddingRight: layout.spacingLarge
   },
   '@media (min-width: 769px)': {
     '.psds-page-width-layout': {
-      paddingLeft: core.layout.spacingXLarge,
-      paddingRight: core.layout.spacingXLarge
+      paddingLeft: layout.spacingXLarge,
+      paddingRight: layout.spacingXLarge
     }
   }
 }
