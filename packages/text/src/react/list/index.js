dc06e655a3757e6a3ed9d565b7d297f3c857a8a6
Edward Irby <edward-irby@pluralsight.com>
2020-01-12 17:16:10 -0800

    feat(text): confirmed colors with Brian

--- packages/text/src/react/list/index.js
+++ packages/text/src/react/list/index.js
@@ -3,27 +3,33 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 import { useTheme } from '@pluralsight/ps-design-system-theme'
+import { useFeatureFlags } from '@pluralsight/ps-design-system-featureflags'
 import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
 
 import stylesheet from '../../css/index.js'
 import * as vars from '../../vars/index.js'
 
 const styles = {
-  list: (themeName, props) =>
-    compose(
-      css(stylesheet[`.psds-text__list.psds-theme--${themeName}`]),
+  list: props => {
+    const flag = props.psds2020Colors ? '.psds-button--psds2020Colors' : ''
+    return compose(
+      css(stylesheet[`.psds-text__list.psds-theme--${props.themeName}${flag}`]),
       css(stylesheet[`.psds-text__list`]),
       css(stylesheet[`.psds-text__list--type-${props.type}`])
     )
+  }
 }
 
 export default function List({ type, ...props }) {
   const themeName = useTheme()
+  const {
+    flags: { psds2020Colors }
+  } = useFeatureFlags()
   const TagName = type === 'numbered' ? 'ol' : 'ul'
 
   return (
     <TagName
-      {...styles.list(themeName, { type })}
+      {...styles.list({ themeName, type, psds2020Colors })}
       {...filterReactProps(props, { tagName: TagName })}
     />
   )
