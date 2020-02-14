5a2b7fc05c1be6e0baa61ccc050e0c721d64ebea
Edward Irby <edward-irby@pluralsight.com>
2020-02-14 11:58:58 -0800

    feat(text): removes featureFlags consumer and old styles

--- packages/text/src/react/code.js
+++ packages/text/src/react/code.js
@@ -2,25 +2,19 @@ import { compose, css } from 'glamor'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { useTheme } from '@pluralsight/ps-design-system-theme'
-import { useFeatureFlags } from '@pluralsight/ps-design-system-featureflags'
 
 import stylesheet from '../css/index.js'
 
-const style = ({ themeName, psds2020Colors }) => {
-  const flag = psds2020Colors ? '.psds-text--2020-colors' : ''
-  return compose(
+const style = ({ themeName }) =>
+  compose(
     css(stylesheet[`.psds-text__code`]),
-    css(stylesheet[`.psds-text__code.psds-theme--${themeName}${flag}`])
+    css(stylesheet[`.psds-text__code.psds-theme--${themeName}`])
   )
-}
 const Code = props => {
   const themeName = useTheme()
-  const {
-    flags: { psds2020Colors }
-  } = useFeatureFlags()
 
   return props.children ? (
-    <code {...props} {...style({ ...props, themeName, psds2020Colors })}>
+    <code {...props} {...style({ ...props, themeName })}>
       {props.children}
     </code>
   ) : null
