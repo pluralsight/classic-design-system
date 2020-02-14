92c677a0d5cc8f35b98f514ecd29517b04e8a1b7
Edward Irby <edward-irby@pluralsight.com>
2020-02-14 12:12:24 -0800

    feat(link): removes featureFlags consumer and old styles

--- packages/link/src/react/index.js
+++ packages/link/src/react/index.js
@@ -1,6 +1,5 @@
 import { css } from 'glamor'
 import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
-import { useFeatureFlags } from '@pluralsight/ps-design-system-featureflags'
 import { useTheme } from '@pluralsight/ps-design-system-theme'
 import { appearances } from '../vars/index.js'
 
@@ -9,23 +8,18 @@ import React from 'react'
 
 import stylesheet from '../css/index.js'
 
-const style = ({ psds2020Colors, appearance, themeName }) => {
-  const flag = psds2020Colors ? '.psds-link--2020-colors' : ''
-  return css(
-    stylesheet[`.psds-link${flag}`],
+const style = ({ appearance, themeName }) =>
+  css(
+    stylesheet[`.psds-link`],
     appearance === appearances.default
       ? stylesheet[
-          `.psds-link--appearance-${appearance}.psds-theme--${themeName}${flag}`
+          `.psds-link--appearance-${appearance}.psds-theme--${themeName}`
         ]
       : stylesheet[`.psds-link--appearance-${appearance}`]
   )
-}
 
 const Link = React.forwardRef((props, forwardedRef) => {
   const ref = forwardedRef || React.useRef()
-  const {
-    flags: { psds2020Colors }
-  } = useFeatureFlags()
   const themeName = useTheme()
 
   let tagName = 'a'
@@ -35,7 +29,7 @@ const Link = React.forwardRef((props, forwardedRef) => {
 
   const { children, ...rest } = props
   return React.cloneElement(React.Children.only(props.children), {
-    ...style({ ...props, psds2020Colors, themeName }),
+    ...style({ ...props, themeName }),
     ...filterReactProps(rest, { tagName })
   })
 })
