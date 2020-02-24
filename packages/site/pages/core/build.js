4b472144aa012c37ed343236a14f6530705a9f08
Edward Irby <edward-irby@pluralsight.com>
2020-02-24 12:13:46 -0800

    feat(site): update build docs with SSR and peer dep setup info

--- packages/site/pages/core/build.js
+++ packages/site/pages/core/build.js
@@ -28,7 +28,7 @@ module.exports = {
 If you use Webpack for loading CSS, you'll also need to install your loaders:
 
 \`\`\`bash
-npm install style-loader css-loader postcss-loader
+npm install style-loader css-loader postcss-loader cssnano
 \`\`\`
 
 And add a \`module.rule\` to your \`webpack.config.js\`:
@@ -71,6 +71,77 @@ module: {
       use: ['style-loader', 'css-loader', 'sass-loader']
     }
   ]
+}
+\`\`\`
+
+## Next.js Config
+
+To avoid flashes of unstyled content when doing SSR with next.js the following is necessary:
+
+Add \`glamor/server\` to the \`_document.js\`:
+
+\`\`\`bash
+npm install glamor
+\`\`\`
+
+\`\`\`js
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { renderStatic } from 'glamor/server'
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const page = ctx.renderPage()
+    const styles = renderStatic(() => page.html || page.errorHtml)
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps, ...page, ...styles }
+  }
+  constructor(props) {
+    super(props)
+    const { __NEXT_DATA__, ids } = props
+    if (ids) {
+      __NEXT_DATA__.ids = this.props.ids
+    }
+  }
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head>
+          <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+export default MyDocument
+\`\`\`
+
+In \`next.config.js\` directly adjust postCSS configuration like so:
+\`\`\`bash
+npm install @pluralsight/ps-design-system-core
+\`\`\`
+*Note css variables consumed by app need to be added to importFrom field.
+
+\`\`\`js
+const postcssPresetEnv = require('postcss-preset-env')
+module.exports = {
+  plugins: [
+    require('postcss-import')(),
+    postcssPresetEnv({
+      stage: 2,
+      features: {
+        'nesting-rules': true,
+      },
+      browsers: ['Last 2 versions', 'IE >= 11'],
+      importFrom: [
+        './node_modules/@pluralsight/ps-design-system-core/css/colors.module.css',
+        './node_modules/@pluralsight/ps-design-system-core/css/type.module.css',
+        './node_modules/@pluralsight/ps-design-system-core/css/colors-2020.module.css'    
+      ],
+    }),
+  ],
 }
 \`\`\`
 
