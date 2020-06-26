6f591f8b86a297f01f95a5639e70f4efc224de98
Dane Thurber <dane.thurber@gmail.com>
2020-06-26 17:01:15 -0600

    feat(docs): setup gatsby apollo theme

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,10 +1,85 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
+
+require('dotenv').config({ path: `.env.${activeEnv}` })
 
 module.exports = {
-  /* Your site config here */
-  plugins: []
+  siteMetadata: {
+    description: 'description',
+    siteName: 'Pluralsight Design System',
+    subtitle: 'Subtitle',
+    title: 'Pluralsight Design System',
+    twitterHandle: ''
+  },
+  pathPrefix: '',
+  plugins: [
+    {
+      resolve: 'gatsby-theme-apollo-docs',
+      options: {
+        algoliaApiKey: process.env.ALGOLIA_API_KEY,
+        algoliaIndexName: process.env.ALGOLIA_INDEX_NAME,
+        baseUrl: 'https://design-system.pluralsight.com',
+        contentDir: 'src',
+        description: 'description',
+        githubRepo: 'pluralsight/design-system',
+        logoLink: 'https://design-system.pluralsight.com',
+        menuTitle: 'Pluralsight Design System',
+        root: __dirname,
+        siteName: 'Pluralsight Design System',
+
+        footerNavConfig: {
+          Contribute: {
+            href: '/guides/contributing'
+          }
+        },
+
+        navConfig: {
+          Github: {
+            url: 'https://github.com/pluralsight/design-system',
+            description: ''
+          }
+        },
+
+        sidebarCategories: {
+          null: ['index'],
+          'Getting Started': ['getting-started/install'],
+          Components: ['components/emptystate']
+        }
+      }
+    },
+
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/source/images`
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-sharp',
+      options: {}
+    },
+    {
+      resolve: 'gatsby-transformer-sharp',
+      options: {}
+    },
+
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'psds-docs',
+        short_name: 'psds',
+        start_url: '/',
+        background_color: '#0E2339',
+        theme_color: '#0E2339',
+        display: 'minimal-ui',
+        icon: 'source/images/icon.png'
+      }
+    },
+
+    {
+      resolve: 'gatsby-plugin-react-axe',
+      options: { showInProduction: false }
+    }
+  ]
 }
---
+++ docs/src/gatsby-theme-apollo-core/components/logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Wrapper = styled.div({
+  display: 'flex'
+})
+
+export default function Logo() {
+  return <Wrapper />
+}
