981497857f00a9214b9ae181e5c30a4076220a19
jaketrent <trent.jake@gmail.com>
2020-06-29 18:10:19 -0600

    refactor(navbrand): props and styling spacing

--- packages/navbrand/src/css/index.js
+++ packages/navbrand/src/css/index.js
@@ -1,5 +1,30 @@
-import * as vars from '../vars/index.js'
+import { layout } from '@pluralsight/ps-design-system-core'
 
 export default {
-  '.psds-navbrand': {}
+  '.psds-navbrand': {
+    display: 'flex',
+    alignItems: 'center',
+    padding: layout.spacingXSmall
+  },
+  '.psds-navbrand__logo': {
+    height: '24px',
+    width: '24px',
+
+    '& svg': {
+      height: '100%',
+      width: '100%'
+    }
+  },
+  '.psds-navbrand__wordmark': {
+    display: 'none',
+    lineHeight: 0
+  },
+  '@media (min-width: 1200px)': {
+    '.psds-navbrand__logo--wordmark': {
+      marginRight: layout.spacingXSmall
+    },
+    '.psds-navbrand__wordmark': {
+      display: 'block'
+    }
+  }
 }
--- packages/navbrand/src/react/index.js
+++ packages/navbrand/src/react/index.js
@@ -1,32 +1,76 @@
-import { css } from 'glamor'
+import { css, media } from 'glamor'
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { useTheme } from '@pluralsight/ps-design-system-theme'
 import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
 
 import stylesheet from '../css/index.js'
 
 const styles = {
-  navbrand: (themeName, props) => css(stylesheet['.psds-navbrand'])
+  logo: props =>
+    css(
+      stylesheet['.psds-navbrand__logo'],
+      props.wordmark &&
+        media(
+          '(min-width: 1200px)',
+          stylesheet['@media (min-width: 1200px)'][
+            '.psds-navbrand__logo--wordmark'
+          ]
+        )
+    ),
+  navBrand: () => css(stylesheet['.psds-navbrand']),
+  wordmark: () =>
+    css(
+      stylesheet['.psds-navbrand__wordmark'],
+      media(
+        '(min-width: 1200px)',
+        stylesheet['@media (min-width: 1200px)']['.psds-navbrand__wordmark']
+      )
+    )
 }
 
-const Navbrand = React.forwardRef((props, forwardedRef) => {
-  const themeName = useTheme()
-
+const NavBrand = React.forwardRef((props, forwardedRef) => {
   const ref = React.useRef()
   React.useImperativeHandle(forwardedRef, () => ref.current)
 
-  return (
-    <div
-      ref={ref}
-      {...styles.navbrand(themeName, props)}
-      {...filterReactProps(props)}
-    />
+  const tagName = props.href ? 'a' : props.onClick ? 'button' : 'div'
+
+  return React.createElement(
+    tagName,
+    {
+      ...styles.navBrand(),
+      ...filterReactProps(props, { tagName }),
+      ref
+    },
+    <>
+      <Logo {...props} />
+      <Wordmark {...props} />
+    </>
   )
 })
 
+NavBrand.displayName = 'NavBrand'
+NavBrand.propTypes = {
+  href: PropTypes.string,
+  logo: PropTypes.node,
+  onClick: PropTypes.func,
+  wordmark: PropTypes.node
+}
 
-Navbrand.propTypes = {}
+export default NavBrand
 
-export default Navbrand
+function Logo(props) {
+  return <div {...styles.logo(props)}>{props.logo}</div>
+}
+Logo.displayName = 'NavBrand.Logo'
+Logo.propTypes = {
+  logo: PropTypes.node
+}
+
+function Wordmark(props) {
+  return <div {...styles.wordmark()}>{props.wordmark}</div>
+}
+Wordmark.displayName = 'NavBrand.Wordmark'
+Wordmark.propTypes = {
+  wordmark: PropTypes.node
+}
