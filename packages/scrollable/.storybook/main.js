7bafd4191c2d0f24a35038de560f6bb332c994bb
Dane Thurber <dane.thurber@gmail.com>
2020-07-15 13:20:31 -0600

    Revert "test(scrollable): migration to new storybook config; add wdyu"
    This reverts commit c12044ae27089ae0517beddbada0828f60a8a173.

--- packages/scrollable/.storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.story.js'],
-  addons: [
-    '@storybook/addon-actions',
-    '@pluralsight/ps-design-system-storybook-addon-theme/register'
-  ]
-}
--- packages/scrollable/.storybook/preview.js
+++
@@ -1,7 +0,0 @@
-import { addDecorator } from '@storybook/react'
-
-import centerDecorator from '@pluralsight/ps-design-system-storybook-addon-center'
-import themeDecorator from '@pluralsight/ps-design-system-storybook-addon-theme'
-
-addDecorator(centerDecorator)
-addDecorator(themeDecorator)
