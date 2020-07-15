4120ada2c2d291d52a44d937dd5743a092778d8c
Dane Thurber <dane.thurber@gmail.com>
2020-07-15 11:37:22 -0600

    test(scrollable): remove wdyr dep until needed

--- packages/scrollable/.storybook/preview.js
+++ packages/scrollable/.storybook/preview.js
@@ -1,8 +1,4 @@
-import './wdyr.js'
-
-import React from 'react'
-
-import { addDecorator, configure } from '@storybook/react'
+import { addDecorator } from '@storybook/react'
 
 import centerDecorator from '@pluralsight/ps-design-system-storybook-addon-center'
 import themeDecorator from '@pluralsight/ps-design-system-storybook-addon-theme'
