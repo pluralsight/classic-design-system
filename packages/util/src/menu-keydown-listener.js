c2c906dca57baa4cc42458a8eaa2ce84c9c29d49
Edward Irby <EdwardIrby@users.noreply.github.com>
2020-07-30 11:22:35 -0700

    Feat/useMenuKeyEvents (#1072)
    * feat(util): intial work on useMenuKeyEvents utils
    
    * feat(util): storybook of interactions
    
    * feat(util): universal menu keydown listener
    
    * feat(util): rename jsdom setup file
    
    * test(util): need to figure out why actionmenu is storyshot is failing
    
    * test(util): add useMenuKeyEvents story to util package

---
+++ packages/util/src/menu-keydown-listener.js
@@ -0,0 +1,114 @@
+import { useCallback, createRef } from 'react'
+import { useCombinedRefs } from './use-combined-refs.js'
+
+const searchListItem = (el, down = true) => {
+  if (!el) return
+  return el.tagName === 'LI'
+    ? el
+    : searchListItem(
+        down ? el.nextElementSibling : el.previousElementSibling,
+        down
+      )
+}
+
+const rightArrow = e => {
+  const subMenu = e.target.querySelector('* > ul')
+  if (subMenu.tagName !== 'UL') return
+  const firstChild = searchListItem(subMenu.firstElementChild)
+  firstChild && firstChild.focus()
+}
+
+const leftArrow = e => {
+  const rootMenu = e.target.parentNode === e.currentTarget
+  if (rootMenu) return
+  e.target.blur()
+  e.target.parentNode.parentNode.focus()
+}
+
+const downArrow = e => {
+  const nextSibling = searchListItem(e.target.nextElementSibling)
+  const firstChild = searchListItem(e.target.parentNode.firstElementChild)
+  nextSibling ? nextSibling.focus() : firstChild.focus()
+}
+
+const upArrow = e => {
+  const prevSibling = searchListItem(e.target.previousElementSibling, false)
+  const lastChild = searchListItem(e.target.parentNode.lastElementChild, false)
+  prevSibling ? prevSibling.focus() : lastChild.focus()
+}
+
+const home = e => {
+  const firstChild = searchListItem(e.target.parentNode.firstElementChild)
+  firstChild.focus()
+}
+
+const end = e => {
+  const lastChild = searchListItem(e.target.parentNode.lastElementChild, false)
+  lastChild.focus()
+}
+
+const startsWith = (el, key) => {
+  return (
+    el.innerText.startsWith(key) || el.innerText.startsWith(key.toUpperCase())
+  )
+}
+
+const character = e => {
+  const target = e.target
+  const char = e.key.length === 1 ? e.key : false
+  if (!char) return
+  const siblings = []
+  let sibling =
+    searchListItem(target.nextElementSibling) ||
+    searchListItem(target.parentNode.firstElementChild)
+  while (sibling) {
+    if (sibling === target) break
+    siblings.push(sibling)
+    sibling =
+      searchListItem(sibling.nextElementSibling) ||
+      searchListItem(target.parentNode.firstElementChild)
+  }
+  for (const el of siblings) {
+    if (startsWith(el, char)) {
+      el.focus()
+      break
+    }
+  }
+}
+
+const menuKeydownEventsHandler = e => {
+  e.key === 'ArrowRight'
+    ? rightArrow(e)
+    : e.key === 'ArrowLeft'
+    ? leftArrow(e)
+    : e.key === 'ArrowDown'
+    ? downArrow(e)
+    : e.key === 'ArrowUp'
+    ? upArrow(e)
+    : e.key === 'Home'
+    ? home(e)
+    : e.key === 'End'
+    ? end(e)
+    : character(e)
+}
+
+export const menuKeydownListener = root => {
+  const firstMenuItem = searchListItem(root.firstElementChild)
+  firstMenuItem && firstMenuItem.focus()
+  firstMenuItem && root.addEventListener('keydown', menuKeydownEventsHandler)
+  return menuKeydownEventsHandler
+}
+
+export const useMenuKeyEvents = () => {
+  const outer = createRef()
+  const inner = useCallback(node => {
+    if (node !== null) {
+      menuKeydownListener(node)
+    }
+  }, [])
+  const ref = useCombinedRefs(outer, inner)
+  return [
+    ref,
+    () => ref.current.removeEventListener('keydown', menuKeydownEventsHandler)
+  ]
+}
