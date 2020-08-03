86345f03822ed6f67cb9ebf94a2139845c150b1e
Dane Thurber <dane.thurber@gmail.com>
2020-08-03 15:10:54 -0600

    feat(util): new method for cloning while preserving refs

---
+++ packages/util/src/clone-element-with-ref.js
@@ -0,0 +1,17 @@
+import { cloneElement } from 'react'
+
+import { isFunction, isRef } from './index.js'
+
+export function cloneElementWithRef(element, outerRef, props) {
+  const { ref } = element
+
+  return cloneElement(element, {
+    ...props,
+    ref: node => {
+      if (isRef(outerRef)) outerRef.current = node
+
+      if (isFunction(ref)) ref(node)
+      else if (isRef(ref)) ref.current = node
+    }
+  })
+}
