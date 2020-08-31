340c7791a7e42a9f101e9ec819d7c5acd28c10bb
jaketrent <trent.jake@gmail.com>
2020-08-31 15:16:57 -0600

    feat(util): add stylesFor helper

---
+++ packages/util/src/__specs__/styles.spec.js
@@ -0,0 +1,25 @@
+import { stylesFor } from '../styles'
+
+it('returns empty object by default', () => {
+  expect(stylesFor()).toEqual({})
+})
+
+it('returns empty object without props', () => {
+  expect(stylesFor('someKey')).toEqual({})
+})
+
+it('returns empty object without selectorKey', () => {
+  expect(stylesFor(null, { UNSAFE_stylesFor: { some: 'css' } })).toEqual({})
+})
+
+it('returns empty object without matching selectorKey', () => {
+  expect(stylesFor('other', { UNSAFE_stylesFor: { some: 'css' } })).toEqual({})
+})
+
+it('returns the css for the specific selectorKey', () => {
+  expect(
+    stylesFor('specific', {
+      UNSAFE_stylesFor: { some: 'css', specific: { desired: 'css' } }
+    })
+  ).toEqual({ desired: 'css' })
+})
