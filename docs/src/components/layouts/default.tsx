11a69a778a7e3855f799441c605535ccadb73998
Dane Thurber <dane.thurber@gmail.com>
2020-09-23 13:14:12 -0600

    feat(docs): add meta title

--- docs/src/components/layouts/default.tsx
+++ docs/src/components/layouts/default.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 import { MDXProvider } from '../mdx'
 import { Frame } from '../frame'
 import { SideNav } from '../side-nav'
 
-const DefaultLayout: React.FC = props => {
+interface LayoutProps {
+  pageContext: PageContext
+}
+interface PageContext {
+  frontmatter?: FrontMatter
+}
+interface FrontMatter {
+  name?: string
+}
+
+const DefaultLayout: React.FC<LayoutProps> = props => {
   const { children } = props
+
+  const name = props.pageContext.frontmatter?.name
+  const title = `${name} | Pluralsight Design System`
 
   return (
     <MDXProvider>
+      <Helmet title={title} />
       <Frame aside={<SideNav />}>{children}</Frame>
     </MDXProvider>
   )
