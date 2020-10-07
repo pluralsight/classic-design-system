3c94297d2c9d730ebc450a4478b92a64c92bd5c1
jaketrent <trent.jake@gmail.com>
2020-10-07 09:29:29 -0600

    refactor(docs): rm theme from monospace component

--- docs/src/components/typography/monospace-font.tsx
+++ docs/src/components/typography/monospace-font.tsx
@@ -1,4 +1,3 @@
-import Theme, { useTheme } from '@pluralsight/ps-design-system-theme'
 import cx from 'classnames'
 import React from 'react'
 
@@ -6,7 +5,7 @@ import { Box } from '../box'
 import * as styles from './monospace-font.module.css'
 
 export const MonospaceGlyphs = () => (
-  <Box className={`${styles.box} ${styles.glyphs}`}>
+  <Box className={cx(styles.box, styles.glyphs)}>
     <div>A B C D E F G H I J K L M N O P Q R S T U V W X Y Z</div>
     <div>a b c d e f g h i j k l m n o p q r s t u v w x y z</div>
     <div>0 1 2 3 4 5 6 7 8 9</div>
@@ -15,19 +14,9 @@ export const MonospaceGlyphs = () => (
   </Box>
 )
 
-export const MonospaceFontWeight = () => {
-  const themeName = useTheme()
-  return (
-    <Box
-      className={cx({
-        [styles.box]: true,
-        [styles.fontWeight]: true,
-        [styles.dark]: themeName === Theme.names.dark,
-        [styles.light]: themeName === Theme.names.light
-      })}
-    >
-      <div className={styles.fontLetter}>Aa</div>
-      <div className={styles.fontLabel}>MEDIUM (500)</div>
-    </Box>
-  )
-}
+export const MonospaceFontWeight = () => (
+  <Box className={cx(styles.box, styles.fontWeight)}>
+    <div className={styles.fontLetter}>Aa</div>
+    <div className={styles.fontLabel}>MEDIUM (500)</div>
+  </Box>
+)
