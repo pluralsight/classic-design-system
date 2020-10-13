c4d25e9e05496992077df5931bd5191cf396699f
jaketrent <trent.jake@gmail.com>
2020-10-13 09:09:27 -0600

    build(circularprogress): update build scripts to ts

--- packages/circularprogress/src/react/__specs__/index.spec.tsx
+++ packages/circularprogress/src/react/__specs__/index.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 
-import CircularProgress from '../index.js'
+import CircularProgress from '..'
 
 describe('CircularProgress', () => {
   it('renders', () => {
