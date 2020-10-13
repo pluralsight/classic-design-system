ccfba2ce5348150f4415b0ca20b48e85134fce88
Dane Thurber <dane.thurber@gmail.com>
2020-10-13 08:21:45 -0600

    refactor(prop-types): change method of determining NODE_ENV to be compat with webpack5

--- packages/prop-types/index.js
+++ packages/prop-types/index.js
@@ -1,4 +1,4 @@
-const isProd = process && process.env && process.env.NODE_ENV === 'production'
-module.exports = isProd
-  ? require('./dist/stubs/index.js')
-  : require('./dist/index.js')
+module.exports =
+  process.env.NODE_ENV === 'production'
+    ? require('./dist/stubs/index.js')
+    : require('./dist/index.js')
