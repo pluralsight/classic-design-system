c860e51de9efb97d7955241003e76ed691e65b66
Dane Thurber <dane.thurber@gmail.com>
2020-10-19 13:38:11 -0600

    refactor(icon): convert to typescript
    BREAKING CHANGE: drop support for antiquated css prop
    
    Co-authored-by: Edward Irby<e.irby@pm.me>

---
+++ packages/icon/src/vars/index.ts
@@ -0,0 +1,19 @@
+import { keyMirror } from '@pluralsight/ps-design-system-util'
+
+export const colors = keyMirror(
+  'blue',
+  'green',
+  'red',
+  'textIconHighOnDark',
+  'textIconHighOnLight',
+  'textIconLowOnDark',
+  'textIconLowOnLight',
+  'yellow'
+)
+export const sizes = keyMirror('small', 'medium', 'large')
+
+export const widths = {
+  small: '16px',
+  medium: '24px',
+  large: '48px'
+}
