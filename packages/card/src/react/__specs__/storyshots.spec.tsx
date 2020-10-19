6707c1116ca0c99e91297b07dda4ae1c383fb0a3
jaketrent <trent.jake@gmail.com>
2020-10-19 14:18:47 -0600

    test(card): ts-ignore createNodeMock for shiitake

---
+++ packages/card/src/react/__specs__/storyshots.spec.tsx
@@ -0,0 +1,13 @@
+import path from 'path'
+import initStoryshots, {
+  snapshotWithOptions
+} from '@storybook/addon-storyshots'
+
+const createNodeMock = () => document.createElement('div')
+
+initStoryshots({
+  configPath: path.resolve(__dirname, '../../../.storybook'),
+  framework: 'react',
+  // @ts-ignore
+  test: snapshotWithOptions({ createNodeMock })
+})
