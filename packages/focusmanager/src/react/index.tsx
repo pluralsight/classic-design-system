d1dcf4fe1aee538c334f9b3bc687818d751e780f
Edward Irby <e.irby@pm.me>
2020-10-21 16:43:43 -0700

    feat(focusmanager): ts conversion

--- packages/focusmanager/src/react/index.tsx
+++ packages/focusmanager/src/react/index.tsx
@@ -1,34 +1,26 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import React, { HTMLAttributes } from 'react'
+import useFocusManager from './use-focus-manager'
 
-import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
-
-import useFocusManager from './use-focus-manager.js'
-
-const FocusManager = React.forwardRef((props, _ref) => {
-  const ref = React.useRef()
-  React.useImperativeHandle(_ref, () => ref.current)
-
-  useFocusManager(ref, {
-    autofocus: props.autofocus,
-    returnFocus: props.returnFocus,
-    trapped: props.trapped
-  })
-
-  return <div ref={ref} {...filterReactProps(props)} />
-})
-
-FocusManager.propTypes = {
-  autofocus: PropTypes.bool,
-  children: PropTypes.node,
-  returnFocus: PropTypes.bool,
-  trapped: PropTypes.bool
+export interface FocusManagerProps extends HTMLAttributes<HTMLDivElement> {
+  autofocus?: boolean
+  returnFocus?: boolean
+  trapped?: boolean
 }
 
-FocusManager.defaultProps = {
-  autofocus: true,
-  returnFocus: true,
-  trapped: true
-}
+const FocusManager = React.forwardRef<HTMLDivElement, FocusManagerProps>(
+  ({ autofocus = true, returnFocus = true, trapped = true, ...rest }, _ref) => {
+    const ref = React.useRef<HTMLDivElement>(null)
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    React.useImperativeHandle(_ref, () => ref.current as HTMLDivElement)
+
+    useFocusManager(ref, {
+      autofocus: autofocus,
+      returnFocus: returnFocus,
+      trapped: trapped
+    })
+
+    return <div ref={ref} {...rest} />
+  }
+)
 
 export default FocusManager
