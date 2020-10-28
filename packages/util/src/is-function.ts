b1d57e43b39c112b8e386a8889272685ab34be47
Dane Thurber <dane.thurber@gmail.com>
2020-10-28 13:04:35 -0600

    feat(util): update function type guard

--- packages/util/src/is-function.ts
+++ packages/util/src/is-function.ts
@@ -1,3 +1,9 @@
-export function isFunction(val: unknown): val is (...args: unknown[]) => unknown {
+export const isFunction = <Params extends unknown[], R>(
+  val: unknown
+): val is Fn<Params, R> => {
   return typeof val === 'function'
+}
+
+interface Fn<P extends unknown[], R = unknown> {
+  (...args: P): R
 }
