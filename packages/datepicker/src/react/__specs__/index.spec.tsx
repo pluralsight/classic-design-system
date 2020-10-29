eb9676297d016a58846b50da163e424155f6ff73
jaketrent <trent.jake@gmail.com>
2020-10-29 11:42:26 -0600

    test(datepicker): fix eslint disable at file level

--- packages/datepicker/src/react/__specs__/index.spec.tsx
+++ packages/datepicker/src/react/__specs__/index.spec.tsx
@@ -1,4 +1,4 @@
-// eslint-disable @typescript-eslint/no-unnecessary-type-assertion
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 
