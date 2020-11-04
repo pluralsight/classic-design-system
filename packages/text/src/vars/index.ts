87679f355765b41cb8e352bf7ffd066617d0f9aa
Brian Sweeting <brian-sweeting@pluralsight.com>
2020-11-04 09:28:20 -0700

    refactor(text): new sizes

--- packages/text/src/vars/index.ts
+++ packages/text/src/vars/index.ts
@@ -1,5 +1,15 @@
 import { keyMirror } from '@pluralsight/ps-design-system-util'
 
-export const headingSizes = keyMirror('smallCaps', 'medium', 'large', 'xLarge')
+export const displaySizes = keyMirror('small', 'medium', 'large')
+
+export const headingSizes = keyMirror('xsmall', 'small', 'medium', 'large', 'xlarge')
+
+export const smallCapsSizes = keyMirror('small', 'medium', 'large')
+
+export const labelSizes = keyMirror('xsmall', 'small', 'normal', 'large')
+
+export const pSizes = keyMirror('small', 'normal', 'large')
+
+export const monospaceSizes = keyMirror('small', 'normal', 'large')
 
 export const listTypes = keyMirror('default', 'bulleted', 'numbered')
