d84d8061cf243c3daafbdd27f325de3f3cc2c4f8
Dane Thurber <dane.thurber@gmail.com>
2020-11-09 10:03:41 -0700

    refactor(navuser): convert to typscript

--- packages/navuser/src/react/index.tsx
+++ packages/navuser/src/react/index.tsx
@@ -1,72 +1,113 @@
 import Avatar from '@pluralsight/ps-design-system-avatar'
-import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
 import Halo from '@pluralsight/ps-design-system-halo'
-import { css } from 'glamor'
-import React from 'react'
-import PropTypes from 'prop-types'
+import { PropsOf, RefFor } from '@pluralsight/ps-design-system-util'
+import { compose, css } from 'glamor'
+import React, { ReactNode, MouseEventHandler } from 'react'
 
-import stylesheet from '../css/index.js'
+import stylesheet from '../css'
 
 const styles = {
-  navUser: props =>
-    css(
-      stylesheet['.psds-navuser'],
-      (props.href || props.onClick) && stylesheet['.psds-navuser--clickable']
-    ),
+  navUser: (props: NavUserProps) => {
+    const clickable: boolean = !!props.href || !!props.onClick
+
+    return compose(
+      css(stylesheet['.psds-navuser']),
+      clickable && css(stylesheet['.psds-navuser--clickable'])
+    )
+  },
   meta: () => css(stylesheet['.psds-navuser__plan-name']),
   name: () => css(stylesheet['.psds-navuser__name']),
   words: () => css(stylesheet['.psds-navuser__words'])
 }
 
-const NavUser = React.forwardRef((props, forwardedRef) => {
-  const tagName = props.href ? 'a' : props.onClick ? 'button' : 'div'
-
-  const ref = React.useRef()
-  React.useImperativeHandle(forwardedRef, () => ref.current)
-
-  return (
-    <Halo inline gapSize={Halo.gapSizes.small}>
-      {React.createElement(
-        tagName,
-        {
-          ...filterReactProps(props, { tagName }),
-          ...styles.navUser(props),
-          ref
-        },
-        <>
-          <Avatar
-            src={props.src}
-            size={Avatar.sizes.xSmall}
-            name={props.name}
-          />
-          <Words {...props} />
-        </>
-      )}
-    </Halo>
-  )
-})
-NavUser.displayName = 'NavUser'
-NavUser.propTypes = {
-  href: PropTypes.string,
-  onClick: PropTypes.func,
-  meta: PropTypes.node,
-  name: PropTypes.node,
-  src: PropTypes.string
+interface BaseNavUserProps {
+  meta?: ReactNode
+  name?: string
+  src?: string
 }
+interface AnchorProps extends BaseNavUserProps, PropsOf<'a'> {
+  href: string
+  onClick?: MouseEventHandler<HTMLAnchorElement>
+}
+interface ButtonProps extends BaseNavUserProps, PropsOf<'button'> {
+  href?: undefined
+  onClick: MouseEventHandler<HTMLButtonElement>
+}
+interface DivProps extends BaseNavUserProps, PropsOf<'div'> {
+  href?: undefined
+  onClick?: MouseEventHandler<HTMLDivElement>
+}
+
+type NavUserElement = HTMLAnchorElement | HTMLButtonElement | HTMLDivElement
+type NavUserProps = AnchorProps | ButtonProps | DivProps
+type NavUserComponent = React.ForwardRefExoticComponent<NavUserProps> & {
+  (props: AnchorProps, ref?: RefFor<'a'>): JSX.Element
+  (props: ButtonProps, ref?: RefFor<'button'>): JSX.Element
+  (props: DivProps, ref?: RefFor<'div'>): JSX.Element
+}
+
+const NavUser = React.forwardRef<NavUserElement, NavUserProps>(
+  (props, forwardedRef) => {
+    const { meta, name, src, ...rest } = props
+
+    const ref = React.useRef<NavUserElement>(null)
+    React.useImperativeHandle(
+      forwardedRef,
+      () => (ref.current as unknown) as NavUserElement
+    )
+
+    const isAnchor = 'href' in props
+    const isButton = !isAnchor && 'onClick' in props
+
+    const Wrapper: React.FC = wrapperProps =>
+      isAnchor ? (
+        <a
+          ref={ref as RefFor<'a'>}
+          {...(rest as PropsOf<'a'>)}
+          {...wrapperProps}
+        />
+      ) : isButton ? (
+        <button
+          ref={ref as RefFor<'button'>}
+          {...wrapperProps}
+          {...(rest as PropsOf<'button'>)}
+        />
+      ) : (
+        <div
+          ref={ref as RefFor<'div'>}
+          {...wrapperProps}
+          {...(rest as PropsOf<'div'>)}
+        />
+      )
+
+    return (
+      <Halo inline gapSize={Halo.gapSizes.small}>
+        <Wrapper {...styles.navUser(props)}>
+          <>
+            <Avatar src={src} size={Avatar.sizes.xSmall} name={name} />
+            <Words name={name} meta={meta} />
+          </>
+        </Wrapper>
+      </Halo>
+    )
+  }
+) as NavUserComponent
+
+NavUser.displayName = 'NavUser'
 
 export default NavUser
 
-function Words(props) {
-  return props.name || props.meta ? (
-    <div {...styles.words()}>
-      <div {...styles.name()}>{props.name}</div>
-      <div {...styles.meta()}>{props.meta}</div>
-    </div>
-  ) : null
+interface WordsProps extends PropsOf<'div'> {
+  meta?: ReactNode
+  name?: string
 }
-NavUser.displayName = 'NavUser.Words'
-Words.propTypes = {
-  meta: PropTypes.string,
-  name: PropTypes.string,
-  src: PropTypes.string
+const Words: React.FC<WordsProps> = props => {
+  const { name, meta, ...rest } = props
+
+  return (
+    <div {...styles.words()} {...rest}>
+      <div {...styles.name()}>{name}</div>
+      <div {...styles.meta()}>{meta}</div>
+    </div>
+  )
 }
