c4b0f2432981ab1db0fe3d593692bfd8175a806e
Edward Irby <e.irby@pm.me>
2020-11-09 17:03:48 -0800

    feat(starrating): ts conversion

--- packages/starrating/src/react/__stories__/index.story.tsx
+++ packages/starrating/src/react/__stories__/index.story.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 
-import StarRating from '../index.js'
+import StarRating from '..'
 
 const VALUES = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, null, undefined]
 const ROUNDING_VALUES = [1.7, 2.0, 2.2, 2.4, 2.6, 2.78]
@@ -16,14 +16,20 @@ const rounding = storiesOf(
 )
 
 VALUES.forEach(value => {
-  interactive.add(`${value}`, _ => (
+  interactive.add(`${value}`, () => (
     <StarRating onChange={action('value changed')} value={value} />
   ))
-  nonInteractive.add(`${value}`, _ => <StarRating value={value} />)
+  nonInteractive.add(`${value}`, () => <StarRating value={value} />)
 })
 
 ROUNDING_VALUES.forEach(value => {
-  rounding.add(`${value}`, _ => (
-    <StarRating onChange={action('value changed')} value={value} />
+  rounding.add(`${value}`, () => (
+    <StarRating
+      onChange={(
+        val: React.Key,
+        evt: React.ChangeEvent<Element> | React.MouseEvent<Element, MouseEvent>
+      ) => action('value changed')}
+      value={value}
+    />
   ))
 })
--- packages/starrating/src/react/index.tsx
+++ packages/starrating/src/react/index.tsx
@@ -1,11 +1,9 @@
 import { css } from 'glamor'
-import PropTypes from 'prop-types'
-import React from 'react'
+import React, { HTMLAttributes } from 'react'
 
-import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
-
-import Star from './star.js'
-import stylesheet from '../css/index.js'
+import Star from './star'
+import stylesheet from '../css'
+import { ValueOf } from '@pluralsight/ps-design-system-util'
 
 const styles = {
   screenReaderInput: () =>
@@ -14,122 +12,133 @@ const styles = {
     css(stylesheet[`.psds-starrating__screen-reader-text`])
 }
 
-function ScreenReaderInput(props) {
-  return <input {...styles.screenReaderInput(props)} tabIndex={-1} {...props} />
+interface ScreenReaderInputProps extends HTMLAttributes<HTMLInputElement> {
+  type: string
+  min: number
+  max: number
+  step: number
+  value: React.ReactText
 }
 
-function ScreenReaderText(props) {
-  return <span {...styles.screenReaderText(props)} {...props} />
+const ScreenReaderInput: React.FC<ScreenReaderInputProps> = props => (
+  <input {...styles.screenReaderInput()} tabIndex={-1} {...props} />
+)
+
+const ScreenReaderText: React.FC<HTMLAttributes<HTMLSpanElement>> = props => (
+  <span {...styles.screenReaderText()} {...props} />
+)
+
+export interface StarRatingProps
+  extends Omit<HTMLAttributes<HTMLDivElement>, 'onChange'> {
+  onChange?: (
+    val: React.ReactText,
+    evt: React.ChangeEvent | React.MouseEvent
+  ) => void
+  starCount?: number
+  value?: React.ReactText | null
 }
 
-const StarRating = React.forwardRef((props, ref) => {
-  const [hoverIndex, setHoverIndex] = React.useState(null)
-  const isInteractive = !!props.onChange
-  const { starCount, value } = props
+const StarRating = React.forwardRef<HTMLDivElement, StarRatingProps>(
+  ({ starCount = 5, value, onChange, ...props }, ref) => {
+    const [hoverIndex, setHoverIndex] = React.useState<null | number>(null)
+    const isInteractive = !!onChange
 
-  const { onChange, ...filteredProps } = filterReactProps(props)
+    const handleInputChanged = (evt: React.ChangeEvent<HTMLInputElement>) => {
+      if (!isInteractive) return
+      onChange && onChange((evt.target as HTMLInputElement).value, evt)
+    }
 
-  function handleInputChanged(evt) {
-    if (!isInteractive) return
-    props.onChange(evt.target.value, evt)
-  }
+    const handleStarClicked = (index: number, evt: React.MouseEvent) => {
+      if (!isInteractive) return
+      onChange && onChange(index + 1, evt)
+    }
 
-  function handleStarClicked(index, evt) {
-    if (!isInteractive) return
-    props.onChange(index + 1, evt)
-  }
+    const handleStarEnter = (index: number) => {
+      if (!isInteractive) return
+      setHoverIndex(index)
+    }
 
-  function handleStarEnter(index) {
-    if (!isInteractive) return
-    setHoverIndex(index)
-  }
+    const handleStarLeave = () => {
+      if (!isInteractive) return
+      setHoverIndex(null)
+    }
 
-  function handleStarLeave() {
-    if (!isInteractive) return
-    setHoverIndex(null)
-  }
-
-  const stars = (() => {
-    const hasValidRating = !Number.isNaN(parseFloat(value))
-    const isHovering = hoverIndex !== null
-
-    const valueRounded = Math.round(value * 2) / 2
-    const fullStars = Math.floor(valueRounded)
-    const halfStars = Math.floor(valueRounded) !== valueRounded
-
-    return [...Array(starCount)].map((_, index) => {
-      let active = false
-      let bright = false
-      let appearance
-
-      if (isHovering) {
-        if (index <= hoverIndex) {
-          active = true
-          appearance = Star.appearances.full
-        } else {
-          appearance = Star.appearances.empty
-        }
-      } else {
-        if (isInteractive && !hasValidRating) {
-          appearance = Star.appearances.empty
-          bright = true
-        } else if (index < fullStars) {
-          active = true
-          appearance = Star.appearances.full
-        } else if (index === fullStars && halfStars) {
-          active = true
-          appearance = Star.appearances.half
-        } else {
-          appearance = Star.appearances.full
-        }
-      }
-
-      return (
-        <Star
-          active={active}
-          appearance={appearance}
-          bright={bright}
-          index={index}
-          interactive={isInteractive}
-          key={index}
-          onClick={handleStarClicked}
-          onEnter={handleStarEnter}
-          onLeave={handleStarLeave}
-        />
+    const stars = (() => {
+      const hasValidRating = !Number.isNaN(
+        parseFloat((value as unknown) as string)
       )
-    })
-  })()
+      const isHovering = hoverIndex !== null
 
-  return (
-    <div {...filteredProps} ref={ref}>
-      {isInteractive && (
-        <label>
-          <ScreenReaderText>Rate</ScreenReaderText>
-          <ScreenReaderInput
-            type="range"
-            onChange={handleInputChanged}
-            min={1}
-            max={starCount}
-            step={1}
-            value={value || 0}
+      const valueRounded = Math.round((value as number) * 2) / 2
+      const fullStars = Math.floor(valueRounded)
+      const halfStars = Math.floor(valueRounded) !== valueRounded
+
+      return [...Array(starCount)].map((_, index) => {
+        let active = false
+        let bright = false
+        let appearance: ValueOf<typeof Star.appearances>
+
+        if (isHovering) {
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+          if (index <= (hoverIndex as number)) {
+            active = true
+            appearance = Star.appearances.full
+          } else {
+            appearance = Star.appearances.empty
+          }
+        } else {
+          if (isInteractive && !hasValidRating) {
+            appearance = Star.appearances.empty
+            bright = true
+          } else if (index < fullStars) {
+            active = true
+            appearance = Star.appearances.full
+          } else if (index === fullStars && halfStars) {
+            active = true
+            appearance = Star.appearances.half
+          } else {
+            appearance = Star.appearances.full
+          }
+        }
+
+        return (
+          <Star
+            active={active}
+            appearance={appearance}
+            bright={bright}
+            index={index}
+            interactive={isInteractive}
+            key={index}
+            onClick={handleStarClicked}
+            onEnter={handleStarEnter}
+            onLeave={handleStarLeave}
           />
-        </label>
-      )}
+        )
+      })
+    })()
 
-      {stars}
+    return (
+      <div {...props} ref={ref}>
+        {isInteractive && (
+          <label>
+            <ScreenReaderText>Rate</ScreenReaderText>
+            <ScreenReaderInput
+              type="range"
+              onChange={handleInputChanged}
+              min={1}
+              max={starCount}
+              step={1}
+              value={value || 0}
+            />
+          </label>
+        )}
 
-      <ScreenReaderText>This is rated {value}</ScreenReaderText>
-    </div>
-  )
-})
+        {stars}
 
-StarRating.propTypes = {
-  onChange: PropTypes.func,
-  starCount: PropTypes.number,
-  value: PropTypes.number
-}
-StarRating.defaultProps = {
-  starCount: 5
-}
+        <ScreenReaderText>This is rated {value}</ScreenReaderText>
+      </div>
+    )
+  }
+)
 
 export default StarRating
