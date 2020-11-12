a1494863ae91654ad265307da2db91275099348d
pierce-bringhurst <pierce-bringhurst@CORP.AMAZONWORKSPACES.COM>
2020-11-12 16:37:09 -0700

    Add help, signin, and signout slash commands

---
+++ IssueSlackApp/commands/issue-help/help.js
@@ -0,0 +1,42 @@
+
+const { App } = require('@slack/bolt');
+const dotenv = require('dotenv');
+dotenv.config()
+
+
+const app = new App({
+
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+  
+});
+
+exports.help = async ({ ack, body, client }) => {
+  
+    // Acknowledge the command request
+    await ack();
+
+    const userID = body['user_id']
+
+    let attachments = null;
+    let msg = "Thanks for using the Issue Poster app! \n\n" +
+            "You can use Issue Poster to submit issues to the design-system GitHub repository. \n\n" +
+            "Don't have a GitHub account? Just use the `/issue` command and follow the prompts. \n\n" +
+            "If you would like to submit the issue using your personal GitHub account, you can first sign-in with the `/issue-signin` command. \n\n" +
+            "You can also sign-out with the `/issue-signout`\n\n" +
+            "Use the `/issue-help` command to see this message. \n\n"
+
+    try {
+        await app.client.chat.postEphemeral({
+            token: client['token'],
+            attachments: null,
+            channel: body["channel_id"],
+            user: userID,
+            text: msg
+        });
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+  }
---
+++ IssueSlackApp/commands/issue-signin/signin.js
@@ -0,0 +1,91 @@
+https = require('https');
+const { App } = require('@slack/bolt');
+const dotenv = require('dotenv');
+dotenv.config()
+const { Client } = require('pg')
+
+const db = new Client();
+db.connect();
+
+const app = new App({
+
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+  
+});
+
+exports.signin = async ({ ack, body, client }) => {
+  
+    // Acknowledge the command request
+    await ack();
+
+    const userID = body['user_id']
+
+    const query1 = {
+      // give the query a unique name
+      name: 'fetch-user',
+      text: 'SELECT * FROM authenticatedusers WHERE slack_id = $1',
+      values: [userID]
+    }
+
+    let user = null;
+
+    await db.query(query1)
+       .then(res => {
+          user = res.rows[0];
+          console.log(user);
+           
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+    let attachments = null;
+    
+    if(user) {
+        attachments = [
+            {
+                text: "You are already signed in.",
+                fallback: "You are unable to sign-in.",
+                callback_id: "signin",
+                color: "#7CFC00",
+                attachment_type: "default",               
+            }
+        ]
+
+    } else {
+        attachments = [
+            {
+                text: "Sign-in to your GitHub account?",
+                fallback: "You are unable to sign-in.",
+                callback_id: "signin",
+                color: "#7CFC00",
+                attachment_type: "default",
+                actions: [
+                    {
+                        name: "signin",
+                        text: "Yes",
+                        type: "button",
+                        url: 'https://github.com/login/oauth/authorize?client_id=' + process.env.OAUTH_CLIENT_ID + '&state=' + userID
+                        +'&scope=repo' + '&redirect_uri=' + process.env.REDIRECT_URI 
+                    }
+                ]
+            }
+        ]
+    }
+    
+    
+        try {
+          await app.client.chat.postEphemeral({
+            token: client['token'],
+            attachments: attachments,
+            channel: body["channel_id"],
+            user: userID
+          });
+        }
+        catch (error) {
+          console.error(error);
+        }  
+        
+  
+  }
