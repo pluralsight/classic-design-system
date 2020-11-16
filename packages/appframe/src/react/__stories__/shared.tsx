7b85dfff4eb2350321a4e8097ba35b2611fe8f94
Brian Sweeting <brian-sweeting@pluralsight.com>
2020-11-16 16:34:52 -0700

    refactor(appframe): clean up linting errors

--- packages/appframe/src/react/__stories__/shared.tsx
+++ packages/appframe/src/react/__stories__/shared.tsx
@@ -278,10 +278,15 @@ export const SectionHeader: React.FC<SectionHeaderProps> = props => {
       }}
       {...rest}
     >
-
-        <h2 style={{ margin: 0 }}><Text.Label size={Text.Label.sizes.xSmall} color={Text.Label.colors.secondary} caps>
-          {title}</Text.Label></h2>
-
+      <h2 style={{ margin: 0 }}>
+        <Text.Label
+          size={Text.Label.sizes.xSmall}
+          color={Text.Label.colors.secondary}
+          caps
+        >
+          {title}
+        </Text.Label>
+      </h2>
     </div>
   )
 }
