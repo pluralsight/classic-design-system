e2cace119ab7bf2749586bbc3e46c0dde5dd7a6e
Brian Sweeting <brian-sweeting@pluralsight.com>
2020-11-17 11:04:15 -0700

    refactor(docs): remove intro styles

--- docs/src/components/intro.tsx
+++ docs/src/components/intro.tsx
@@ -1,8 +1,6 @@
 import { P } from '@pluralsight/ps-design-system-text'
 import React from 'react'
 
-import * as styles from './intro.module.css'
-
 export const Intro: React.FC = props => {
   return <P size={P.sizes.large} {...props} />
 }
