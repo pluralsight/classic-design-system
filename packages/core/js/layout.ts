15f90283d67f6b53c8db3638bd6181a8b6b3805c
jaketrent <trent.jake@gmail.com>
2020-12-17 05:47:42 -0700

    feat(core): add layers for z-index
    Combined from appframe and prism codebases:
    
    // appframe
    /* DONE content: 0, */
    /* DONE sidenav: 930, */
    /* DONE topnav: 950, */
    /* DONE skipBanner: 1600 */
    
    //prism
    /* DONE main: 0, */
    /* RM, moved away from in design - footerMobile: 930, */
    /* RM, not used in new appframe - sidenavMobile: 940, */
    /* DONE topnav: 950, */
    /* RM, not used in new appframe - sidenavDesktop: 980, */
    /* RM, moved away from in design - footerDesktop: 990, */
    /* DONE dropdown: 1000, */
    /* RM, unused cookieBanner: 1002, */
    /* RM, unused dropdownArrow: 1002, */
    /* RM, unused in appframe - searchMobile: 1010, */
    /* RM, unused searchOverlay: 1500, */
    /* DONE skipBanner: 1600 */

--- packages/core/js/layout.ts
+++ packages/core/js/layout.ts
@@ -1,9 +1,7 @@
 export default {
-  spacingXXSmall: '4px',
-  spacingXSmall: '8px',
-  spacingSmall: '12px',
-  spacingMedium: '16px',
-  spacingLarge: '24px',
-  spacingXLarge: '48px',
-  spacingXXLarge: '64px'
+  main: '0',
+  globalSidenav: '930',
+  globalTopnav: '950',
+  globalDropdown: '1000',
+  globalSkiptoContentBanner: '1600'
 }
