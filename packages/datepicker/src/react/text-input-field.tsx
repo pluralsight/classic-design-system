421d6b2ef1dcaecff3589870bbc76df42f0adb98
jaketrent <trent.jake@gmail.com>
2021-03-05 11:30:56 -0700

    refactor(datepicker): memoize datepicker computed subcomponents

--- packages/datepicker/src/react/text-input-field.tsx
+++ packages/datepicker/src/react/text-input-field.tsx
@@ -1,7 +1,7 @@
 import Field from '@pluralsight/ps-design-system-field'
 import { CalendarIcon } from '@pluralsight/ps-design-system-icon'
 import { useUniqueId } from '@pluralsight/ps-design-system-util'
-import React, { ComponentProps, FC } from 'react'
+import React, { ComponentProps, FC, isValidElement, useMemo } from 'react'
 
 interface TextInputFieldProps extends ComponentProps<typeof Field> {
   onChange?: React.ChangeEventHandler<HTMLInputElement>
@@ -10,7 +10,7 @@ interface TextInputFieldProps extends ComponentProps<typeof Field> {
 }
 
 export const TextInputField: FC<TextInputFieldProps> = props => {
-  let {
+  const {
     disabled,
     label,
     onChange,
@@ -24,26 +24,36 @@ export const TextInputField: FC<TextInputFieldProps> = props => {
   const labelId = useUniqueId('text-input__label-')
   const inputId = useUniqueId('text-input__input-')
 
-  if (!prefix) {
-    prefix = <CalendarIcon onClick={onClick} style={{ cursor: 'pointer' }} />
-  }
-  if (typeof label === 'string') {
-    label = (
+  const Prefix = useMemo(() => {
+    if (isValidElement(prefix)) return prefix
+
+    return <CalendarIcon onClick={onClick} style={{ cursor: 'pointer' }} />
+  }, [prefix])
+
+  const Label = useMemo(() => {
+    if (!label) return
+    if (isValidElement(label)) return label
+
+    return (
       <Field.Label htmlFor={inputId} id={labelId}>
         {label}
       </Field.Label>
     )
-  }
-  if (typeof subLabel === 'string') {
-    subLabel = <Field.SubLabel>{subLabel}</Field.SubLabel>
-  }
+  }, [label])
+
+  const SubLabel = useMemo(() => {
+    if (!subLabel) return
+    if (isValidElement(subLabel)) return subLabel
+
+    return <Field.SubLabel>{subLabel}</Field.SubLabel>
+  }, [subLabel])
 
   return (
     <Field
       disabled={disabled}
-      prefix={prefix}
-      label={label}
-      subLabel={subLabel}
+      prefix={Prefix}
+      label={Label}
+      subLabel={SubLabel}
       {...rest}
     >
       <Field.Input
