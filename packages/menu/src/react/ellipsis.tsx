aaca0e731564d8eec32f24e1b222bfbd3bc412d9
Edward Irby <EdwardIrby@users.noreply.github.com>
2021-03-16 16:01:56 -0700

    new components: useListbox, Select, & Menu (#1648)
    * feat(menu): adds stateless menu component to design system
    
    * feat(select): create new stateless Select.Button and Select.Slected and sateful hook"
    
    working on Select styles and tests
    
    * feat(menu): items use future collection api { id, name}
    
    * feat(select): convience wrapper
    
    * feat(menu): adds MenuItemWithDescription named epxport
    
    * feat(menu): add's optional menu item with desc component
    
    * docs: fix workspace configuration

---
+++ packages/menu/src/react/ellipsis.tsx
@@ -0,0 +1,13 @@
+import { HTMLPropsFor } from '@pluralsight/ps-design-system-util'
+import { css } from 'glamor'
+import React, { forwardRef } from 'react'
+
+import stylesheet from '../css'
+
+const styles = css(stylesheet[`.psds-menu__ellipsis`])
+
+export const Ellipsis = forwardRef<HTMLSpanElement, HTMLPropsFor<'span'>>(
+  (props, ref) => <span ref={ref} {...props} {...styles} />
+)
+
+Ellipsis.displayName = 'Menu.Ellipsis'
---
+++ packages/select/src/react/button.tsx
@@ -0,0 +1,86 @@
+import React, { forwardRef } from 'react'
+import { compose, css } from 'glamor'
+import {
+  names as themeNames,
+  useTheme
+} from '@pluralsight/ps-design-system-theme'
+import stylesheet from '../css'
+import { WarningIcon, CaretDownIcon } from '@pluralsight/ps-design-system-icon'
+import { HTMLPropsFor, ValueOf } from '@pluralsight/ps-design-system-util'
+import * as vars from '../vars'
+
+const styles = {
+  button: ({
+    error,
+    size,
+    themeName,
+    isOpen
+  }: {
+    error: boolean
+    size: ValueOf<typeof vars.sizes>
+    themeName: ValueOf<typeof themeNames>
+    isOpen: boolean
+  }) => {
+    const label = 'psds-select__button'
+    return compose(
+      css(stylesheet[`.${label}`]),
+      error && css(stylesheet['.psds-select__button--error']),
+      size === vars.sizes.small &&
+        css(stylesheet['.psds-select__button--small']),
+      css(stylesheet[`.${label}.psds-theme--${themeName}`]),
+      isOpen && css(stylesheet[`.${label}--is-open.psds-theme--${themeName}`])
+    )
+  },
+  inner: () => css(stylesheet['.psds-select__button-inner']),
+  icon: (themeName: ValueOf<typeof themeNames>) =>
+    compose(
+      css(stylesheet['.psds-select__icon']),
+      css(stylesheet[`.psds-select__icon.psds-theme--${themeName}`])
+    ),
+  warningIcon: () => css(stylesheet['.psds-select__error'])
+}
+
+interface SelectButtonProps extends HTMLPropsFor<'button'> {
+  disabled?: boolean
+  error?: boolean
+  isOpen?: boolean
+  'aria-haspopup'?: 'listbox'
+  onClick?: (evt: React.MouseEvent | React.KeyboardEvent) => void
+  onKeyDown?: (evt: React.MouseEvent | React.KeyboardEvent) => void
+  size?: ValueOf<typeof vars.sizes>
+}
+
+export const Button = forwardRef<HTMLButtonElement, SelectButtonProps>(
+  (
+    {
+      children,
+      disabled,
+      error,
+      onClick,
+      size = vars.sizes.medium,
+      isOpen = false,
+      ...rest
+    },
+    ref
+  ) => {
+    const themeName = useTheme()
+
+    return (
+      <button
+        {...styles.button({ error: Boolean(error), size, themeName, isOpen })}
+        ref={ref}
+        disabled={disabled}
+        onClick={disabled ? undefined : onClick}
+        {...rest}
+      >
+        <div {...styles.inner()}>
+          {children}
+          <CaretDownIcon {...styles.icon(themeName)} />
+        </div>
+        {error && <WarningIcon {...styles.warningIcon()} />}
+      </button>
+    )
+  }
+)
+
+Button.displayName = 'Select.Button'
