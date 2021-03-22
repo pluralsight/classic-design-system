690aaa8cd53fca2248a0faefb4e405af4a5603d0
Dane Thurber <dane.thurber@gmail.com>
2021-03-22 12:38:53 -0600

    refactor(navcookiebanner): convert stories to csf. add jest-axe tests

--- packages/navcookiebanner/src/react/__stories__/index.story.tsx
+++ packages/navcookiebanner/src/react/__stories__/index.story.tsx
@@ -1,16 +1,24 @@
-import { storiesOf } from '@storybook/react'
-import React from 'react'
+import { Meta, Story } from '@storybook/react/types-6-0'
+import React, { ComponentProps } from 'react'
 
 import NavCookieBanner from '..'
 
-storiesOf('NavCookieBanner', module)
-  .add('Basic', () => <NavCookieBanner />)
-  .add('Custom message', () => (
-    <NavCookieBanner
-      message={
-        <>
-          <label>This is here to annoy you.</label>
-        </>
-      }
-    />
-  ))
+const defaultArgs = {}
+
+export default {
+  title: 'Components/NavCookieBanner',
+  component: NavCookieBanner
+} as Meta
+
+const Template: Story<ComponentProps<typeof NavCookieBanner>> = args => (
+  <NavCookieBanner {...args} />
+)
+
+export const Basic = Template.bind({})
+Basic.args = { ...defaultArgs }
+
+export const CustomMessage = Template.bind({})
+CustomMessage.args = {
+  ...defaultArgs,
+  message: <label>This is here to annoy you.</label>
+}
