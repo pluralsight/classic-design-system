332b07424f0df5299e4d53fa758b911f43ac6e85
Dane Thurber <dane.thurber@gmail.com>
2021-03-23 16:08:10 -0600

    refactor(halo): convert stories to csf, add jest-axe tests

--- packages/halo/src/react/__specs__/index.spec.tsx
+++ packages/halo/src/react/__specs__/index.spec.tsx
@@ -1,11 +1,16 @@
+import { convertStoriesToJestCases } from '@pluralsight/ps-design-system-util'
 import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
 import React from 'react'
 
+import Halo from '..'
 import * as vars from '../../vars'
 
-import Halo from '..'
+import * as stories from '../__stories__/index.story'
 
 describe('Halo', () => {
+  const cases = convertStoriesToJestCases(stories)
+
   describe('.gapSizes', () => {
     it('exists', () => {
       expect(Halo.gapSizes).toEqual(vars.gapSizes)
@@ -18,8 +23,18 @@ describe('Halo', () => {
     })
   })
 
-  it('renders children', () => {
-    const { container } = render(<Halo>child</Halo>)
-    expect(container).toHaveTextContent('child')
+  it('forwards ref', () => {
+    const ref = React.createRef<HTMLDivElement>()
+    render(<Halo ref={ref} />)
+    expect(ref).not.toBeNull()
+  })
+
+  describe.each(cases)('%s story', (_name, Story) => {
+    it('has no axe-core violations', async () => {
+      const { container } = render(<Story {...Story.args} />)
+      const results = await axe(container)
+
+      expect(results).toHaveNoViolations()
+    })
   })
 })
--- packages/halo/src/react/__stories__/focusable.tsx
+++ packages/halo/src/react/__stories__/focusable.tsx
@@ -9,8 +9,9 @@ import * as vars from '../../vars'
 
 const stylesheet: { [selector: string]: Record<string, unknown> } = {
   '.focusable': {
-    background: core.colorsBackgroundLight[1],
+    background: core.colorsBackgroundDark[3],
     borderRadius: '2px',
+    color: core.colorsTextIcon.highOnDark,
     cursor: 'pointer',
     fontSize: core.type.fontSize200,
     fontWeight: core.type.fontWeightDefault,
@@ -24,7 +25,7 @@ const stylesheet: { [selector: string]: Record<string, unknown> } = {
   '.focusable--theme-light': {
     background: core.colorsBackgroundLight[1],
     borderColor: core.colorsBorder.highOnLight,
-    color: core.colorsWhite
+    color: core.colorsTextIcon.highOnLight
   },
   '.focusable--shape-pill': {
     borderRadius: '1000px'
@@ -45,7 +46,7 @@ interface FocusableProps {
 }
 const Focusable: React.FC<FocusableProps> = props => {
   const themeName = useTheme()
-  const { shape, children = 'focusable' } = props
+  const { shape, children = 'focusable area' } = props
 
   return (
     <div {...styles.focusable(themeName, { shape })} tabIndex={0}>
