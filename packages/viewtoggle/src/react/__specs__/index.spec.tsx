71f4334ff88539b80a4a1a7f9d432471011b1101
Dane Thurber <dane.thurber@gmail.com>
2021-03-23 15:24:41 -0600

    refactor(viewtoggle): convert stories to csf

--- packages/viewtoggle/src/react/__specs__/index.spec.tsx
+++ packages/viewtoggle/src/react/__specs__/index.spec.tsx
@@ -1,34 +1,37 @@
-import React from 'react'
+import { convertStoriesToJestCases } from '@pluralsight/ps-design-system-util'
 import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
+import React from 'react'
 
 import ViewToggle from '..'
 
+import * as stories from '../__stories__/index.story'
+
 describe('ViewToggle', () => {
+  const cases = convertStoriesToJestCases(stories)
+
+  it('forwards ref', () => {
+    const ref = React.createRef<HTMLDivElement>()
+    render(<ViewToggle ref={ref} />)
+    expect(ref).not.toBeNull()
+  })
+
   describe('.Option component', () => {
     it('exists', () => expect(ViewToggle.Option).toBeDefined())
+
+    it('forwards ref', () => {
+      const ref = React.createRef<HTMLButtonElement>()
+      render(<ViewToggle.Option ref={ref} />)
+      expect(ref).not.toBeNull()
+    })
   })
 
-  it('renders', () => {
-    const { getByTestId } = render(
-      <ViewToggle data-testid="mock-component">
-        <ViewToggle.Option>The Only Option</ViewToggle.Option>
-      </ViewToggle>
-    )
+  describe.skip.each(cases)('%s story', (_name, Story) => {
+    it('has no axe-core violations', async () => {
+      const { container } = render(<Story {...Story.args} />)
+      const results = await axe(container)
 
-    expect(getByTestId('mock-component')).toBeInTheDocument()
-  })
-
-  it('fowards refs', () => {
-    const toggleRef = React.createRef<HTMLDivElement>()
-    const optionRef = React.createRef<HTMLButtonElement>()
-
-    render(
-      <ViewToggle ref={toggleRef}>
-        <ViewToggle.Option ref={optionRef}>The Only Option</ViewToggle.Option>
-      </ViewToggle>
-    )
-
-    expect(toggleRef.current).not.toBeNull()
-    expect(optionRef.current).not.toBeNull()
+      expect(results).toHaveNoViolations()
+    })
   })
 })
