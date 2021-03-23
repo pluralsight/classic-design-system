802a4ad765ece3f6fdcc2044fab595a57c595be1
jaketrent <trent.jake@gmail.com>
2021-03-23 15:22:14 -0600

    test(banner): add axe-core test

--- packages/banner/src/react/__specs__/index.spec.tsx
+++ packages/banner/src/react/__specs__/index.spec.tsx
@@ -1,15 +1,28 @@
+import { convertStoriesToJestCases } from '@pluralsight/ps-design-system-util'
 import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
 import React from 'react'
 
-import Banner from '..'
+import Banner from '../index'
+import * as stories from '../__stories__/index.story'
 
 describe('Banner', () => {
+  const cases = convertStoriesToJestCases(stories)
+
   it('renders', () => {
     const { getByTestId } = render(
       <Banner data-testid="undertest">text</Banner>
     )
 
     expect(getByTestId('undertest')).toBeInTheDocument()
+  })
+
+  describe.each(cases)('% story', (_name, Story) => {
+    it('has no axe-core violations', async () => {
+      const { container } = render(<Story {...Story.args} />)
+      const results = await axe(container)
+      expect(results).toHaveNoViolations()
+    })
   })
 
   it('forwards refs', () => {
