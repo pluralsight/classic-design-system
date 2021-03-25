2f7c1127638eb4a36afeab982969abe0b010aee6
Dane Thurber <dane.thurber@gmail.com>
2021-03-25 14:10:29 -0600

    refactor(textinput): convert stories to csf. add jest-axe tests

--- packages/textinput/src/react/__specs__/index.spec.tsx
+++ packages/textinput/src/react/__specs__/index.spec.tsx
@@ -1,34 +1,59 @@
+import { convertStoriesToJestCases } from '@pluralsight/ps-design-system-util'
 import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
 import React from 'react'
 
 import TextInput from '..'
+import * as vars from '../../vars'
+
+import * as stories from '../__stories__/index.story'
 
 describe('TextInput', () => {
-  it('renders', () => {
-    const { getByTestId } = render(<TextInput data-testid="undertest" />)
-    expect(getByTestId('undertest')).toBeInTheDocument()
-  })
+  const cases = convertStoriesToJestCases(stories)
 
-  describe('with a single ref', () => {
-    it('forwards a single ref to the input element', () => {
-      const ref = React.createRef<HTMLInputElement>()
-
-      const { container } = render(<TextInput ref={ref} />)
-      const inputEl = container.querySelector('input')
-
-      expect(ref.current).not.toBeNull()
-      expect(inputEl).toEqual(ref.current)
+  describe('.appearances', () => {
+    it('exists', () => {
+      expect(TextInput.appearances).toEqual(vars.appearances)
     })
   })
 
-  describe('with multiple refs', () => {
-    it('forwards the input ref to the input element', () => {
-      const inputRef = React.createRef<HTMLInputElement>()
+  describe('.iconAligns', () => {
+    it('exists', () => {
+      expect(TextInput.iconAligns).toEqual(vars.iconAligns)
+    })
+  })
 
-      const { container } = render(<TextInput ref={inputRef} />)
+  describe('.sizes', () => {
+    it('exists', () => {
+      expect(TextInput.sizes).toEqual(vars.sizes)
+    })
+  })
 
-      const inputEl = container.querySelector('input')
-      expect(inputEl).toEqual(inputRef.current)
+  it('forwards the ref', () => {
+    const ref = React.createRef<HTMLInputElement>()
+    render(<TextInput ref={ref} />)
+    expect(ref).not.toBeNull()
+  })
+
+  describe.each(cases)('%s story', (_name, Story) => {
+    it('has no axe-core violations', async () => {
+      const { container } = render(<Story {...Story.args} />)
+      const results = await axe(container)
+
+      expect(results).toHaveNoViolations()
+    })
+  })
+
+  describe('Basic story', () => {
+    const { Basic } = stories
+
+    it('forwards className', () => {
+      const { getByTestId } = render(
+        <Basic data-testid="undertest" className="testclass" {...Basic.args} />
+      )
+
+      const el = getByTestId('undertest')
+      expect(el).toHaveClass('testclass')
     })
   })
 })
