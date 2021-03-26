a043560d8f7104e2c0150680b645122bad791fc9
jaketrent <trent.jake@gmail.com>
2021-03-26 10:12:34 -0600

    test: provide regeneratorRuntime to jest

--- jest/babel.config.cjs
+++ jest/babel.config.cjs
@@ -3,5 +3,6 @@ module.exports = {
     '@babel/preset-react',
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript'
-  ]
+  ],
+  plugins: ['@babel/plugin-transform-runtime']
 }
