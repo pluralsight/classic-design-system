8a7a19ace60f54781734be608d5ac9ad090601b0
jaketrent <trent.jake@gmail.com>
2021-03-30 09:18:30 -0600

    refactor(screenreaderonly): convert to esm
    BREAKING CHANGE: Drop cjs; esm only; all in on tree shaking
    BREAKING CHANGE: Remove file imports (eg, packageName/react)

--- packages/screenreaderonly/src/react/index.tsx
+++ packages/screenreaderonly/src/react/index.tsx
@@ -1,14 +1,14 @@
 import { accessibility } from '@pluralsight/ps-design-system-core'
 import { HTMLPropsFor } from '@pluralsight/ps-design-system-util'
 import { css } from 'glamor'
-import React, { ReactNode, forwardRef } from 'react'
+import React from 'react'
 
 interface Props extends HTMLPropsFor<'div'> {
-  children?: ReactNode
+  children?: React.ReactNode
   as?: React.ElementType | keyof JSX.IntrinsicElements
 }
 
-const ScreenReaderOnly = forwardRef<HTMLElement, Props>((props, ref) => {
+const ScreenReaderOnly = React.forwardRef<HTMLElement, Props>((props, ref) => {
   const { as: Tag = 'div', ...rest } = props
 
   return <Tag ref={ref} {...css(accessibility.screenReaderOnly)} {...rest} />
