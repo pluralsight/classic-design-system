ac62182dfa3bd5e5583a2516b08309c901e2be1c
jaketrent <trent.jake@gmail.com>
2021-04-05 15:38:02 -0600

    refactor(breadcrumb): import glamor cjs default and named exports
    When building and testing locally:
    
    import * as glamor from 'glamor
    
    worked. But when taking that ESM building installing to be run
    elsewhere, in a kitchen sink, app:
    
    import glamor from 'glamor'
    
    worked, but then DS building and testing failed.
    
    This combines those approaches to work in both environments.
    
    Related: https://github.com/babel/babel/issues/7294

--- packages/breadcrumb/src/react/index.tsx
+++ packages/breadcrumb/src/react/index.tsx
@@ -1,10 +1,12 @@
 import Button from '@pluralsight/ps-design-system-button'
 import { CaretLeftIcon } from '@pluralsight/ps-design-system-icon'
 import { RefForwardingComponent } from '@pluralsight/ps-design-system-util'
-import glamor from 'glamor'
+import glamorDefault, * as glamorExports from 'glamor'
 import React from 'react'
 
 import stylesheet from '../css/index'
+
+const glamor = glamorDefault || glamorExports
 
 const styles = {
   breadcrumb: () => glamor.css(stylesheet['.psds-breadcrumb'])
