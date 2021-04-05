bfe3d5ce2429616ef99b72298ca68d08f808e4d0
jaketrent <trent.jake@gmail.com>
2021-04-05 16:53:19 -0600

    refactor(screenreaderonly): import glamor cjs default and named exports
    When building and testing locally:
    
    import * as glamor from 'glamor
    
    worked. But when taking that ESM building installing to be run
    elsewhere, in a kitchen sink, app:
    
    import glamor from 'glamor'
    
    worked, but then DS building and testing failed.
    
    This combines those approaches to work in both environments.
    
    Related: https://github.com/babel/babel/issues/7294

--- packages/screenreaderonly/src/react/index.tsx
+++ packages/screenreaderonly/src/react/index.tsx
@@ -1,12 +1,14 @@
 import { accessibility } from '@pluralsight/ps-design-system-core'
 import { HTMLPropsFor } from '@pluralsight/ps-design-system-util'
-import glamor from 'glamor'
+import glamorDefault, * as glamorExports from 'glamor'
 import React from 'react'
 
 interface Props extends HTMLPropsFor<'div'> {
   children?: React.ReactNode
   as?: React.ElementType | keyof JSX.IntrinsicElements
 }
+
+const glamor = glamorDefault || glamorExports
 
 const ScreenReaderOnly = React.forwardRef<HTMLElement, Props>((props, ref) => {
   const { as: Tag = 'div', ...rest } = props
