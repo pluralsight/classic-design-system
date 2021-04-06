c22cd939ce0442ba805d816343d7cfd619552a54
jaketrent <trent.jake@gmail.com>
2021-04-06 11:16:10 -0600

    refactor(typeahead): import glamor cjs default and named exports
    When building and testing locally:
    
    import * as glamor from 'glamor
    
    worked. But when taking that ESM building installing to be run
    elsewhere, in a kitchen sink, app:
    
    import glamor from 'glamor'
    
    worked, but then DS building and testing failed.
    
    This combines those approaches to work in both environments.
    
    Related: https://github.com/babel/babel/issues/7294

--- packages/typeahead/src/react/menu.tsx
+++ packages/typeahead/src/react/menu.tsx
@@ -1,10 +1,12 @@
 import { CheckIcon } from '@pluralsight/ps-design-system-icon'
 import { HTMLPropsFor, combineFns } from '@pluralsight/ps-design-system-util'
-import glamor from 'glamor'
+import glamorDefault, * as glamorExports from 'glamor'
 import React from 'react'
 
 import stylesheet from '../css/index'
 import { SuggestionOption } from './suggestion'
+
+const glamor = glamorDefault || glamorExports
 
 const styles = {
   menu: () => glamor.css(stylesheet['.psds-typeahead__menu']),
