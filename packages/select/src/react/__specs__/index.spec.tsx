365e5b8562b837051d58264bbfa88fc7786998a1
Dane Thurber <dane.thurber@gmail.com>
2021-04-08 12:34:09 -0600

    refactor(select): convert stories to csf. add jest-axe tests

--- packages/select/src/react/__specs__/index.spec.tsx
+++ packages/select/src/react/__specs__/index.spec.tsx
@@ -1,213 +1,250 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
-import { fireEvent, screen } from '@testing-library/dom'
+import { convertStoriesToJestCases } from '@pluralsight/ps-design-system-util'
+import { fireEvent, screen, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
 import React from 'react'
 
 import Select from '..'
-const items = [
-  { id: 'One item', name: 'One item' },
-  { id: 'Two item', name: 'Two item' },
-  { id: 'Three item', name: 'Three item' }
-]
-it('button click opens the menu', async () => {
-  render(<Select items={items} placeholder={'Select'} />)
-  const button = screen.getByRole('button', { name: /Select/ })
-  userEvent.click(button)
-  const menu = await screen.findByRole('listbox')
-  expect(menu).toBeInTheDocument()
-})
+import * as vars from '../../vars'
 
-it('disabled click doesnt open menu', () => {
-  render(<Select items={items} placeholder={'Select'} disabled />)
-  const button = screen.getByRole('button', { name: /Select/ })
-  userEvent.click(button)
-  const menu = screen.queryByRole('listbox')
-  expect(menu).not.toBeInTheDocument()
-})
+import * as stories from '../__stories__/index.story'
 
-it('opens the menu with space key', async () => {
-  render(<Select items={items} placeholder={'Select'} />)
-
-  const button = screen.getByRole('button', { name: /Select/ })
-  button.focus()
-  spaceKey()
-
-  const menu = await screen.findByRole('listbox')
-  expect(menu).toBeInTheDocument()
-})
-
-it('opens the menu with enter key', async () => {
-  render(<Select items={items} placeholder={'Select'} />)
-
-  const button = screen.getByRole('button', { name: /Select/ })
-  button.focus()
-  enterKey()
-
-  const menu = await screen.findByRole('listbox')
-  expect(menu).toBeInTheDocument()
-})
-
-it('opens the menu with down arrow', async () => {
-  render(<Select items={items} placeholder={'Select'} />)
-
-  const button = screen.getByRole('button', { name: /Select/ })
-  button.focus()
-  arrowDownKey()
-
-  const menu = await screen.findByRole('listbox')
-  expect(menu).toBeInTheDocument()
-})
-
-it('closes the menu with escape', async () => {
-  render(<Select items={items} placeholder={'Select'} />)
-
-  const button = screen.getByRole('button', { name: /Select/ })
-  button.focus()
-  arrowDownKey()
-
-  let menu
-  menu = await screen.findByRole('listbox')
-  expect(menu).toBeInTheDocument()
-
-  escapeKey()
-  menu = screen.queryByRole('listbox')
-  expect(menu).not.toBeInTheDocument()
-})
-
-it('selects by click', async () => {
-  render(<Select items={items} placeholder={'Select'} />)
-  const button = screen.getByRole('button', { name: /Select/ })
-  userEvent.click(button)
-
-  const item = await screen.findByRole('option', { name: /Two item/ })
-  userEvent.click(item)
-
-  expect(button).toHaveTextContent('Two item')
-})
-
-it('navigates by arrows, selects by enter', async () => {
-  render(<Select items={items} placeholder={'Select'} />)
-  const button = screen.getByRole('button', { name: /Select/ })
-  button.focus()
-  enterKey()
-  await screen.findByRole('listbox')
-  arrowDownKey()
-  arrowUpKey()
-  arrowUpKey()
-  enterKey()
-
-  expect(button).toHaveTextContent('Three item')
-})
-
-it('re-navigates, active index is maintained on selection', async () => {
-  render(<Select items={items} placeholder={'Select'} />)
-  const button = screen.getByRole('button', { name: /Select/ })
-  button.focus()
-  enterKey()
-  await screen.findByRole('listbox')
-  arrowDownKey()
-  arrowUpKey()
-  arrowUpKey()
-  enterKey()
-
-  expect(button).toHaveTextContent('Three item')
-
-  arrowDownKey()
-  await screen.findByRole('listbox')
-  arrowDownKey()
-  enterKey()
-
-  expect(button).toHaveTextContent('One item')
-})
-
-it('select button onClick is triggered', async () => {
-  const onClick = jest.fn()
-  render(<Select items={items} placeholder={'Select'} onClick={onClick} />)
-  const button = screen.getByRole('button', { name: /Select/ })
-  userEvent.click(button)
-
-  expect(onClick).toHaveBeenCalledTimes(1)
-
-  const item = await screen.findByRole('option', { name: /One item/ })
-  userEvent.click(item)
-
-  expect(onClick).toHaveBeenCalledTimes(1)
-})
-
-it('calls on change with click on label text', async () => {
-  const onChange = jest.fn()
-  render(<Select items={items} placeholder={'Select'} onChange={onChange} />)
-  const button = screen.getByRole('button', { name: /Select/ })
-  userEvent.click(button)
-
-  const item = await screen.findByRole('option', { name: /Three/ })
-  userEvent.click(item)
-
-  expect(onChange).toHaveBeenCalledWith(expect.any(Object), {
-    id: 'Three item',
-    name: 'Three item'
-  })
-})
-
-it('calls on change with keydown on value', async () => {
-  const onChange = jest.fn()
-  render(<Select items={items} placeholder={'Select'} onChange={onChange} />)
-  const button = screen.getByRole('button', { name: /Select/ })
-  button.focus()
-  enterKey()
-  await screen.findByRole('listbox')
-  arrowDownKey()
-  enterKey()
-
-  expect(onChange).toHaveBeenCalledWith(expect.any(Object), {
-    id: 'Two item',
-    name: 'Two item'
-  })
-})
-
-it('should have a pre-selected value', async () => {
-  render(
-    <Select
-      items={items}
-      value={{
-        id: 'Two item',
-        name: 'Two item'
-      }}
-    />
-  )
-  const button = screen.getByRole('button', { name: /Two item/ })
-  button.focus()
-  enterKey()
-  const item = await screen.findByRole('option', { selected: true })
-  expect(item).toHaveTextContent(/Two item/)
-})
-
-function enterKey() {
-  fireEvent.keyDown(document.activeElement!, {
-    key: 'Enter',
-    code: 'Enter'
-  })
+const pressArrowDown = (el = document?.activeElement) => {
+  fireEvent.keyDown(el as HTMLElement, { key: 'ArrowDown', code: 'ArrowDown' })
 }
-
-function spaceKey() {
-  fireEvent.keyDown(document.activeElement!, { key: ' ', code: 'Space' })
+const pressArrowUp = (el = document?.activeElement) => {
+  fireEvent.keyDown(el as HTMLElement, { key: 'ArrowUp', code: 'ArrowUp' })
 }
-
-function arrowDownKey() {
-  fireEvent.keyDown(document.activeElement!, {
-    key: 'ArrowDown',
-    code: 'ArrowDown'
-  })
+const pressEnter = (el = document?.activeElement) => {
+  fireEvent.keyDown(el as HTMLElement, { key: 'Enter', code: 'Enter' })
 }
-
-function arrowUpKey() {
-  fireEvent.keyDown(document.activeElement!, {
-    key: 'ArrowUp',
-    code: 'ArrowUp'
-  })
-}
-
-function escapeKey() {
+const pressEscape = () => {
   fireEvent.keyDown(document, { key: 'Escape', code: 'Escape' })
 }
+const pressSpace = (el = document?.activeElement) => {
+  fireEvent.keyDown(el as HTMLElement, { key: ' ', code: 'Space' })
+}
+
+describe('Select', () => {
+  const cases = convertStoriesToJestCases(stories)
+
+  it('forwards ref', () => {
+    const ref = React.createRef<HTMLButtonElement>()
+    render(<Select ref={ref} />)
+    expect(ref).not.toBeNull()
+  })
+
+  describe('.Button component', () => {
+    it('exists', () => expect(Select.Button).toBeDefined())
+
+    it('forwards ref', () => {
+      const ref = React.createRef<HTMLButtonElement>()
+      render(<Select.Button ref={ref} />)
+      expect(ref).not.toBeNull()
+    })
+  })
+
+  describe('.Selected component', () => {
+    it('exists', () => expect(Select.Selected).toBeDefined())
+  })
+
+  describe.each(cases)('%s story', (_name, Story) => {
+    it('has no axe-core violations', async () => {
+      const { container } = render(<Story {...Story.args} />)
+      const results = await axe(container)
+
+      expect(results).toHaveNoViolations()
+    })
+  })
+
+  describe('.sizes', () => {
+    it('exists', () => {
+      expect(Select.sizes).toEqual(vars.sizes)
+    })
+  })
+
+  describe('Basic story', () => {
+    const { Basic } = stories
+
+    it('button click opens the menu', async () => {
+      render(<Basic {...(Basic.args as any)} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      userEvent.click(button)
+
+      const menu = await screen.findByRole('listbox')
+      expect(menu).toBeInTheDocument()
+    })
+
+    it('disabled click does NOT open the menu', () => {
+      render(<Basic {...(Basic.args as any)} disabled />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      userEvent.click(button)
+
+      const menu = screen.queryByRole('listbox')
+      expect(menu).not.toBeInTheDocument()
+    })
+
+    it('pressing Space opens the menu', async () => {
+      render(<Basic {...(Basic.args as any)} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      button.focus()
+
+      pressSpace()
+
+      const menu = await screen.findByRole('listbox')
+      expect(menu).toBeInTheDocument()
+    })
+
+    it('pressing Enter opens the menu', async () => {
+      render(<Basic {...(Basic.args as any)} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      button.focus()
+
+      pressEnter()
+
+      const menu = await screen.findByRole('listbox')
+      expect(menu).toBeInTheDocument()
+    })
+
+    it('pressing ArrowDown opens the menu', async () => {
+      render(<Basic {...(Basic.args as any)} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      button.focus()
+
+      pressArrowDown()
+
+      const menu = await screen.findByRole('listbox')
+      expect(menu).toBeInTheDocument()
+    })
+
+    it('pressing Escape closes the menu', async () => {
+      let menu
+      render(<Basic {...(Basic.args as any)} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      button.focus()
+
+      pressArrowDown()
+
+      menu = await screen.findByRole('listbox')
+      expect(menu).toBeInTheDocument()
+
+      pressEscape()
+
+      menu = screen.queryByRole('listbox')
+      expect(menu).not.toBeInTheDocument()
+    })
+
+    it('click selects an item', async () => {
+      render(<Basic {...(Basic.args as any)} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      userEvent.click(button)
+
+      const item = await screen.findByRole('option', { name: /Can edit/ })
+      userEvent.click(item)
+
+      expect(button).toHaveTextContent('Can edit')
+    })
+
+    it('navigates by arrows, selects by enter', async () => {
+      render(<Basic {...(Basic.args as any)} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      button.focus()
+      pressEnter()
+
+      await screen.findByRole('listbox')
+
+      pressArrowDown()
+      pressArrowUp()
+      pressArrowUp()
+      pressEnter()
+
+      expect(button).toHaveTextContent('Make Owner')
+    })
+
+    it('re-navigates, active index is maintained on selection', async () => {
+      render(<Basic {...(Basic.args as any)} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      button.focus()
+      pressEnter()
+
+      await screen.findByRole('listbox')
+
+      pressArrowDown()
+      pressArrowUp()
+      pressArrowUp()
+      pressEnter()
+
+      expect(button).toHaveTextContent('Make Owner')
+
+      button.focus()
+      pressEnter()
+
+      await screen.findByRole('listbox')
+
+      pressArrowDown()
+      pressArrowDown()
+      pressEnter()
+
+      expect(button).toHaveTextContent('Can view')
+    })
+
+    it('select button onClick is triggered', async () => {
+      const onClick = jest.fn()
+      render(<Basic {...(Basic.args as any)} onClick={onClick} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      userEvent.click(button)
+
+      expect(onClick).toHaveBeenCalledTimes(1)
+
+      const item = await screen.findByRole('option', { name: /Can edit/ })
+      userEvent.click(item)
+
+      expect(onClick).toHaveBeenCalledTimes(1)
+    })
+
+    it('calls on change with click on label text', async () => {
+      const onChange = jest.fn()
+      render(<Basic {...(Basic.args as any)} onChange={onChange} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      userEvent.click(button)
+
+      const item = await screen.findByRole('option', { name: /Can edit/ })
+      userEvent.click(item)
+
+      expect(onChange).toHaveBeenCalledWith(expect.any(Object), {
+        id: 'Can edit',
+        name: 'Can edit'
+      })
+    })
+
+    it('calls on change with keydown on value', async () => {
+      const onChange = jest.fn()
+      render(<Basic {...(Basic.args as any)} onChange={onChange} />)
+
+      const button = screen.getByRole('button', { name: /Select/ })
+      button.focus()
+      pressEnter()
+
+      await screen.findByRole('listbox')
+
+      pressArrowDown()
+      pressEnter()
+
+      expect(onChange).toHaveBeenCalledWith(expect.any(Object), {
+        id: 'Can view',
+        name: 'Can view'
+      })
+    })
+  })
+})
--- packages/select/src/react/__stories__/index.story.tsx
+++ packages/select/src/react/__stories__/index.story.tsx
@@ -1,128 +1,170 @@
-import { storiesOf } from '@storybook/react'
-import React, { useRef } from 'react'
-import { action } from '@storybook/addon-actions'
-
-import Select from '..'
+import { colorsStatus } from '@pluralsight/ps-design-system-core'
 import Menu, {
   MenuItemWithDescription
 } from '@pluralsight/ps-design-system-menu'
-import { colorsStatus } from '@pluralsight/ps-design-system-core'
-import { RefFor } from '@pluralsight/ps-design-system-util'
+import { HTMLPropsFor, ValueOf } from '@pluralsight/ps-design-system-util'
+import { action } from '@storybook/addon-actions'
+import { Meta, Story } from '@storybook/react/types-6-0'
 
-storiesOf('button', module)
-  .add('Select.Button', () => <Select.Button>Hello</Select.Button>)
-  .add('Select.Button: focused', () => {
-    const ref = useRef<HTMLButtonElement>()
-    ref.current && ref.current.focus()
-    return <Select.Button ref={ref as RefFor<'button'>}>Hello</Select.Button>
-  })
-  .add('Select.Button: erred', () => <Select.Button error>Hello</Select.Button>)
-  .add('Select.Button: disabled', () => (
-    <Select.Button disabled>Hello</Select.Button>
-  ))
-  .add('Select.Button: isOpen', () => (
-    <Select.Button isOpen>Hello</Select.Button>
-  ))
-  .add('Select.Button: size', () => (
-    <Select.Button size="small">Hello</Select.Button>
-  ))
-storiesOf('selected', module)
-  .add('Select.Selected: placeholder', () => (
-    <Select.Selected
-      selectedItem={{ name: 'placeholder', id: 'placeholder' }}
-      placeholder="placeholder"
-    />
-  ))
-  .add('Select.Selected: selected', () => (
-    <Select.Selected
-      selectedItem={{ name: 'selected', id: 'selected' }}
-      placeholder="placeholder"
-    />
-  ))
+import React from 'react'
 
-const items = [
-  { id: 'One item', name: 'One item' },
-  { id: 'Two item', name: 'Two item' },
-  { id: 'Three item', name: 'Three item' }
-]
-const itemsWithDescription = [
-  {
-    id: 'Can view',
-    name: 'Can view',
-    description: 'View details, content and other members in the channel.'
-  },
-  {
-    id: 'Can edit',
-    name: 'Can edit',
-    description:
-      'Edit details, add or remove content and invite or remove members'
-  },
-  {
-    id: 'Make Owner',
-    name: 'Make Owner',
-    description:
-      'Edit details, add or remove content, invite or remove members and delete Channel'
-  }
-]
+import Select from '..'
 
-const handleClick = (_e: unknown, value: unknown) => {
-  action('remove click')(value)
+export default {
+  title: 'Components/Select'
+} as Meta
+
+interface StoryArgs {
+  onChange?: any
+  onClick?: any
+  items: {
+    description: React.ReactText
+    id: React.ReactText
+    name: React.ReactText
+  }[]
+  placeholder: string
+  position: ValueOf<typeof Select.positions>
 }
 
-storiesOf('dropdown', module)
-  .add('custom item: static manual', () => {
-    return (
-      <Select
-        position={Select.positions.belowRight}
-        value={{
-          name: itemsWithDescription[0].name,
-          id: itemsWithDescription[0].id
-        }}
+const defaultArgs: StoryArgs = {
+  items: [
+    {
+      description: 'View details, content and other members in the channel.',
+      id: 'Can view',
+      name: 'Can view'
+    },
+    {
+      description:
+        'Edit details, add or remove content and invite or remove members',
+      id: 'Can edit',
+      name: 'Can edit'
+    },
+    {
+      description:
+        'Edit details, add or remove content, invite or remove members',
+      id: 'Make Owner',
+      name: 'Make Owner'
+    }
+  ],
+  onClick: action('on click'),
+  placeholder: 'Select item',
+  position: Select.positions.belowLeft
+}
+
+const Template: Story<StoryArgs> = args => {
+  const { items } = args
+  const [selected] = React.useState()
+
+  const handleClick = (_evt: React.MouseEvent, nextValue: unknown) => {
+    action('remove click')(nextValue)
+  }
+
+  return (
+    <Select
+      onChange={args.onChange}
+      onClick={args.onClick}
+      placeholder={args.placeholder}
+      position={args.position}
+      value={selected}
+    >
+      {items.map(item => (
+        <MenuItemWithDescription {...item} key={item.id} />
+      ))}
+
+      <Menu.Divider />
+
+      <Menu.Item
+        onClick={handleClick}
+        style={{ color: colorsStatus.error }}
+        value={selected}
       >
-        {itemsWithDescription.map(i => (
-          <MenuItemWithDescription {...i} key={i.id} />
-        ))}
-        <Menu.Divider />
-        <Menu.Item
-          onClick={handleClick}
-          style={{ color: colorsStatus.error }}
-          value={{
-            name: itemsWithDescription[0].name,
-            id: itemsWithDescription[0].id
-          }}
-        >
-          Remove Member
-        </Menu.Item>
-      </Select>
-    )
-  })
-  .add('custom item: dynamic', () => {
-    return (
-      <Select
-        items={itemsWithDescription}
-        position={Select.positions.belowRight}
-        renderOption={(MenuItemWithDescription as unknown) as React.FC}
-        value={{
-          name: itemsWithDescription[0].name,
-          id: itemsWithDescription[0].id
-        }}
-      />
-    )
-  })
-  .add('BelowLeft: placholder', () => (
+        Remove Member
+      </Menu.Item>
+    </Select>
+  )
+}
+
+export const Basic = Template.bind({})
+Basic.args = { ...defaultArgs }
+
+export const PositionBelowLeft = Template.bind({})
+PositionBelowLeft.args = {
+  ...defaultArgs,
+  position: Select.positions.belowRight
+}
+
+export const CustomRenderOption: Story<StoryArgs> = args => {
+  const { items, placeholder, position } = args
+  const [selected] = React.useState()
+
+  return (
     <Select
-      position={Select.positions.belowLeft}
       items={items}
-      placeholder={'Select item'}
+      placeholder={placeholder}
+      position={position}
+      renderOption={(MenuItemWithDescription as unknown) as React.FC}
+      value={selected}
     />
-  ))
-  .add('BelowRight: selected value', () => (
-    <Select
-      position={Select.positions.belowRight}
-      items={items}
-      value={{
-        name: items[1].name,
-        id: items[1].id
-      }}
-    />
-  ))
+  )
+}
+CustomRenderOption.args = { ...defaultArgs }
+
+const defaultButtonArgs = { children: 'Hello' }
+
+const ButtonTemplate: Story<
+  React.ComponentProps<typeof Select.Button>
+> = args => {
+  const { children, ...rest } = args
+  return <Select.Button {...rest}>{children}</Select.Button>
+}
+
+export const ButtonBasic = ButtonTemplate.bind({})
+ButtonBasic.args = { ...defaultButtonArgs }
+
+export const ButtonIsOpen = ButtonTemplate.bind({})
+ButtonIsOpen.args = { ...defaultButtonArgs, isOpen: true }
+
+export const ButtonDisabled = ButtonTemplate.bind({})
+ButtonDisabled.args = { ...defaultButtonArgs, disabled: true }
+
+export const ButtonError = ButtonTemplate.bind({})
+ButtonError.args = { ...defaultButtonArgs, error: true }
+
+export const ButtonSmall = ButtonTemplate.bind({})
+ButtonSmall.args = { ...defaultButtonArgs, size: 'small' }
+
+export const ButtonFocused: Story<
+  React.ComponentProps<typeof Select.Button>
+> = args => {
+  const { children, ...rest } = args
+  const ref = React.useRef<HTMLButtonElement>(null)
+
+  React.useEffect(() => {
+    if (ref.current) ref.current.focus()
+  }, [])
+
+  return (
+    <Select.Button ref={ref} {...rest}>
+      {children}
+    </Select.Button>
+  )
+}
+ButtonFocused.args = { ...defaultButtonArgs }
+
+const defaultSelectedArgs = {
+  placeholder: 'placeholder',
+  selectedItem: { name: 'placeholder', id: 'placeholder' }
+}
+
+const SelectedTemplate: Story<
+  React.ComponentProps<typeof Select.Selected>
+> = args => <Select.Selected {...args} />
+
+export const SelectedPlaceholder = SelectedTemplate.bind({})
+SelectedPlaceholder.args = { ...defaultSelectedArgs }
+
+export const SelectedSelected = SelectedTemplate.bind({})
+SelectedSelected.args = {
+  ...defaultSelectedArgs,
+  selectedItem: { name: 'selected', id: 'selected' }
+}
