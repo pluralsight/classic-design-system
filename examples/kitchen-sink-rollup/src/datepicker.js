20aa015f7fba239f027265bde1b4d432814d9b1f
jaketrent <trent.jake@gmail.com>
2021-04-13 11:22:25 -0600

    feat(examples): add kitchen sink rollup, works with esm

---
+++ examples/kitchen-sink-rollup/src/datepicker.js
@@ -0,0 +1,63 @@
+import * as Dayzed from 'dayzed'
+import { CalendarIcon } from '@pluralsight/ps-design-system-icon'
+import TextInput from '@pluralsight/ps-design-system-textinput'
+import {
+  Calendar,
+  CalendarDates,
+  useDateSelectChange
+} from '@pluralsight/ps-design-system-datepicker'
+import React from 'react'
+
+const Example = () => {
+  const [selected, setSelected] = React.useState()
+  const [open, setOpen] = React.useState(false)
+  const onDateSelected = (dateObj, evt) => {
+    setSelected(dateObj.date)
+    setOpen(false)
+  }
+  const { getDateProps, ...dayzedData } = Dayzed.useDayzed({
+    date: selected || new Date('05/30/2020'),
+    selected,
+    onDateSelected
+  })
+  const handleIconClick = evt => {
+    setOpen(!open)
+  }
+  const [slide, setSlide] = React.useState()
+  const [value, onChange] = useDateSelectChange({
+    selected,
+    setSlide,
+    setSelected
+  })
+  return (
+    <div style={{ display: 'inline-block', position: 'relative' }}>
+      <TextInput
+        onChange={onChange}
+        value={value}
+        placeholder="mm/dd/yyyy"
+        icon={
+          <CalendarIcon
+            onClick={handleIconClick}
+            style={{ cursor: 'pointer' }}
+          />
+        }
+      />
+      <br />
+      {open && (
+        <Calendar
+          {...dayzedData}
+          style={{ position: 'absolute', zIndex: 1, marginTop: 4 }}
+          slide={slide}
+        >
+          <CalendarDates getDateProps={getDateProps}>
+            {renderProps => {
+              return <button {...renderProps} />
+            }}
+          </CalendarDates>
+        </Calendar>
+      )}
+    </div>
+  )
+}
+
+export default Example
