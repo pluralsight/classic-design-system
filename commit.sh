f49811559ee4a4c40a3c891c9ca6b5b1c415178b
jaketrent <trent.jake@gmail.com>
2021-04-14 18:26:02 -0600

    build: rm unused commit.sh file

--- commit.sh
+++
@@ -1,5 +0,0 @@
-for FILE in $(git diff --name-only); do
-  PACKAGE=`echo $FILE | sed "s|packages/\(.*\)/.*|\1|"`
-  git add "$FILE"
-  git commit -m "fix($PACKAGE): properly order the conditional exports in package"
-done
