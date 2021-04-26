051d38f9a77f5e276e7d996e6739dc47654f5852
Dane Thurber <dane.thurber@gmail.com>
2021-04-26 13:02:36 -0600

    test(tag): csf and jest-axe refactor

--- packages/tag/src/react/__stories__/index.story.tsx
+++ packages/tag/src/react/__stories__/index.story.tsx
@@ -1,88 +1,136 @@
+import { PlaceholderIcon } from '@pluralsight/ps-design-system-icon'
 import { action } from '@storybook/addon-actions'
-import * as Icon from '@pluralsight/ps-design-system-icon'
+import { Meta, Story } from '@storybook/react/types-6-0'
+import glamorDefault, * as glamorExports from 'glamor'
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 
 import Tag from '../index'
 
-const sizeStory = storiesOf('size', module)
-Object.values(Tag.sizes).forEach(size =>
-  sizeStory.add(size, () => <Tag size={size}>{size}</Tag>)
-)
+const glamor = glamorDefault || glamorExports
 
-const iconStory = storiesOf('icon', module)
-Object.values(Tag.sizes).forEach(size =>
-  iconStory.add(size, () => (
-    <Tag size={size} icon={<Icon.CloseIcon />}>
-      With Icon
-    </Tag>
-  ))
-)
+const defaultArgs = { children: 'hello world' }
 
-const PressedTag = React.forwardRef((props, _ref) => {
-  const [pressed, setPressed] = React.useState(true)
+export default {
+  title: 'Components/Tag',
+  component: Tag
+} as Meta
+
+const StoryGrid: React.FC<{ cols?: number }> = props => {
+  const { cols = 2, ...rest } = props
+
   return (
-    <Tag isPressed={pressed} {...props} onClick={() => setPressed(!pressed)} />
+    <div
+      {...glamor.css({
+        display: 'grid',
+        gap: '20px',
+        gridTemplateColumns: Array(cols).fill('1fr').join(' ')
+      })}
+      {...rest}
+    />
   )
-}) as typeof Tag
+}
 
-storiesOf('actions', module)
-  .add('link', () => <Tag href="https://duckduckgo.com/">As a link</Tag>)
-  .add('link with target', () => (
-    <Tag target="_blank" href="https://duckduckgo.com/">
-      Opens in new window
-    </Tag>
-  ))
-  .add('button', () => (
-    <Tag onClick={action('button clicked')}>As a button</Tag>
-  ))
-  .add('small button', () => (
-    <Tag size={Tag.sizes.small} onClick={action('button clicked')}>
-      As a button
-    </Tag>
-  ))
-  .add('just icon button', () => (
-    <Tag icon={<Icon.CloseIcon onClick={action('icon clicked')} />}>
-      As a button
-    </Tag>
-  ))
-  .add('button w/ icon button', () => (
-    <Tag
-      onClick={action('button clicked')}
-      icon={<Icon.CloseIcon onClick={action('icon clicked')} />}
-    >
-      As a button
-    </Tag>
-  ))
+const Template: Story<React.ComponentProps<typeof Tag>> = args => (
+  <Tag {...args} />
+)
 
-storiesOf('isPressed', module)
-  .add('link', () => (
-    <PressedTag href="https://duckduckgo.com/">As a link</PressedTag>
-  ))
-  .add('button', () => <PressedTag>As a button</PressedTag>)
-  .add('button w/ icon button', () => (
-    <PressedTag icon={<Icon.CloseIcon onClick={action('icon clicked')} />}>
-      As a button
-    </PressedTag>
-  ))
-  .add('without click/href', () => <Tag isPressed>no click/href</Tag>)
+export const Basic = Template.bind({})
+Basic.args = { ...defaultArgs }
 
-storiesOf('long', module)
-  .add('string', () => (
-    <Tag>
-      This is the string that goes on forever and always. There really isn't an
-      end. Wow.
-    </Tag>
-  ))
-  .add('with icon', () => (
-    <Tag icon={<Icon.CheckIcon />}>
-      This is the string that goes on forever and always. There really isn't an
-      end. Wow.
-    </Tag>
-  ))
+export const Button = Template.bind({})
+Button.args = { ...defaultArgs, onClick: action('click') }
 
-storiesOf('error', module).add('focusable', () => (
-  <Tag onClick={action('button click')} error>
-    I have a problem
-  </Tag>
-))
+export const SmallButton = Template.bind({})
+SmallButton.args = {
+  ...defaultArgs,
+  onClick: action('click'),
+  size: Tag.sizes.small
+}
+
+export const IconButton = Template.bind({})
+IconButton.args = {
+  ...defaultArgs,
+  onClick: action('click'),
+  icon: <PlaceholderIcon />
+}
+
+export const WithIconButton = Template.bind({})
+WithIconButton.args = {
+  ...defaultArgs,
+  onClick: action('click'),
+  icon: <PlaceholderIcon onClick={action('icon click')} />
+}
+
+export const Link = Template.bind({})
+Link.args = { ...defaultArgs, href: 'https://duckduckgo.com/' } as any
+
+export const Error = Template.bind({})
+Error.args = { ...defaultArgs, error: true }
+
+export const LongText = Template.bind({})
+LongText.args = {
+  ...defaultArgs,
+  children:
+    "This is the string that goes on forever and always. There really isn't an end. Wow."
+}
+
+export const LongTextWithIcon = Template.bind({})
+LongTextWithIcon.args = {
+  ...defaultArgs,
+  children:
+    "This is the string that goes on forever and always. There really isn't an end. Wow.",
+  icon: <PlaceholderIcon />
+}
+
+export const Pressed = Template.bind({})
+Pressed.args = { ...defaultArgs, isPressed: true }
+
+export const PressedButton = Template.bind({})
+PressedButton.args = {
+  ...defaultArgs,
+  isPressed: true,
+  onClick: action('click')
+}
+
+export const PressedLink = Template.bind({})
+PressedLink.args = {
+  ...defaultArgs,
+  href: '#',
+  isPressed: true
+} as any
+
+export const PressedWithIcon = Template.bind({})
+PressedWithIcon.args = {
+  ...defaultArgs,
+  isPressed: true,
+  icon: <PlaceholderIcon />
+}
+
+export const Sizes: Story = () => (
+  <StoryGrid>
+    {Object.values(Tag.sizes).map((size, i) => (
+      <Tag key={i} size={size}>
+        {size}
+      </Tag>
+    ))}
+  </StoryGrid>
+)
+
+export const SizesWithIcons: Story = () => (
+  <StoryGrid>
+    {Object.values(Tag.sizes).map((size, i) => (
+      <Tag key={i} icon={<PlaceholderIcon />} size={size}>
+        {size}
+      </Tag>
+    ))}
+  </StoryGrid>
+)
+
+export const StyleOverride = Template.bind({})
+StyleOverride.args = { ...defaultArgs, style: { background: 'red' } }
+
+export const ClassNameOverride = Template.bind({})
+ClassNameOverride.args = {
+  ...defaultArgs,
+  className: glamor.css({ background: 'green' }).toString()
+}
