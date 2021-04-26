61b0b11343131089197e8eea5e338cc11de7d4fd
Dane Thurber <dane.thurber@gmail.com>
2021-04-26 09:39:43 -0600

    refactor(text): convert stories to csf

--- packages/text/src/react/__stories__/p.story.tsx
+++ packages/text/src/react/__stories__/p.story.tsx
@@ -1,86 +1,78 @@
-import {
-  colorsPink,
-  layout,
-  colorsBlue
-} from '@pluralsight/ps-design-system-core'
-import { storiesOf } from '@storybook/react'
+import { colorsPink, colorsBlue } from '@pluralsight/ps-design-system-core'
+import { action } from '@storybook/addon-actions'
+import { Meta, Story } from '@storybook/react/types-6-0'
 import glamorDefault, * as glamorExports from 'glamor'
 import React from 'react'
 
-import P from '../p'
+import { PaddingDecorator, StoryGrid } from './shared'
+import { P } from '../index'
 
 const glamor = glamorDefault || glamorExports
 
-const className = glamor
-  .css({ color: `${colorsBlue[6]} !important` })
-  .toString()
+export default {
+  title: 'Components/Text/P',
+  component: P,
+  decorators: [PaddingDecorator]
+} as Meta
 
-const PaddingDecorator = (storyFn: () => React.ReactNode) => (
-  <div style={{ padding: layout.spacingXLarge }}>{storyFn()}</div>
-)
-
-const stories = storiesOf('P', module).addDecorator(PaddingDecorator)
-
-Object.keys(P.sizes).forEach(size =>
-  stories.add(`size: ${size}`, () => (
-    <P size={size as keyof typeof P.sizes}>
+const defaultArgs = {
+  children: (
+    <>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam
       pharetra arcu in commodo. Cras faucibus ex id ligula aliquam, eget porta
       tortor efficitur. Duis eget ultrices ligula. Pellentesque laoreet massa
       interdum, venenatis sem pretium, efficitur enim. Vestibulum id nisi a
       massa tincidunt malesuada vitae non risus. Sed eget convallis libero. Nam
       ac libero luctus, euismod lectus eget, cursus nibh.
-    </P>
-  ))
+    </>
+  ),
+  onClick: action('click')
+}
+
+const Template: Story<React.ComponentProps<typeof P>> = args => <P {...args} />
+
+export const Basic = Template.bind({})
+Basic.args = { ...defaultArgs }
+
+export const WithEm = Template.bind({})
+WithEm.args = {
+  ...defaultArgs,
+  children: (
+    <>
+      Cras faucibus ex id ligula aliquam, eget porta
+      <em>this is em</em> tortor efficitur. Duis eget ultrices ligula.
+      Pellentesque laoreet massa interdum. Vestibulum id nisi a massa
+      <em>THIS IS EM</em> tincidunt malesuada vitae non risus. cursus nibh.
+    </>
+  )
+}
+
+export const Colors: Story<React.ComponentProps<typeof P>> = args => (
+  <StoryGrid cols={1}>
+    {Object.values(P.colors).map((color, i) => (
+      <P {...args} key={i} color={color} />
+    ))}
+  </StoryGrid>
 )
-stories.add('em', () => (
-  <P color="secondary">
-    Cras faucibus ex id ligula aliquam, eget porta
-    <em>this is em</em> tortor efficitur. Duis eget ultrices ligula.
-    Pellentesque laoreet massa interdum. Vestibulum id nisi a massa{' '}
-    <em>THIS IS EM</em> tincidunt malesuada vitae non risus. cursus nibh.
-  </P>
-))
+Colors.args = { ...defaultArgs }
 
-stories.add('color: primary', () => (
-  <P color="primary">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam
-    pharetra arcu in commodo. Cras faucibus ex id ligula aliquam, eget porta
-    tortor efficitur. Duis eget ultrices ligula. Pellentesque laoreet massa
-    interdum, venenatis sem pretium, efficitur enim. Vestibulum id nisi a massa
-    tincidunt malesuada vitae non risus. Sed eget convallis libero. Nam ac
-    libero luctus, euismod lectus eget, cursus nibh.
-  </P>
-))
-stories.add('color: secondary', () => (
-  <P color="secondary">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam
-    pharetra arcu in commodo. Cras faucibus ex id ligula aliquam, eget porta
-    tortor efficitur. Duis eget ultrices ligula. Pellentesque laoreet massa
-    interdum, venenatis sem pretium, efficitur enim. Vestibulum id nisi a massa
-    tincidunt malesuada vitae non risus. Sed eget convallis libero. Nam ac
-    libero luctus, euismod lectus eget, cursus nibh.
-  </P>
-))
+export const Sizes: Story<React.ComponentProps<typeof P>> = args => (
+  <StoryGrid cols={1}>
+    {Object.values(P.sizes).map((size, i) => (
+      <P {...args} key={i} size={size} />
+    ))}
+  </StoryGrid>
+)
+Sizes.args = { ...defaultArgs }
 
-stories.add('style override', () => (
-  <P style={{ color: colorsPink[6] }}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam
-    pharetra arcu in commodo. Cras faucibus ex id ligula aliquam, eget porta
-    tortor efficitur. Duis eget ultrices ligula. Pellentesque laoreet massa
-    interdum, venenatis sem pretium, efficitur enim. Vestibulum id nisi a massa
-    tincidunt malesuada vitae non risus. Sed eget convallis libero. Nam ac
-    libero luctus, euismod lectus eget, cursus nibh.
-  </P>
-))
+export const ClassOverride = Template.bind({})
+ClassOverride.args = {
+  ...defaultArgs,
+  className: glamor.css({ color: colorsBlue[6] }).toString()
+}
 
-stories.add('className override', () => (
-  <P className={className}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam
-    pharetra arcu in commodo. Cras faucibus ex id ligula aliquam, eget porta
-    tortor efficitur. Duis eget ultrices ligula. Pellentesque laoreet massa
-    interdum, venenatis sem pretium, efficitur enim. Vestibulum id nisi a massa
-    tincidunt malesuada vitae non risus. Sed eget convallis libero. Nam ac
-    libero luctus, euismod lectus eget, cursus nibh.
-  </P>
-))
+export const StyleOverride = Template.bind({})
+StyleOverride.args = {
+  ...defaultArgs,
+  style: { color: colorsPink[6] }
+}
