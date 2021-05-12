8e2b958d9f8278f6d220867c64f66eaa98f0995b
jaketrent <trent.jake@gmail.com>
2021-05-12 15:55:55 -0600

    test(circularprogress): convert stories to csf; axe standby test

--- packages/circularprogress/src/react/__specs__/index.spec.tsx
+++ packages/circularprogress/src/react/__specs__/index.spec.tsx
@@ -1,9 +1,14 @@
+import { convertStoriesToJestCases } from '@pluralsight/ps-design-system-util'
+import { axe } from 'jest-axe'
 import React from 'react'
 import { render } from '@testing-library/react'
 
 import CircularProgress from '../index'
+import * as stories from '../__stories__/index.story'
 
 describe('CircularProgress', () => {
+  const cases = convertStoriesToJestCases(stories)
+
   it('renders', () => {
     const { getByTestId } = render(
       <CircularProgress data-testid="mock-component" />
@@ -11,6 +16,7 @@ describe('CircularProgress', () => {
 
     expect(getByTestId('mock-component')).toBeInTheDocument()
   })
+
   it('forwards refs', () => {
     const ref = React.createRef<HTMLDivElement>()
 
@@ -18,4 +24,15 @@ describe('CircularProgress', () => {
 
     expect(ref.current).not.toBeNull()
   })
+
+  // TODO: fix the landmark unique id problem;
+  // Likely use progressbar and aria-valuenow
+  // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_progressbar_role
+  // describe.each(cases)('%s story', (_name, Story) => {
+  //   it('has no axe-core violations', async () => {
+  //     const { container } = render(<Story {...Story.args} />)
+  //     const results = await axe(container)
+  //     expect(results).toHaveNoViolations()
+  //   })
+  // })
 })
