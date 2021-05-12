80d3e8cbbd182f6a173ea90ecdcaa5e08f9ee483
jaketrent <trent.jake@gmail.com>
2021-05-12 16:07:47 -0600

    test(linearprogress): convert stories to csf; axe and forwardref standby test

---
+++ packages/linearprogress/src/react/__specs__/index.spec.tsx
@@ -0,0 +1,39 @@
+import { convertStoriesToJestCases } from '@pluralsight/ps-design-system-util'
+import { axe } from 'jest-axe'
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import LinearProgress from '../index'
+import * as stories from '../__stories__/index.story'
+
+describe('LinearProgress', () => {
+  const cases = convertStoriesToJestCases(stories)
+
+  it('renders', () => {
+    const { getByTestId } = render(
+      <LinearProgress data-testid="mock-component" />
+    )
+
+    expect(getByTestId('mock-component')).toBeInTheDocument()
+  })
+
+  // TODO: make it forward ref quickly when you do the a11y update below
+  // it('forwards refs', () => {
+  //   const ref = React.createRef<HTMLDivElement>()
+
+  //   render(<LinearProgress ref={ref} />)
+
+  //   expect(ref.current).not.toBeNull()
+  // })
+
+  // TODO: fix the landmark unique id problem;
+  // Likely use progressbar and aria-valuenow
+  // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_progressbar_role
+  // describe.each(cases)('%s story', (_name, Story) => {
+  //   it('has no axe-core violations', async () => {
+  //     const { container } = render(<Story {...Story.args} />)
+  //     const results = await axe(container)
+  //     expect(results).toHaveNoViolations()
+  //   })
+  // })
+})
--- packages/linearprogress/src/react/__stories__/index.story.tsx
+++ packages/linearprogress/src/react/__stories__/index.story.tsx
@@ -1,51 +1,83 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
 import * as core from '@pluralsight/ps-design-system-core'
-import { storiesOf } from '@storybook/react'
 import React from 'react'
 
 import LinearProgress from '../index'
 
-const useRandom = () => {
-  const [value, setValue] = React.useState(0)
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      const rando = Math.floor(Math.random() * 115)
-      setValue(rando > 100 ? 100 : rando)
-    }, 2500)
-    return () => clearInterval(interval)
-  })
-  return value
+export default {
+  title: 'Components/LinearProgress',
+  component: LinearProgress
+} as Meta
+
+export const MinWidthNoVal: Story = () => <LinearProgress />
+
+export const FillsContainer: Story = () => (
+  <div
+    style={{
+      width: '200px',
+      background: 'rgba(0, 100, 255, 0.5)',
+      padding: '8px'
+    }}
+  >
+    <LinearProgress value={66} />
+  </div>
+)
+
+export const ValuesAndSizes: Story = () => (
+  <StoryGrid>
+    {[0, 25, 33, 50, 66, 75, 100].reduce<(Element | JSX.Element)[]>(
+      (acc, value) => [
+        ...acc,
+        <div key={'val' + value}>{value}</div>,
+        <LinearProgress key={'line' + value} value={value} />
+      ],
+      []
+    )}
+  </StoryGrid>
+)
+
+export const Animation: Story = () => {
+  const useRandom = () => {
+    const [value, setValue] = React.useState(0)
+    React.useEffect(() => {
+      const interval = setInterval(() => {
+        const rando = Math.floor(Math.random() * 115)
+        setValue(rando > 100 ? 100 : rando)
+      }, 2500)
+      return () => clearInterval(interval)
+    })
+    return value
+  }
+
+  const AnimationDemo = props => {
+    const value = useRandom()
+    return (
+      <div
+        style={{ display: 'flex', flexDirection: 'column', minWidth: '300px' }}
+      >
+        <LinearProgress value={value} />
+        <div style={{ color: core.colorsTextIcon.highOnDark }}>
+          Value: {value}
+        </div>
+      </div>
+    )
+  }
+  return <AnimationDemo />
 }
 
-const AnimationDemo = props => {
-  const value = useRandom()
+const StoryGrid: React.FC<{ cols?: number }> = props => {
+  const { cols = 2, ...rest } = props
+
   return (
     <div
-      style={{ display: 'flex', flexDirection: 'column', minWidth: '300px' }}
-    >
-      <LinearProgress value={value} />
-      <div style={{ color: core.colorsTextIcon.highOnDark }}>
-        Value: {value}
-      </div>
-    </div>
+      style={{
+        display: 'grid',
+        gap: '20px',
+        gridTemplateColumns: Array(cols).fill('1fr').join(' '),
+        alignItems: 'center',
+        justifyItems: 'left'
+      }}
+      {...rest}
+    />
   )
 }
-
-storiesOf('LinearProgress', module)
-  .add('min width, no val', _ => <LinearProgress />)
-  .add('fills container', _ => (
-    <div
-      style={{
-        width: '200px',
-        background: 'rgba(0, 100, 255, 0.5)',
-        padding: '8px'
-      }}
-    >
-      <LinearProgress value={66} />
-    </div>
-  ))
-  .add('value, 0%', _ => <LinearProgress value={0} />)
-  .add('value, 33%', _ => <LinearProgress value={33} />)
-  .add('value, 50%', _ => <LinearProgress value={50} />)
-  .add('value, 100%', _ => <LinearProgress value={100} />)
-  .add('value, 999%', _ => <LinearProgress value={999} />)
-  .add('animates to new values', _ => <AnimationDemo />)
