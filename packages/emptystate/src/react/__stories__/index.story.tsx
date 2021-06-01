436909916495cab5a5c8a01a097bf191753d1b62
jaketrent <trent.jake@gmail.com>
2021-06-01 15:21:45 -0600

    test(emptystate): convert stories to csf; add axe test

--- packages/emptystate/src/react/__stories__/index.story.tsx
+++ packages/emptystate/src/react/__stories__/index.story.tsx
@@ -1,11 +1,17 @@
-import Button from '@pluralsight/ps-design-system-button'
-import * as Icon from '@pluralsight/ps-design-system-icon'
-import Tag from '@pluralsight/ps-design-system-tag'
-import TextInput from '@pluralsight/ps-design-system-textinput'
-import { storiesOf } from '@storybook/react'
-import React from 'react'
+import Button from "@pluralsight/ps-design-system-button";
+import * as Icon from "@pluralsight/ps-design-system-icon";
+import Tag from "@pluralsight/ps-design-system-tag";
+import TextInput from "@pluralsight/ps-design-system-textinput";
+import { storiesOf } from "@storybook/react";
+import { Meta, Story } from "@storybook/react/types-6-0";
+import React from "react";
 
-import EmptyState, { EmptyStateProps } from '../index'
+import EmptyState, { EmptyStateProps } from "../index";
+
+export default {
+  title: "Components/EmptyState",
+  component: EmptyState
+} as Meta;
 
 const CustomIllustration = () => (
   <svg viewBox="0 0 128 128" aria-hidden role="img">
@@ -14,7 +20,7 @@ const CustomIllustration = () => (
       d="M44.99 43.356v-16.77L109.792 64 44.99 101.414v-16.77l-14.897 8.601v-58.49l14.897 8.601zm0 7.162l-8.695-5.02v37.003l8.695-5.02V50.518zm6.202 30.544v9.609L97.387 64 51.192 37.329v9.608l29.552 17.062-29.552 17.063zm0-7.162l17.147-9.901-17.147-9.9v19.8z"
     />
   </svg>
-)
+);
 
 const EmptyStateWithDefaults = (
   props: Partial<React.PropsWithoutRef<EmptyStateProps>>
@@ -23,7 +29,7 @@ const EmptyStateWithDefaults = (
     <EmptyState.Actions>
       <Button>Do a Thing</Button>
     </EmptyState.Actions>
-  )
+  );
 
   const caption = (
     <EmptyState.Caption>
@@ -31,15 +37,15 @@ const EmptyStateWithDefaults = (
       Bacon venison meatball pork, alcatra capicola turducken. Salami shoulder
       strip steak filet mignon pork.
     </EmptyState.Caption>
-  )
+  );
   const heading = (
     <EmptyState.Heading>
       Filet mignon pork chop salami shank tail.
     </EmptyState.Heading>
-  )
+  );
   const illustration = (
     <EmptyState.Illustration name={EmptyState.Illustration.names.magnify} />
-  )
+  );
 
   return (
     <EmptyState
@@ -49,22 +55,22 @@ const EmptyStateWithDefaults = (
       illustration={illustration}
       {...props}
     />
-  )
-}
+  );
+};
 
-const SearchInput: React.FC = props => <TextInput {...props} />
+const SearchInput: React.FC = props => <TextInput {...props} />;
 SearchInput.defaultProps = {
   appearance: TextInput.appearances.subtle,
   icon: <Icon.SearchIcon />,
-  placeholder: 'Find Something'
-}
+  placeholder: "Find Something"
+};
 
 const InlineList: React.FC = props => {
-  const { children, ...rest } = props
+  const { children, ...rest } = props;
   const styles: { [key: string]: React.CSSProperties } = {
-    list: { display: 'flex', flexWrap: 'wrap', justifyContent: 'center' },
-    item: { display: 'inline-flex', margin: '0 5px 12px' }
-  }
+    list: { display: "flex", flexWrap: "wrap", justifyContent: "center" },
+    item: { display: "inline-flex", margin: "0 5px 12px" }
+  };
 
   return (
     <div style={styles.list} {...rest}>
@@ -74,117 +80,113 @@ const InlineList: React.FC = props => {
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-storiesOf('EmptyState', module)
-  .add('combo', () => <EmptyStateWithDefaults />)
-  .add('in a small fixed size container', () => (
-    <div style={{ width: 400 }}>
-      <EmptyStateWithDefaults />
-    </div>
-  ))
+export const Combo: Story = () => (
+  <EmptyStateWithDefaults />
+);
 
-Object.values(EmptyState.sizes).forEach(size => {
-  storiesOf('EmptyState/sizes', module).add(size, () => (
-    <EmptyStateWithDefaults size={size} />
-  ))
-})
+export const SmallFixedSizeContainer: Story = () => (
+  <div style={{ width: 400 }}>
+    <EmptyStateWithDefaults />
+  </div>
+);
 
-Object.values(EmptyState.Illustration.names).forEach(name => {
-  storiesOf('EmptyState/illustrations/large', module).add(name, () => (
-    <div style={{ background: 'black' }}>
-      <EmptyState
-        heading={<EmptyState.Heading>{name}</EmptyState.Heading>}
-        illustration={<EmptyState.Illustration name={name} />}
-        size={EmptyState.sizes.large}
+export const Sizes: Story = () => (
+  <>
+    {Object.values(EmptyState.sizes).map(size => (
+      <EmptyStateWithDefaults size={size} key={size} />
+    ))}
+  </>
+);
+
+export const Illustrations: Story = () => (
+  <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gap: "16px", background: "black" }}>
+    <EmptyState
+      heading={<EmptyState.Heading>Custom Illustration</EmptyState.Heading>}
+      illustration={
+        <EmptyState.Illustration>
+          <CustomIllustration />
+        </EmptyState.Illustration>
+      }
+      size={EmptyState.sizes.large}
+    />
+    <EmptyState
+      heading={<EmptyState.Heading>Custom Illustration</EmptyState.Heading>}
+      illustration={
+        <EmptyState.Illustration>
+          <CustomIllustration />
+        </EmptyState.Illustration>
+      }
+      size={EmptyState.sizes.small}
+    />
+    {Object.values(EmptyState.Illustration.names).map(name => [
+        <EmptyState
+          heading={<EmptyState.Heading>{name}</EmptyState.Heading>}
+          illustration={<EmptyState.Illustration name={name} />}
+          size={EmptyState.sizes.large}
+          key={name + 'lrg'}
+        />,
+        <EmptyState
+          heading={<EmptyState.Heading>{name}</EmptyState.Heading>}
+          illustration={<EmptyState.Illustration name={name} />}
+          size={EmptyState.sizes.small}
+          key={name + 'sm'}
       />
-    </div>
-  ))
+    ])}
+  </div>
+);
 
-  storiesOf('EmptyState/illustrations/small', module).add(name, () => (
-    <div style={{ background: 'black' }}>
-      <EmptyState
-        heading={<EmptyState.Heading>{name}</EmptyState.Heading>}
-        illustration={<EmptyState.Illustration name={name} />}
-        size={EmptyState.sizes.small}
-      />
-    </div>
-  ))
-})
-
-storiesOf('EmptyState/illustrations/small', module).add('custom', () => (
-  <EmptyState
-    heading={<EmptyState.Heading>Custom Illustration</EmptyState.Heading>}
-    illustration={
-      <EmptyState.Illustration>
-        <CustomIllustration />
-      </EmptyState.Illustration>
+export const ActionsWithAnInput: Story = () => (
+  <EmptyStateWithDefaults
+    actions={
+      <EmptyState.Actions>
+        <SearchInput />
+      </EmptyState.Actions>
     }
-    size={EmptyState.sizes.small}
   />
-))
+);
 
-storiesOf('EmptyState/illustrations/large', module).add('custom', () => (
-  <EmptyState
-    heading={<EmptyState.Heading>Custom Illustration</EmptyState.Heading>}
-    illustration={
-      <EmptyState.Illustration>
-        <CustomIllustration />
-      </EmptyState.Illustration>
+export const ActionsWithAButton: Story = () => (
+  <EmptyStateWithDefaults
+    actions={
+      <EmptyState.Actions>
+        <InlineList>
+          <Button appearance={Button.appearances.stroke}>Asssessments</Button>
+          <Button appearance={Button.appearances.stroke}>Guides</Button>
+          <Button appearance={Button.appearances.stroke}>
+            Interactive Courses
+          </Button>
+          <Button appearance={Button.appearances.stroke}>Paths</Button>
+          <Button appearance={Button.appearances.stroke}>Projects</Button>
+          <Button appearance={Button.appearances.stroke}>
+            Video Courses
+          </Button>
+        </InlineList>
+      </EmptyState.Actions>
     }
-    size={EmptyState.sizes.large}
   />
-))
+);
 
-storiesOf('EmptyState/actions', module)
-  .add('with an input', () => (
-    <EmptyStateWithDefaults
-      actions={
-        <EmptyState.Actions>
-          <SearchInput />
-        </EmptyState.Actions>
-      }
-    />
-  ))
-  .add('with buttons', () => (
-    <EmptyStateWithDefaults
-      actions={
-        <EmptyState.Actions>
-          <InlineList>
-            <Button appearance={Button.appearances.stroke}>Asssessments</Button>
-            <Button appearance={Button.appearances.stroke}>Guides</Button>
-            <Button appearance={Button.appearances.stroke}>
-              Interactive Courses
-            </Button>
-            <Button appearance={Button.appearances.stroke}>Paths</Button>
-            <Button appearance={Button.appearances.stroke}>Projects</Button>
-            <Button appearance={Button.appearances.stroke}>
-              Video Courses
-            </Button>
-          </InlineList>
-        </EmptyState.Actions>
-      }
-    />
-  ))
-  .add('with tags', () => (
-    <EmptyStateWithDefaults
-      actions={
-        <EmptyState.Actions>
-          <InlineList>
-            <Tag>Asssessments</Tag>
-            <Tag>Guides</Tag>
-            <Tag>Interactive Courses</Tag>
-            <Tag>Paths</Tag>
-            <Tag>Projects</Tag>
-            <Tag>Video Courses</Tag>
-          </InlineList>
-        </EmptyState.Actions>
-      }
-    />
-  ))
+export const ActionsWithTags: Story = () => (
+  <EmptyStateWithDefaults
+    actions={
+      <EmptyState.Actions>
+        <InlineList>
+          <Tag>Asssessments</Tag>
+          <Tag>Guides</Tag>
+          <Tag>Interactive Courses</Tag>
+          <Tag>Paths</Tag>
+          <Tag>Projects</Tag>
+          <Tag>Video Courses</Tag>
+        </InlineList>
+      </EmptyState.Actions>
+    }
+  />
+);
 
-storiesOf('EmptyState/mounting', module).add('toggle show/hide', () => {
+export const MountingToggleShowHide: Story = () => {
   const MountStory: React.FC = props => {
     const [shown, setShown] = React.useState(true)
 
@@ -207,4 +209,4 @@ storiesOf('EmptyState/mounting', module).add('toggle show/hide', () => {
       <EmptyStateWithDefaults />
     </MountStory>
   )
-})
+};
