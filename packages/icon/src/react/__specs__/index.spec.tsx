29030476f194ead5e950ce0c4700ecb80c3b76e6
jaketrent <trent.jake@gmail.com>
2021-06-02 11:40:37 -0600

    test(icon): convert stories to csf; add axe test

--- packages/icon/src/react/__specs__/index.spec.tsx
+++ packages/icon/src/react/__specs__/index.spec.tsx
@@ -1,8 +1,11 @@
+import { convertStoriesToJestCases } from '@pluralsight/ps-design-system-util'
 import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
 import React from 'react'
 
 import Icon from '../index'
 import * as Icons from '../icons'
+import * as stories from '../__stories__/index.story'
 import * as vars from '../../vars'
 
 describe('Icon', () => {
@@ -24,5 +27,16 @@ describe('Icon', () => {
     const { container } = render(<Icons.SearchIcon aria-label="test label" />)
     const svg = container.querySelector('svg')
     expect(svg).toHaveAttribute('aria-label', 'test label')
+  })
+
+  const cases = convertStoriesToJestCases(stories)
+  describe('accessibility', () => {
+    describe.each(cases)('%s story', (_name, Story) => {
+      it('has no axe-core violations', async () => {
+        const { container } = render(<Story {...Story.args} />)
+        const results = await axe(container)
+        expect(results).toHaveNoViolations()
+      })
+    })
   })
 })
--- packages/icon/src/react/__stories__/index.story.tsx
+++ packages/icon/src/react/__stories__/index.story.tsx
@@ -1,84 +1,77 @@
-import glamorDefault, * as glamorExports from 'glamor'
+import { Meta, Story } from '@storybook/react/types-6-0'
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 
 import Icon from '../index'
 import * as Icons from '../icons'
 
-const glamor = glamorDefault || glamorExports
+export default {
+  title: 'Components/Icon',
+  component: Icon
+} as Meta
 
-const styles = {
-  grid: glamor.css({
-    display: 'grid',
-    gridGap: '1em',
-    gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr'
-  }),
-  gridItem: glamor.css({
-    height: '24px',
-    width: '24px'
-  }),
-  name: glamor.css({
-    height: '24px',
-    lineHeight: '24px',
-    paddingLeft: '8px'
-  })
-}
-
-const Grid: React.FC<unknown> = props => <div {...props} {...styles.grid} />
-
-const GridItem: React.FC<unknown> = props => (
-  <div {...props} {...styles.gridItem} />
+export const Colors: Story = () => (
+  <div>
+    {Object.values(Icon.colors).map(color => (
+      <Icons.CheckIcon key={color} color={Icon.colors[color]} />
+    ))}
+  </div>
 )
 
-const colorStory = storiesOf('appearance', module)
-Object.values(Icon.colors).forEach(color =>
-  colorStory.add(color, () => <Icons.CheckIcon color={Icon.colors[color]} />)
+export const Sizes: Story = () => (
+  <div>
+    {Object.values(Icon.sizes).map(size => (
+      <Icons.CheckIcon key={size} size={Icon.sizes[size]} />
+    ))}
+  </div>
 )
 
-const sizeStory = storiesOf('size', module)
-Object.values(Icon.sizes).forEach(size =>
-  sizeStory.add(size, () => (
-    <Icons.CheckIcon color={Icon.colors.textIconHighOnDark} size={size} />
-  ))
-)
-
-const idStory = storiesOf('names', module)
-idStory.add('all export names', () => (
-  <Grid>
+export const ExportedIcons: Story = () => (
+  <StoryGrid>
     {Object.keys(Icons).map(id => {
       const Comp = Icons[id]
-      return (
-        <GridItem key={`item-${id}`}>
-          <Comp color={Icon.colors.textIconHighOnDark} />
-        </GridItem>
-      )
+      return [
+        <div>{id}</div>,
+        <Comp
+          id={`icon-${id}`}
+          color={Icon.colors.textIconHighOnDark}
+          size="large"
+        />
+      ]
     })}
-  </Grid>
-))
-Object.keys(Icons).forEach(id =>
-  idStory.add(id, () => {
-    const Comp = Icons[id]
-    return (
-      <Comp color={Icon.colors.textIconHighOnDark} size={Icon.sizes.large} />
-    )
-  })
+  </StoryGrid>
 )
 
-storiesOf('custom props', module).add('aria-label', () => (
+export const CustomProps: Story = () => (
   <Icons.CheckIcon
     color={Icon.colors.textIconHighOnDark}
     aria-label="completed"
   />
-))
+)
 
-const SvgAccounticon: React.FC<unknown> = props => (
+export const CustomIcon: Story = () => (
+  <Icon color={Icon.colors.textIconHighOnDark}>
+    <CustomSvgIcon />
+  </Icon>
+)
+
+const CustomSvgIcon: React.FC<unknown> = props => (
   <svg aria-label="account icon" viewBox="0 0 24 24" role="img" {...props}>
     <path d="M20 2c1.103 0 2 .898 2 2v16c0 1.103-.897 2-2 2H4c-1.103 0-2-.897-2-2V4c0-1.102.897-2 2-2h16zm-4 18h4.001L20 4h-4v3h-2V4h-4v8H8V4H4v16h4v-3h2v3h4v-8h2v8zM6 13h6v3H6v-3zm6-5h6v3h-6V8z" />
   </svg>
 )
 
-storiesOf('custom icon', module).add('aria-label', () => (
-  <Icon color={Icon.colors.textIconHighOnDark}>
-    <SvgAccounticon />
-  </Icon>
-))
+const StoryGrid: React.FC<{ cols?: number }> = props => {
+  const { cols = 2, ...rest } = props
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gap: '20px',
+        gridTemplateColumns: Array(cols).fill('1fr').join(' '),
+        justifyItems: 'left'
+      }}
+      {...rest}
+    />
+  )
+}
