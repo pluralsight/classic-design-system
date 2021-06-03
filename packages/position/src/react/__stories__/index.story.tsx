a9702cd7bd23df3232e75c5c3c692a0ea17f2e47
jaketrent <trent.jake@gmail.com>
2021-06-03 13:38:04 -0600

    test(position): convert stories to csf; add axe test

--- packages/position/src/react/__stories__/index.story.tsx
+++ packages/position/src/react/__stories__/index.story.tsx
@@ -2,7 +2,7 @@ import { InfoIcon } from '@pluralsight/ps-design-system-icon'
 import * as core from '@pluralsight/ps-design-system-core'
 import Tooltip from '@pluralsight/ps-design-system-tooltip'
 import { HTMLPropsFor, usePortal } from '@pluralsight/ps-design-system-util'
-import { storiesOf } from '@storybook/react'
+import { Meta, Story } from '@storybook/react/types-6-0'
 import glamorDefault, * as glamorExports from 'glamor'
 import React from 'react'
 
@@ -28,6 +28,10 @@ import {
 } from '../index'
 
 const glamor = glamorDefault || glamorExports
+
+export default {
+  title: 'Components/Position'
+} as Meta
 
 const positionComponents = {
   Above,
@@ -121,138 +125,128 @@ const ScrollContainer: React.FC = props => {
   )
 }
 
-const basicStories = storiesOf('Components | Position / basic', module)
-Object.values(positionComponents).forEach(Comp => {
-  const { displayName } = Comp
-  const name = `<${Comp.displayName} />`
-
-  basicStories.add(displayName as string, () => (
-    <Comp show={<MockToolip />}>
-      <Box>{name}</Box>
-    </Comp>
-  ))
-})
-
-storiesOf('Components | Position / custom style', module).add(
-  'shown element keeps style prop',
-  () => (
-    <positionComponents.Above
-      show={<MockToolip style={{ color: core.colorsPink[6] }} />}
-    >
-      <Box>Tooltip is pink</Box>
-    </positionComponents.Above>
-  )
-)
-
-storiesOf('Components | Position / custom ref', module).add(
-  'shown element keeps style prop',
-  () => {
-    const ref = React.useRef<HTMLDivElement>(null)
-    const { x, y } = !ref.current
-      ? { x: -1, y: -1 }
-      : ref.current.getBoundingClientRect()
-
-    return (
-      <>
-        <positionComponents.Above
-          show={<MockToolip style={{ color: core.colorsPink[6] }} ref={ref} />}
-        >
-          <Box>Tooltip is pink</Box>
-        </positionComponents.Above>
-        <span style={{ color: 'white', position: 'absolute', top: 250 }}>
-          X: {x}, Y: {y}
-        </span>
-      </>
-    )
-  }
-)
-
-const portalStories = storiesOf(
-  'Components | Position / in custom portal',
-  module
-)
-Object.values(positionComponents).forEach(Comp => {
-  const { displayName } = Comp
-  const name = `<${Comp.displayName} />`
-
-  const Outer: React.FC = props => {
-    const selectors = glamor.css({
-      border: `4px dashed ${core.colorsOrange[6]}`,
-      color: core.colorsTextIcon.highOnDark,
-      height: 500,
-      padding: 20,
-      width: 500
-    })
-    return <div {...props} {...selectors} />
-  }
-
-  interface PortalStoryProps {
-    children(props: {
-      // TODO: make this type more precise in util package
-      portal: React.MutableRefObject<HTMLDivElement>
-    }): React.ReactNode
-  }
-  const PortalStory = (props: PortalStoryProps) => {
-    const portal = usePortal() as React.MutableRefObject<HTMLDivElement>
-    return <Outer>{props.children({ portal })}</Outer>
-  }
-
-  portalStories.add(displayName as string, () => (
-    <PortalStory>
-      {({ portal }) => (
-        <Comp show={<MockToolip />} inNode={portal.current}>
-          <Box>{name}</Box>
-        </Comp>
-      )}
-    </PortalStory>
-  ))
-})
-
-const targetStories = storiesOf('Components | Position / custom target', module)
-Object.values(positionComponents).forEach(Comp => {
-  const TargetStory: React.FC = props => {
-    const ref = React.useRef<HTMLDivElement>(null)
-
-    return (
-      <Comp show={<MockToolip />} target={ref}>
-        <Box>
-          <div ref={ref} style={{ border: '1px solid yellow' }}>
-            {name}
-          </div>
-        </Box>
+export const Positions: Story = () => (
+  <StoryGrid style={{ gap: '128px' }}>
+    {Object.values(positionComponents).map(Comp => (
+      <Comp show={<MockToolip />}>
+        <Box>{`<${Comp.displayName} />`}</Box>
       </Comp>
-    )
-  }
-  const { displayName } = Comp
-  const name = `<${Comp.displayName} />`
-
-  targetStories.add(displayName as string, () => <TargetStory />)
-})
-
-storiesOf('Components | Position / in scrollable container', module).add(
-  'RightOf',
-  () => {
-    function ScrollStory() {
-      const ref = React.useRef<HTMLDivElement>(null)
-
-      return (
-        <ScrollContainer>
-          <RightOf show={<Tooltip>The tip</Tooltip>} target={ref}>
-            <Box ref={ref}>target</Box>
-          </RightOf>
-        </ScrollContainer>
-      )
-    }
-
-    return <ScrollStory />
-  }
+    ))}
+  </StoryGrid>
 )
 
-const jsStory = storiesOf('Utilities | Position / position fns', module)
-Object.keys(positionFns).forEach((pos: string) =>
-  jsStory.add(pos, () => (
-    <JsStory positionFnName={pos as keyof typeof positionFns} />
-  ))
+export const ShownElementKeepsStyleProp: Story = () => (
+  <Above show={<MockToolip style={{ color: core.colorsPink[6] }} />}>
+    <Box>Tooltip is pink</Box>
+  </Above>
+)
+
+// TODO: this looks broken -- tooltip ref is not maintained
+export const ShownElementKeepsRef: Story = () => {
+  const ref = React.useRef<HTMLDivElement>(null)
+  const { x, y } = !ref.current
+    ? { x: -1, y: -1 }
+    : ref.current.getBoundingClientRect()
+
+  return (
+    <>
+      <positionComponents.Above
+        show={<MockToolip style={{ color: core.colorsPink[6] }} ref={ref} />}
+      >
+        <Box>Tooltip is pink</Box>
+      </positionComponents.Above>
+      <span style={{ color: 'white', position: 'absolute', top: 250 }}>
+        X: {x}, Y: {y}
+      </span>
+    </>
+  )
+}
+
+export const PositionsInCustomPortal: Story = () => {
+  return (
+    <StoryGrid>
+      {Object.values(positionComponents).map(Comp => {
+        const { displayName } = Comp
+        const name = `<${Comp.displayName} />`
+
+        const Outer: React.FC = props => {
+          const selectors = glamor.css({
+            border: `4px dashed ${core.colorsOrange[6]}`,
+            color: core.colorsTextIcon.highOnDark,
+            height: 500,
+            padding: 20,
+            width: 500
+          })
+          return <div {...props} {...selectors} />
+        }
+
+        interface PortalStoryProps {
+          children(props: {
+            // TODO: make this type more precise in util package
+            portal: React.MutableRefObject<HTMLDivElement>
+          }): React.ReactNode
+        }
+        const PortalStory = (props: PortalStoryProps) => {
+          const portal = usePortal() as React.MutableRefObject<HTMLDivElement>
+          return <Outer>{props.children({ portal })}</Outer>
+        }
+
+        return (
+          <PortalStory>
+            {({ portal }) => (
+              <Comp show={<MockToolip />} inNode={portal.current}>
+                <Box>{name}</Box>
+              </Comp>
+            )}
+          </PortalStory>
+        )
+      })}
+    </StoryGrid>
+  )
+}
+
+export const Target: Story = () => (
+  <StoryGrid style={{ gap: '128px' }}>
+    {Object.values(positionComponents).map(Comp => {
+      const TargetStory: React.FC = props => {
+        const ref = React.useRef<HTMLDivElement>(null)
+
+        return (
+          <Comp show={<MockToolip />} target={ref}>
+            <Box>
+              <div ref={ref} style={{ border: '1px solid yellow' }}>
+                {name}
+              </div>
+            </Box>
+          </Comp>
+        )
+      }
+      const { displayName } = Comp
+      const name = `<${Comp.displayName} />`
+
+      return <TargetStory />
+    })}
+  </StoryGrid>
+)
+
+export const ScrollableContainer: Story = () => {
+  const ref = React.useRef<HTMLDivElement>(null)
+
+  return (
+    <ScrollContainer>
+      <RightOf show={<Tooltip>The tip</Tooltip>} target={ref}>
+        <Box ref={ref}>target</Box>
+      </RightOf>
+    </ScrollContainer>
+  )
+}
+
+export const JavascriptPositions: Story = () => (
+  <StoryGrid style={{ gap: '128px' }}>
+    {Object.keys(positionFns).map((pos: string) => (
+      <JsStory positionFnName={pos as keyof typeof positionFns} />
+    ))}
+  </StoryGrid>
 )
 
 interface JsStoryProps {
@@ -301,97 +295,121 @@ const JsStory: React.FC<JsStoryProps> = ({ positionFnName }) => {
   )
 }
 
-const edgeCaseStories = storiesOf('Position Edge', module)
+export const PositionsOnEdge: Story = () => (
+  <StoryGrid style={{ gap: '128px' }}>
+    {Object.values(positionComponents).map(Comp => {
+      const { displayName } = Comp
+      function Demo() {
+        const [b1hover, setb1hover] = React.useState(false)
+        const [b2hover, setb2hover] = React.useState(false)
+        const [b3hover, setb3hover] = React.useState(false)
+        const [b4hover, setb4hover] = React.useState(false)
 
-Object.values(positionComponents).forEach(Comp => {
-  const { displayName } = Comp
-  function Demo() {
-    const [b1hover, setb1hover] = React.useState(false)
-    const [b2hover, setb2hover] = React.useState(false)
-    const [b3hover, setb3hover] = React.useState(false)
-    const [b4hover, setb4hover] = React.useState(false)
+        return (
+          <div
+            style={{
+              display: 'grid',
+              justifyItems: 'start',
+              grid: '1fr 1fr/ 1fr 1fr',
+              width: '100%',
+              height: '100vh',
+              overflow: 'hidden'
+            }}
+          >
+            <Comp
+              when={b1hover}
+              show={
+                <Tooltip>
+                  I am some really long content, look at me go, i am so long and
+                  contentful
+                </Tooltip>
+              }
+            >
+              <InfoIcon
+                onMouseEnter={() => setb1hover(true)}
+                onMouseLeave={() => setb1hover(false)}
+                style={{ color: 'white' }}
+              >
+                Hover me
+              </InfoIcon>
+            </Comp>
+            <Comp
+              when={b2hover}
+              show={
+                <Tooltip>
+                  I am some really long content, look at me go, i am so long and
+                  contentful
+                </Tooltip>
+              }
+            >
+              <InfoIcon
+                onMouseEnter={() => setb2hover(true)}
+                onMouseLeave={() => setb2hover(false)}
+                style={{ color: 'white', justifySelf: 'end' }}
+              >
+                Hover me
+              </InfoIcon>
+            </Comp>
+            <Comp
+              when={b3hover}
+              show={
+                <Tooltip>
+                  I am some really long content, look at me go, i am so long and
+                  contentful
+                </Tooltip>
+              }
+            >
+              <InfoIcon
+                onMouseEnter={() => setb3hover(true)}
+                onMouseLeave={() => setb3hover(false)}
+                style={{ color: 'white', alignSelf: 'end' }}
+              >
+                Hover me
+              </InfoIcon>
+            </Comp>
+            <Comp
+              when={b4hover}
+              show={
+                <Tooltip>
+                  I am some really long content, look at me go, i am so long and
+                  contentful
+                </Tooltip>
+              }
+            >
+              <InfoIcon
+                style={{ color: 'white', justifySelf: 'end', alignSelf: 'end' }}
+                onMouseEnter={() => setb4hover(true)}
+                onMouseLeave={() => setb4hover(false)}
+              >
+                Hover me
+              </InfoIcon>
+            </Comp>
+          </div>
+        )
+      }
+      return <Demo />
+    })}
+  </StoryGrid>
+)
 
-    return (
-      <div
-        style={{
-          display: 'grid',
-          justifyItems: 'start',
-          grid: '1fr 1fr/ 1fr 1fr',
-          width: '100%',
-          height: '100vh',
-          overflow: 'hidden'
-        }}
-      >
-        <Comp
-          when={b1hover}
-          show={
-            <Tooltip>
-              I am some really long content, look at me go, i am so long and
-              contentful
-            </Tooltip>
-          }
-        >
-          <InfoIcon
-            onMouseEnter={() => setb1hover(true)}
-            onMouseLeave={() => setb1hover(false)}
-            style={{ color: 'white' }}
-          >
-            Hover me
-          </InfoIcon>
-        </Comp>
-        <Comp
-          when={b2hover}
-          show={
-            <Tooltip>
-              I am some really long content, look at me go, i am so long and
-              contentful
-            </Tooltip>
-          }
-        >
-          <InfoIcon
-            onMouseEnter={() => setb2hover(true)}
-            onMouseLeave={() => setb2hover(false)}
-            style={{ color: 'white', justifySelf: 'end' }}
-          >
-            Hover me
-          </InfoIcon>
-        </Comp>
-        <Comp
-          when={b3hover}
-          show={
-            <Tooltip>
-              I am some really long content, look at me go, i am so long and
-              contentful
-            </Tooltip>
-          }
-        >
-          <InfoIcon
-            onMouseEnter={() => setb3hover(true)}
-            onMouseLeave={() => setb3hover(false)}
-            style={{ color: 'white', alignSelf: 'end' }}
-          >
-            Hover me
-          </InfoIcon>
-        </Comp>
-        <Comp
-          when={b4hover}
-          show={
-            <Tooltip>
-              I am some really long content, look at me go, i am so long and
-              contentful
-            </Tooltip>
-          }
-        >
-          <InfoIcon
-            style={{ color: 'white', justifySelf: 'end', alignSelf: 'end' }}
-            onMouseEnter={() => setb4hover(true)}
-            onMouseLeave={() => setb4hover(false)}
-          >
-            Hover me
-          </InfoIcon>
-        </Comp>
-      </div>
-    )
-  }
-  edgeCaseStories.add(displayName as string, () => <Demo />)
-})
+// TODO: make reusable
+//
+interface StoryGridProps extends HTMLPropsFor<'div'> {
+  cols?: number
+}
+const StoryGrid: React.FC<StoryGridProps> = props => {
+  const { cols = 2, style, ...rest } = props
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gap: '20px',
+        gridTemplateColumns: Array(cols).fill('1fr').join(' '),
+        justifyItems: 'left',
+        ...style
+      }}
+      {...rest}
+    />
+  )
+}
