ba69047492496e0a7abbf48b9556a1f7b01f2f56
jaketrent <trent.jake@gmail.com>
2021-06-14 13:29:52 -0600

    style(verticaltabs): spread a11y hidden props differently

--- packages/verticaltabs/src/react/item.tsx
+++ packages/verticaltabs/src/react/item.tsx
@@ -242,13 +242,16 @@ const Tier2Header = React.forwardRef<any, ItemHeaderProps>((props, ref) => {
       <span {...wrapperProps} ref={ref as RefFor<'span'>} />
     )
 
-  if (props.collapsed || hideLabels) {
-    rest['aria-hidden'] = 'true'
-    rest.tabIndex = -1
-  }
+  const hideItemProps =
+    props.collapsed || hideLabels
+      ? {
+          'aria-hidden': 'true',
+          tabIndex: -1
+        }
+      : {}
 
   return (
-    <Tag {...styles.tier2Header} {...rest}>
+    <Tag {...styles.tier2Header} {...rest} {...hideItemProps}>
       <span {...styles.tierHeaderLabel(null, { hideLabels })}>
         {props.children}
       </span>
