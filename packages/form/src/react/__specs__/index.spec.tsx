d127ddc48ea9cc39977d48ab82a2c7b94db91715
jaketrent <trent.jake@gmail.com>
2021-06-16 15:36:04 -0600

    test(form): enable axe tests

--- packages/form/src/react/__specs__/index.spec.tsx
+++ packages/form/src/react/__specs__/index.spec.tsx
@@ -1,20 +1,16 @@
-// import { convertStoriesToJestCases } from '@pluralsight/ps-design-system-util'
-// import { render } from '@testing-library/react'
-// import { axe } from 'jest-axe'
-// import React from 'react'
-//
-// import Form from '../index'
-// import * as stories from '../__stories__/index.story'
+import { convertStoriesToJestCases } from '@pluralsight/ps-design-system-util'
+import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
+import React from 'react'
 
-it('is a placeholder', () => {
-  expect(true).toEqual(true)
+import Form from '../index'
+import * as stories from '../__stories__/index.story'
+
+const cases = convertStoriesToJestCases(stories)
+describe.each(cases)('%s story', (_name, Story) => {
+  it('has no axe-core violations', async () => {
+    const { container } = render(<Story {...Story.args} />)
+    const results = await axe(container)
+    expect(results).toHaveNoViolations()
+  })
 })
-// TODO: fix and enable
-// const cases = convertStoriesToJestCases(stories)
-// describe.each(cases)('%s story', (_name, Story) => {
-//   it('has no axe-core violations', async () => {
-//     const { container } = render(<Story {...Story.args} />)
-//     const results = await axe(container)
-//     expect(results).toHaveNoViolations()
-//   })
-// })
