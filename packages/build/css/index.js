26ba6bea34ff02eea682b15068cd94a126444929
jaketrent <trent.jake@gmail.com>
2021-06-21 13:24:06 -0600

    feat(build): expose postcss-config

--- packages/build/css/index.js
+++ packages/build/css/index.js
@@ -1,9 +1,11 @@
 const buildComponentStylesheet = require('./build-component-stylesheet')
 const jsToCss = require('./js-to-css')
+const postcssConfig = require('./postcss-config')
 const postcssCompile = require('./postcss-compile')
 
 module.exports = {
   buildComponentStylesheet,
   jsToCss,
+  postcssConfig,
   postcssCompile
 }
