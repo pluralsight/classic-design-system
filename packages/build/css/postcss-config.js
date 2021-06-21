5ef3c41182997f18c7cc1d2e8a5ebaa2d77151aa
jaketrent <trent.jake@gmail.com>
2021-06-21 14:59:59 -0600

    build(build): fix postcss config importfrom paths

--- packages/build/css/postcss-config.js
+++ packages/build/css/postcss-config.js
@@ -1,16 +1,22 @@
+const path = require('path')
+
 module.exports = {
   plugins: [
     require('postcss-nesting'),
     require('postcss-preset-env')({
       browsers: ['Last 2 versions', 'IE >= 11'],
       importFrom: [
-        '../core/dist/css/breakpoints.module.css',
-        '../core/dist/css/colors.module.css',
-        '../core/dist/css/layers.module.css',
-        '../core/dist/css/layout.module.css',
-        '../core/dist/css/type.module.css',
-        '../core/dist/css/motion.module.css'
+        coreCssPathTo('breakpoints.module.css'),
+        coreCssPathTo('colors.module.css'),
+        coreCssPathTo('layers.module.css'),
+        coreCssPathTo('layout.module.css'),
+        coreCssPathTo('type.module.css'),
+        coreCssPathTo('motion.module.css')
       ]
     })
   ]
+}
+
+function coreCssPathTo(filename) {
+  path.join(__dirname, '..', '..', 'core', 'src', 'css', filename)
 }
