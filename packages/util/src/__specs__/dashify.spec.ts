3fd1e5b4f19f2b2a41c2f5a5151aae2114d7c55a
jaketrent <trent.jake@gmail.com>
2021-07-06 09:01:34 -0600

    refactor(util): dashify has exception for xSmall abbreviation

--- packages/util/src/__specs__/dashify.spec.ts
+++ packages/util/src/__specs__/dashify.spec.ts
@@ -16,7 +16,17 @@ it('converts camelCase to kebab-case', () => {
   expect(dashify('selectMe')).toEqual('select-me')
 })
 
-it('appends dash to numbers', () => {
-  expect(dashify('select1')).toEqual('select-1')
-  expect(dashify('select2again')).toEqual('select-2-again')
+describe('dash exceptions', () => {
+  it('appends dash to numbers', () => {
+    expect(dashify('select1')).toEqual('select-1')
+    expect(dashify('select2again')).toEqual('select-2-again')
+  })
+
+  it('does not dashify x (extra) abbreviations', () => {
+    expect(dashify('xSmall')).toEqual('xsmall')
+    expect(dashify('buttonXSmall')).toEqual('button-xsmall')
+    expect(dashify('buttonXSmallTailPosition')).toEqual(
+      'button-xsmall-tail-position'
+    )
+  })
 })
--- packages/util/src/dashify.ts
+++ packages/util/src/dashify.ts
@@ -1,10 +1,30 @@
 export function dashify(str?: string) {
   if (!str) return
 
-  return str
-    .trim()
-    .replace(/([a-z])([A-Z])/g, '$1-$2')
+  return finalize(convertCamelCase(convertExceptions(prep(str))))
+}
+
+function prep(str: string) {
+  return str.trim()
+}
+
+function convertExceptions(str: string) {
+  const dashifyNumbers = str
     .replace(/([a-z])([0-9]+)/g, '$1-$2')
     .replace(/([0-9])([a-z])/g, '$1-$2')
-    .toLowerCase()
+
+  const avoidXAbbreviation = dashifyNumbers.replace(
+    /(x)([A-Z])/g,
+    (_match, p1, p2) => p1 + p2.toLowerCase()
+  )
+
+  return avoidXAbbreviation
+}
+
+function convertCamelCase(str: string) {
+  return str.replace(/([a-z])([A-Z])/g, '$1-$2')
+}
+
+function finalize(str: string) {
+  return str.toLowerCase()
 }
