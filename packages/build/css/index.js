4de1f6cebd9cfaec917e49acbed3604ecc8419fd
jaketrent <trent.jake@gmail.com>
2021-08-11 14:19:32 -0600

    refactor(build): remove glamor and css building scripts
    BREAKING CHANGE: remove glamorToCss, jsToCss, build-css script

--- packages/build/css/index.js
+++ packages/build/css/index.js
@@ -1,11 +1,7 @@
-const buildComponentStylesheet = require('./build-component-stylesheet')
-const jsToCss = require('./js-to-css')
 const postcssConfig = require('./postcss-config')
 const postcssCompile = require('./postcss-compile')
 
 module.exports = {
-  buildComponentStylesheet,
-  jsToCss,
   postcssConfig,
   postcssCompile
 }
