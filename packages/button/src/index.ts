f7d3d408e59a1a8c69609e7571a0bf28b01c33f9
jaketrent <trent.jake@gmail.com>
2021-08-23 09:10:24 -0600

    fix(button): prevent styles treeshaken by adjusting export pattern

--- packages/button/src/index.ts
+++ packages/button/src/index.ts
@@ -1,5 +1,8 @@
 import '@pluralsight/ps-design-system-icon/styles.css'
-import './css/index.css'
 
-export { default, ButtonProps } from './react/index'
+import './css/index.css'
+import Button from './react/index'
+
+export default Button
+export type { ButtonProps } from './react/index'
 export * from './vars/index'
