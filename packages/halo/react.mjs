fb6497864c8e836e45f1f7971b99afd2fb83ffbc
jaketrent <trent.jake@gmail.com>
2021-08-23 13:39:26 -0600

    feat(halo): allow separate import for react and css

---
+++ packages/halo/react.mjs
@@ -0,0 +1,4 @@
+import Halo from './dist/esm/react/index'
+
+export default Halo
+export * from './dist/esm/vars/index'
--- packages/halo/src/index.ts
+++ packages/halo/src/index.ts
@@ -1,2 +1,5 @@
-export { default } from './react/index'
+import './css/index.css'
+import Halo from './react/index'
+
+export default Halo
 export * from './vars/index'
