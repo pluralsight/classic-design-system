096957c0877616a91ddd43ee43ae3e6c4328242c
jaketrent <trent.jake@gmail.com>
2021-08-23 16:06:47 -0600

    fix(icon): update /react export for named icons

--- packages/icon/react.js
+++ packages/icon/react.js
@@ -1,7 +1,23 @@
+/* eslint-disable no-prototype-builtins */
 module.exports = require('./dist/cjs/react/index.js')
 
-const vars = require('./dist/cjs/vars/index.js')
-Object.keys(vars).forEach(key => (exports[key] = vars[key]))
+const __createBinding = function (o, m, k, k2) {
+  if (k2 === undefined) k2 = k
+  Object.defineProperty(o, k2, {
+    enumerable: true,
+    get: function () {
+      return m[k]
+    }
+  })
+}
 
-const icons = require('./dist/cjs/react/icons/index.js')
-Object.keys(icons).forEach(key => (exports[key] = icons[key]))
+const __exportStar = function (m, exports) {
+  for (const p in m)
+    if (p !== 'default' && !exports.hasOwnProperty(p))
+      __createBinding(exports, m, p)
+}
+
+__exportStar(require('./dist/cjs/react/icons/index.js'), module.exports)
+
+const vars = require('./dist/cjs/vars/index.js')
+Object.keys(vars).forEach(key => __createBinding(module.exports, vars, key))
