ffba2e3e6fce443472667d9873259073f4a34819
jaketrent <trent.jake@gmail.com>
2021-08-24 14:33:20 -0600

    feat(textarea): allow separate import for react and css

--- packages/textarea/src/index.ts
+++ packages/textarea/src/index.ts
@@ -1,3 +1,8 @@
+import '@pluralsight/ps-design-system-halo/styles.css'
+import '@pluralsight/ps-design-system-icon/styles.css'
+
+import './css/index.css'
+
 export { default } from './react/index'
 
 export * from './vars/index'
