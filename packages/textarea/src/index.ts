00ff71dd4b3e82594b3c90fe7cf9774a0be0b795
jaketrent <trent.jake@gmail.com>
2021-08-24 14:51:52 -0600

    refactor(textarea): ensure src/index.ts styles are not treeshaken

--- packages/textarea/src/index.ts
+++ packages/textarea/src/index.ts
@@ -2,7 +2,8 @@ import '@pluralsight/ps-design-system-halo/styles.css'
 import '@pluralsight/ps-design-system-icon/styles.css'
 
 import './css/index.css'
+import TextArea from './react/index'
 
-export { default } from './react/index'
+export default TextArea
 
 export * from './vars/index'
