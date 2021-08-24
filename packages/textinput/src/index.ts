924ab081d636abcda3236738a8d74a4793a7dcc7
Edward Irby <edward-irby@pluralsight.com>
2021-08-24 11:33:33 -0700

    refactor(textinput): css import paths

--- packages/textinput/src/index.ts
+++ packages/textinput/src/index.ts
@@ -1,2 +1,5 @@
-export { default, TextInputProps } from './react/index'
+import './css/index.css'
+import TextInput from './react/index'
+export default TextInput
+export { TextInputProps } from './react/index'
 export * from './vars/index'
