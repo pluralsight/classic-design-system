7ec56db280350e12b33351fa41e7fd0e75140b6a
jaketrent <jake@jaketrent.com>
2021-08-27 11:41:08 -0600

    style(checkbox): lint

--- packages/checkbox/react.d.ts
+++ packages/checkbox/react.d.ts
@@ -1,13 +1,24 @@
-import React from 'react';
-interface CheckboxProps extends React.HTMLAttributes<HTMLDivElement>, Record<string, unknown> {
-    checked?: boolean;
-    disabled?: boolean;
-    error?: boolean;
-    indeterminate?: boolean;
-    label: React.ReactNode;
-    name?: string;
-    onCheck?: (evt: React.KeyboardEvent<HTMLLabelElement> | React.MouseEvent<HTMLLabelElement, MouseEvent>, checked: boolean, value: React.ReactText, name: string | undefined) => void;
-    value: string | number;
+import React from 'react'
+interface CheckboxProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    Record<string, unknown> {
+  checked?: boolean
+  disabled?: boolean
+  error?: boolean
+  indeterminate?: boolean
+  label: React.ReactNode
+  name?: string
+  onCheck?: (
+    evt:
+      | React.KeyboardEvent<HTMLLabelElement>
+      | React.MouseEvent<HTMLLabelElement, MouseEvent>,
+    checked: boolean,
+    value: React.ReactText,
+    name: string | undefined
+  ) => void
+  value: string | number
 }
-declare const Checkbox: React.ForwardRefExoticComponent<Pick<CheckboxProps, React.ReactText> & React.RefAttributes<HTMLInputElement>>;
-export default Checkbox;
+declare const Checkbox: React.ForwardRefExoticComponent<
+  Pick<CheckboxProps, React.ReactText> & React.RefAttributes<HTMLInputElement>
+>
+export default Checkbox
