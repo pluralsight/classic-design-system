65f78e30a27c2f0ac331902b996a43db7b5cd56c
jaketrent <jake@jaketrent.com>
2021-08-30 11:34:19 -0600

    refactor(textarea): remove separate style and react imports
    Feature implementation was to support NextJS, but NextJS is moving away
    from this restriction and there are workarounds.
    
    See #1977
    
    BREAKING CHANGE: Remove /styles.css and /react imports

--- packages/textarea/src/index.ts
+++ packages/textarea/src/index.ts
@@ -1,7 +1,3 @@
-import '@pluralsight/ps-design-system-halo/styles.css'
-import '@pluralsight/ps-design-system-icon/styles.css'
-
-import './css/index.css'
 import TextArea from './react/index'
 
 export default TextArea
