553e7c30b4b2cc5fcc4377e4c827aa5a6bb47323
jaketrent <jake@jaketrent.com>
2021-08-30 13:08:11 -0600

    refactor(icon): inline export for react in src/index.ts

--- packages/icon/src/index.ts
+++ packages/icon/src/index.ts
@@ -1,6 +1,4 @@
-import Icon from './react/index'
-
-export default Icon
+export { default } from './react/index'
 export * from './react/icons'
 export * as icons from './react/icons'
 export * from './vars/index'
