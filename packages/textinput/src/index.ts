dc645bee89a37c2917d41279a852116acf696682
jaketrent <jake@jaketrent.com>
2021-08-30 11:21:44 -0600

    refactor(textinput): remove separate style and react imports
    Feature implementation was to support NextJS, but NextJS is moving away
    from this restriction and there are workarounds.
    
    See #1977
    
    BREAKING CHANGE: Remove /styles.css and /react imports

--- packages/textinput/src/index.ts
+++ packages/textinput/src/index.ts
@@ -1,8 +1,5 @@
-import '@pluralsight/ps-design-system-halo/styles.css'
-import '@pluralsight/ps-design-system-icon/styles.css'
-
-import './css/index.css'
 import TextInput from './react/index'
 
 export default TextInput
+export * from './react/index'
 export * from './vars/index'
