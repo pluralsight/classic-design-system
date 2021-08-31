a1285659344903eb7fd24b826799d4516ca30f29
Brian Sweeting <b@sweeting.net>
2021-08-31 10:36:52 -0600

    refactor(docs): update usage component module to include links to source, changelog, NPM, Figma

--- docs/src/components/usage.tsx
+++ docs/src/components/usage.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 import { A } from './mdx/links'
+import Button from '@pluralsight/ps-design-system-button'
+import {
+  SocialGithubIcon,
+  ExternalLinkIcon,
+  ListIcon
+} from '@pluralsight/ps-design-system-icon'
 import * as styles from './usage.module.css'
 
 interface UsageProps {
+  figmaUrl: string
   install: string
   import: string
   packageName: string
@@ -11,17 +18,58 @@ interface UsageProps {
 }
 export const Usage: React.FC<UsageProps> = props => {
   return (
-    <ul className={styles.usage}>
-      <Item label="Version">
-        <A
-          href={`https://github.com/pluralsight/design-system/blob/master/packages/${props.packageName}/CHANGELOG.md`}
-        >
-          {props.version || 'CHANGELOG'}
-        </A>
-      </Item>
-      <Item label="Install">{props.install.trim()}</Item>
-      <Item label="Import">{props.import.trim()}</Item>
-    </ul>
+    <>
+      <ul className={styles.links}>
+        <li>
+          <Button
+            appearance="secondary"
+            href={`https://github.com/pluralsight/design-system/blob/master/packages/${props.packageName}`}
+            target="_blank"
+            size="small"
+            icon={<SocialGithubIcon />}
+          >
+            Source
+          </Button>
+        </li>
+        <li>
+          <Button
+            appearance="secondary"
+            href={`https://github.com/pluralsight/design-system/blob/master/packages/${props.packageName}/CHANGELOG.md`}
+            target="_blank"
+            size="small"
+            icon={<ListIcon />}
+          >
+            Changelog
+          </Button>
+        </li>
+        <li>
+          <Button
+            appearance="secondary"
+            href={`https://www.npmjs.com/package/@pluralsight/ps-design-system-${props.packageName}`}
+            target="_blank"
+            size="small"
+            icon={<ExternalLinkIcon />}
+          >
+            NPM
+          </Button>
+        </li>
+        <li>
+          <Button
+            appearance="secondary"
+            href={`${props.figmaUrl}`}
+            target="_blank"
+            size="small"
+            icon={<ExternalLinkIcon />}
+          >
+            Figma
+          </Button>
+        </li>
+      </ul>
+      <ul className={styles.usage}>
+        <Item label="Install">{props.install.trim()}</Item>
+        <Item label="Import">{props.import.trim()}</Item>
+      </ul>
+    </>
   )
 }
 
