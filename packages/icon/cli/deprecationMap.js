c5860cc6e359bd21522415a48abb723ac25ded27
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-02-28 16:56:54 -0500

    chore(icon): remove all deprecated icons

--- packages/icon/cli/deprecationMap.js
+++ packages/icon/cli/deprecationMap.js
@@ -1,14 +1,4 @@
 const deprecationMap = {
-  ChatFillIcon: 'ChatFilledIcon',
-  ChatAltFillIcon: 'ChatAltFilledIcon',
-  CheckCircleFillIcon: 'CheckCircleFilledIcon',
-  CloseCaptionedFillIcon: 'CloseCaptionedFilledIcon',
-  MenuCloseIcon: 'MenuArrowRightIcon',
-  PlusCircleFillIcon: 'PlusCircleFilledIcon',
-  StarFillIcon: 'StarFilledIcon',
-  ThumbDownFillIcon: 'ThumbDownFilledIcon',
-  ThumbUpFillIcon: 'ThumbUpFilledIcon',
-  XCircleFillIcon: 'XCircleFilledIcon'
 }
 
 module.exports = {
